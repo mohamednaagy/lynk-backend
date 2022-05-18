f57907adce06b8076be30761b4c56454ac51f3f6
Ahmed Medhat <ahmed.medhat12@outlook.com>
2022-05-18 12:45:53 +0300

    removing unecessary apis

--- Modules/Permission/Routes/web.php
+++ Modules/Permission/Routes/web.php
@@ -10,7 +10,3 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::prefix('permission')->group(function() {
-    Route::get('/', 'PermissionController@index');
-});
