ac8c512c63276c6030cc8e8ca50d33f160a99edd
Fady Ibrahim <fadysobhy111@gmail.com>
2022-06-05 15:41:52 +0200

    [Added] getAllPermissionsInSubjectAction function logic [Changed] logic of some other functions [Removed] the driver methodology from Grantify Facade

--- Modules/Permission/GrantifyManager.php
+++ Modules/Permission/GrantifyManager.php
@@ -3,11 +3,15 @@
 namespace Modules\Permission;
 
 use Illuminate\Support\Manager;
-use Modules\Permission\Contracts\GrantifyDriverInterface;
-use Modules\Permission\Drivers\SpatieDriver;
+use Modules\Permission\Contracts\Grantifiable;
+use Modules\Permission\Exceptions\PermissionNotFoundException;
+use Modules\Permission\Exceptions\RoleNotFoundException;
+use Modules\Permission\Traits\CanGrantify;
 
 class GrantifyManager extends Manager
 {
+    use CanGrantify;
+
     /**
      * Get the default driver name.
      *
@@ -15,17 +19,88 @@ class GrantifyManager extends Manager
      */
     public function getDefaultDriver()
     {
-        return config('permission.default', 'spatie');
+        //
     }
 
     /**
-     * Access the roles and permissions using spatie.
+     * get Role Permissions
      *
-     * @return GrantifyDriverInterface
+     * @param string $roleName
+     * @param string|null $guardName
+     * @return array
+     * @throws RoleNotFoundException
      */
-    public function createSpatieDriver(): GrantifyDriverInterface
+    public function getRolePermissions(string $roleName, string $guardName = null): array
     {
-        return new SpatieDriver();
+        $role = $this->findRole($roleName, $guardName);
+        return $role->permissions->toArray();
+    }
+
+    /**
+     * Get All Permissions for Model
+     *
+     * @param Grantifiable $grantifiable
+     * @return array
+     */
+    public function getAllPermissionsForModel(Grantifiable $grantifiable): array
+    {
+        return $grantifiable->getAllPermissions()->toArray();
+    }
+
+    /**
+     * Get All Permissions In Subject Action Format
+     *
+     * @param array $permissions
+     * @return array
+     */
+    public function getAllPermissionsInSubjectAction(array $permissions): array
+    {
+        $permissionsSubjectAction = [];
+
+        foreach ($permissions as $permission) {
+            $subjectAction = explode('.', $permission['name']);
+
+            if (array_key_exists(current($subjectAction), $permissionsSubjectAction))
+                array_push($permissionsSubjectAction[current($subjectAction)], end($subjectAction));
+            else
+                $permissionsSubjectAction[current($subjectAction)] = array(end($subjectAction));
+        }
+
+        return $permissionsSubjectAction;
+    }
+
+    /**
+     * Assign a permission to a role.
+     *
+     * @param string $roleName
+     * @param string $permissionName
+     * @param string|null $guardName
+     * @return void
+     * @throws PermissionNotFoundException
+     * @throws RoleNotFoundException
+     */
+    public function assignPermissionToRole(string $roleName, string $permissionName, string $guardName = null): void
+    {
+        $role = $this->findRole($roleName, $guardName);
+        $permission = $this->findPermission($permissionName, $guardName);
+        $role->givePermissionTo($permission);
+    }
+
+    /**
+     * Remove a permission that is assigned to a role.
+     *
+     * @param string $roleName
+     * @param string $permissionName
+     * @param string|null $guardName
+     * @return void
+     * @throws PermissionNotFoundException
+     * @throws RoleNotFoundException
+     */
+    public function removePermissionFromRole(string $roleName, string $permissionName, string $guardName = null): void
+    {
+        $role = $this->findRole($roleName, $guardName);
+        $permission = $this->findPermission($permissionName, $guardName);
+        $role->revokePermissionTo($permission);
     }
 
 }
