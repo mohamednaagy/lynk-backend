928aca4e1e4b8769ba6d1fac25dadead455b1cd8
Ahmed Medhat <ahmed.medhat12@outlook.com>
2022-10-06 07:28:42 +0300

    testing pint + hooks

--- app/Actions/AssignPermissionToUserAction.php
+++ app/Actions/AssignPermissionToUserAction.php
@@ -2,16 +2,16 @@
 
 namespace App\Actions;
 
-use App\Actions\Contracts\AssignPermissionToUser;
 use App\Models\User;
 use Modules\Grantify\Facades\Grantify;
 use Spatie\Permission\Models\Permission;
+use App\Actions\Contracts\AssignPermissionToUser;
 
 class AssignPermissionToUserAction implements AssignPermissionToUser
 {
     /**
-     * @param  User  $user
-     * @param  string|array|Permission  $permission
+     * @param User $user
+     * @param string|array|Permission $permission
      * @return void
      */
     public function handle(User $user, string|array|Permission $permission): void
