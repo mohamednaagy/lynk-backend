bd73d216ef326868c8d4d66bbd52e2ba0c373d4e
Ahmed Medhat <ahmed.medhat12@outlook.com>
2022-10-06 07:32:06 +0300

    testing pint + hooks

--- app/Actions/AssignRoleToUserAction.php
+++ app/Actions/AssignRoleToUserAction.php
@@ -2,20 +2,21 @@
 
 namespace App\Actions;
 
-use App\Models\User;
-use Spatie\Permission\Models\Role;
-use Modules\Grantify\Facades\Grantify;
 use App\Actions\Contracts\AssignRoleToUser;
+use App\Models\User;
+use Modules\Grantify\Facades\Grantify;
+use Spatie\Permission\Models\Role;
 
 class AssignRoleToUserAction implements AssignRoleToUser
 {
     /**
-     * @param User $user
-     * @param string|array|Role $role
+     * @param  User  $user
+     * @param  string|array|Role  $role
      * @return void
      */
     public function handle(User $user, string|array|Role $role): void
     {
+        //2test 3
         Grantify::assignRoleToModel($user, $role);
     }
 }
