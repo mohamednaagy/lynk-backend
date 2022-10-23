600624560f20bdd5bb464e988c4bb79e3ec92cee
Ahmed Medhat <ahmed.medhat12@outlook.com>
2022-10-23 14:38:00 +0300

    adding improvements

--- app/Http/Controllers/Api/V1/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Api/V1/Auth/ResetPasswordController.php
@@ -23,28 +23,34 @@ class ResetPasswordController extends Controller
      */
     public function __invoke(ResetPasswordRequest $request)
     {
+        $company = null;
+
         if (($companyUniqueName = $request->safeInput('company_unique_name')) != null) {
             $company = Company::where('unique_name', $companyUniqueName)->first();
             tenancy()->initialize($company);
         }
 
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token') + ['company_id' => tenant('id')],
+        Password::reset(
+            $request->validated(
+                ['email', 'password', 'password_confirmation', 'token']
+            ) + [
+                function ($query) use ($company) {
+                    if ($company === null) {
+                        $query->whereNull('company_id');
+                    }
+                },
+            ],
             function ($user, $password) {
                 $user->forceFill([
                     'password' => Hash::make($password),
                 ])->setRememberToken(Str::random(60));
+
                 $user->save();
+
                 event(new PasswordReset($user));
             }
         );
 
-        return $status == Password::PASSWORD_RESET
-            ? $this->successResponse([
-                'message' => trans($status),
-            ])
-            : throw ValidationException::withMessages([
-                'email' => [trans($status)],
-            ]);
+        return $this->successResponse();
     }
 }
