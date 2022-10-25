8770f46ee1ece8fb5c2d6e9bf088b62e39e3287a
Ahmed Medhat <ahmed.medhat12@outlook.com>
2022-10-25 13:08:06 +0300

    fixing balances

--- app/Http/Controllers/Api/V1/Lender/Wallets/GetBalance.php
+++ app/Http/Controllers/Api/V1/Lender/Wallets/GetBalance.php
@@ -14,8 +14,8 @@ class GetBalance extends Controller
         $balances = $getBalance->handle(tenant());
 
         return $this->successResponse(data: [
-            'balance' => number_format($getBalance['balance'], 2),
-            'available_orders' => number_format($getBalance['availableOrders'], 2),
+            'balance' => number_format($balances['balance'], 2),
+            'available_orders' => number_format($balances['availableOrders'], 2),
         ]);
     }
 }
