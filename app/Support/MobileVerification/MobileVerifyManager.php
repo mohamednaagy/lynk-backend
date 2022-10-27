25f1e38ba84f799f1ed785871aac8ac24c979d44
Fady Ibrahim <fadysobhy111@gmail.com>
2022-10-27 18:50:27 +0200

    [Mobile Verification] Add the drivers methodology to integrate with tcc provider for mobile number verification

---
+++ app/Support/MobileVerification/MobileVerifyManager.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Support\MobileVerification;
+
+use App\Support\MobileVerification\Contracts\MobileVerifyDriverInterface;
+use App\Support\MobileVerification\Drivers\TCCDriver;
+use Illuminate\Support\Manager;
+
+class MobileVerifyManager extends Manager
+{
+    /**
+     * Get the default driver name.
+     *
+     * @return string
+     */
+    public function getDefaultDriver(): string
+    {
+        return config('mobile-verify.default', 'tcc');
+    }
+
+    /**
+     * Verify mobile number via TCC.
+     *
+     * @return MobileVerifyDriverInterface
+     */
+    public function createTccDriver(): MobileVerifyDriverInterface
+    {
+        return new TCCDriver();
+    }
+
+    /**
+     * Get all the drivers.
+     *
+     * @return array
+     */
+    public function getMobileVerifyDrivers(): array
+    {
+        return array_keys(config('mobile-verify.drivers'));
+    }
+}
