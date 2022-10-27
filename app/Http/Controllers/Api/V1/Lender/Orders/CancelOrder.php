11568b5b91f25a25a2cdd8aa6064c1be59d49383
KhuloodBatis <k.batis@bimventures.com>
2022-10-27 14:35:34 +0300

    LYNKB-56-Lender-cancel-order-endpoint

---
+++ app/Http/Controllers/Api/V1/Lender/Orders/CancelOrder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Lender\Orders;
+
+use App\Actions\Contracts\Orders\CancelOrder as CancelOrderInterface;
+use App\Enums\FinancingOrderStatus;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\Lender\Orders\CancelOrderRequest;
+use App\Models\FinancingOrder;
+use Illuminate\Http\JsonResponse;
+
+class CancelOrder extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  CancelOrderRequest  $cancelOrderRequest
+     * @param  CancelOrderInterface  $cancelOrder,
+     * @param  FinancingOrder  $order
+     * @return JsonResponse
+     */
+    public function __invoke(CancelOrderRequest $cancelOrderRequest, CancelOrderInterface $cancelOrder, FinancingOrder $order): JsonResponse
+    {
+        if ($order->status->is(FinancingOrderStatus::PendingApproval)) {
+            $order->update(['status' => 3]);
+        }
+
+        $cancelOrder->handle(
+            $order,
+            $cancelOrderRequest->user(),
+            $cancelOrderRequest->validated()
+        );
+
+        return $this->successResponse();
+    }
+}
