61241ba9c8061840a97a3a56e3ec0c424127dc33
Ahmed Hamdi <ahmed.hamdi181@gmail.com>
2022-10-31 11:58:38 +0200

    Update DMCC notification handler

--- app/Support/Traders/Drivers/DmccDriver.php
+++ app/Support/Traders/Drivers/DmccDriver.php
@@ -8,21 +8,6 @@ class DmccDriver
 {
     public function createTTI(): string
     {
-        return (new DMCCService())->getTTI('', '', '');
-    }
-
-    public function createPTP(): string
-    {
-        return (new DMCCService())->respondPTPService('');
-    }
-
-    public function createMPO(): string
-    {
-        return (new DMCCService())->issueMurabahaPurchaseOffer('');
-    }
-
-    public function processNotifications(): string
-    {
-        return 'processNotifications';
+//        return (new DMCCService())->getTTI('', '', '');
     }
 }
