6219e63a888dde65cf055d87a58e155c534c0bb7
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2022-11-01 13:30:28 +0200

    WIP

--- app/Http/Controllers/Api/V1/Lender/Auth/UpdateMyProfile.php
+++ app/Http/Controllers/Api/V1/Lender/Auth/UpdateMyProfile.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1\Lender\Auth;
 use App\Actions\Contracts\UpdateUser;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Lender\Auth\UpdateMyProfileRequest;
+use Illuminate\Support\Arr;
 
 class UpdateMyProfile extends Controller
 {
@@ -17,7 +18,12 @@ class UpdateMyProfile extends Controller
      */
     public function __invoke(UpdateUser $updateUser, UpdateMyProfileRequest $updateLenderRequest)
     {
-        $updateUser->handle(auth()->user(), $updateLenderRequest->validated());
+        $vaLidated = $updateLenderRequest->validated();
+        if (array_key_exists('password', $vaLidated) && is_null($vaLidated['password'])) {
+            $vaLidated = Arr::except($vaLidated, 'password');
+        }
+
+        $updateUser->handle(auth()->user(), $vaLidated);
 
         return $this->successResponse();
     }
