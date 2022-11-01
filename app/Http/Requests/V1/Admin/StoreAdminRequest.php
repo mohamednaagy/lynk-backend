be85397c67bc79f23a8c0867c704c2d721230c15
Ahmed Bally <ahmedbaly87@gmail.com>
2022-11-01 07:04:32 +0200

    Increase Password Security

--- app/Http/Requests/V1/Admin/StoreAdminRequest.php
+++ app/Http/Requests/V1/Admin/StoreAdminRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\V1\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 use function trans;
 
 class StoreAdminRequest extends FormRequest
@@ -30,7 +31,7 @@ class StoreAdminRequest extends FormRequest
             'phone_country_code' => ['required_with:phone_number', 'string', 'size:2'],
             'phone_number' => ['required', 'phone:phone_country_code', 'string'],
             'email' => ['required', 'email', 'unique:users,email'],
-            'password' => ['required', 'confirmed', 'min:8'],
+            'password' => ['required', 'confirmed', Password::defaults()],
         ];
 
         if (! empty($this->role)) {
