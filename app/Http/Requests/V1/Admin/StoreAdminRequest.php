245e29180fd7868f6dc4cb0acc530072e5daba5b
Ahmed Bally <ahmedbaly87@gmail.com>
2022-11-01 13:53:42 +0000

    Update StoreAdminRequest.php

--- app/Http/Requests/V1/Admin/StoreAdminRequest.php
+++ app/Http/Requests/V1/Admin/StoreAdminRequest.php
@@ -7,9 +7,6 @@ use App\Enums\Subject;
 use App\Rules\HostWhitelistRule;
 use BenSampo\Enum\Rules\EnumValue;
 use Illuminate\Foundation\Http\FormRequest;
-<<<<<<< app/Http/Requests/V1/Admin/StoreAdminRequest.php
-use Illuminate\Validation\Rules\Password;
-use function trans;
 
 class StoreAdminRequest extends FormRequest
 {
@@ -34,7 +31,6 @@ class StoreAdminRequest extends FormRequest
             'first_name' => ['required', 'string', 'min:3', 'max:100'],
             'last_name' => ['required', 'string', 'min:3', 'max:100'],
             'email' => ['required', 'email', 'unique:users,email'],
-            'password' => ['required', 'confirmed', Password::defaults()],
             'permissions' => ['required', 'array', 'min:1'],
             'permissions.*' => ['required', 'array'],
             'permissions.*.subject' => ['required', 'string', new EnumValue(Subject::class)],
@@ -42,28 +38,6 @@ class StoreAdminRequest extends FormRequest
             'permissions.*.actions.*' => ['required', 'string', new EnumValue(Action::class)],
             'redirect_url' => ['required', 'url', new HostWhitelistRule()],
         ];
-
-        if (! empty($this->role)) {
-            $rules['role'] = ['required', 'string', 'exists:roles,name'];
-        }
-
-        if (! empty($this->permissions)) {
-            $rules['permissions.*'] = ['required', 'array', 'distinct'];
-        }
-
-        return $rules;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function messages(): array
-    {
-        return [
-            'phone_number.phone' => trans('validation.phone'),
-
-        ];
     }
 }
+
