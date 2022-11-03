733becf8c166e24c5207a69b599948d92781faa7
Ahmed Bally <ahmedbaly87@gmail.com>
2022-11-03 03:48:47 +0200

    fix settings

--- app/Settings/Classes/Areas/LenderSettings.php
+++ app/Settings/Classes/Areas/LenderSettings.php
@@ -6,6 +6,10 @@ use Spatie\LaravelSettings\Settings;
 
 class LenderSettings extends Settings
 {
+    public int $company_registration_status;
+
+    public int $company_created_by_operation_status;
+
     public bool $email_verification_enabled;
 
     public float $order_cost;
