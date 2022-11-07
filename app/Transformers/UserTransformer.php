48cc04ef1be7eaba53e4bea1da4af0b073d21d3a
youssofOkiel <y.okiel@bimsaudi.com>
2022-11-07 12:00:05 +0200

    {updated} fix response

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -62,7 +62,16 @@ class UserTransformer extends TransformerAbstract
 
     public function includeCompany(User $user)
     {
-        return $this->item($user->company, new CompanyTransformer());
+        return $this->primitive([
+            'id' => $user->company->id,
+            'name' => $user->company->name,
+            'status' => [
+                'value' => $user->company->status->value,
+                'description' => $user->company->status->description,
+            ],
+            'orders_count' => $user->company->orders_count,
+            'created_at' => optional($user->company->created_at)->format('Y-m-d'),
+        ]);
     }
 
     public function includePermissions(User $user)
