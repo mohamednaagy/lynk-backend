47833c54b9816c9d079387eebeda101f22ba09c8
Ahmed Bally <ahmedbaly87@gmail.com>
2022-11-13 21:50:22 +0200

    can move state to other

--- app/Enums/FinancingOrderStatus.php
+++ app/Enums/FinancingOrderStatus.php
@@ -2,8 +2,10 @@
 
 namespace App\Enums;
 
+use App\Enums\FinancingOrderStatus as Status;
 use BenSampo\Enum\Contracts\LocalizedEnum;
 use BenSampo\Enum\Enum;
+use UnexpectedValueException;
 
 final class FinancingOrderStatus extends Enum implements LocalizedEnum
 {
@@ -38,4 +40,75 @@ final class FinancingOrderStatus extends Enum implements LocalizedEnum
     const PtpDocumentRetrieved = 15;
 
     const PendingCancel = 15;
+
+    private static array $state = [
+        self::InProgress => [
+            self::PendingApproval,
+        ],
+        self::Rejected => [
+            self::PendingApproval,
+        ],
+        self::Completed => [
+            self::MurabahaSaleCompleted,
+        ],
+        self::Canceled => [
+            self::Rejected,
+            self::InProgress,
+            self::RespondedToPtp,
+            self::PendingApproval,
+            self::CommodityPurchased,
+            self::WaitingClientWakala,
+            self::PtpDocumentRetrieved,
+            self::ClientWakalaCompleted,
+            self::CommoditySoldToCustomer,
+            self::WaitingPurchasingCommodity,
+        ],
+        self::WaitingClientWakala => [
+            self::InProgress,
+        ],
+        self::ClientWakalaCompleted => [
+            self::WaitingClientWakala,
+        ],
+        self::WaitingPurchasingCommodity => [
+            self::ClientWakalaCompleted,
+        ],
+        self::RespondedToPtp => [
+            self::WaitingPurchasingCommodity,
+        ],
+        self::PtpDocumentRetrieved => [
+            self::RespondedToPtp,
+        ],
+        self::CommodityPurchased => [
+            self::PtpDocumentRetrieved,
+        ],
+        self::ContractSigned => [
+            self::CommodityPurchased,
+        ],
+        self::CommoditySoldToCustomer => [
+            self::ContractSigned,
+        ],
+        self::MurabhaOfferIssued => [
+            self::CommoditySoldToCustomer,
+        ],
+        self::MurabahaSaleCompleted => [
+            self::MurabhaOfferIssued,
+        ],
+    ];
+
+    /**
+     * @param  Status|int  $status
+     * @return bool
+     */
+    public function canMoveTo(Status|int $status): bool
+    {
+        if (! isset(self::$state[$this->value])) {
+            throw new UnexpectedValueException('status not exists');
+        }
+
+        if ($status instanceof Status) {
+            $status = $status->value;
+        }
+
+        return in_array($status, self::$state[$this->value]);
+    }
 }
