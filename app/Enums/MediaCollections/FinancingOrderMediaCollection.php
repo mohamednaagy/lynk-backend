17ec626ad1f4943c4ae8f5d1db0336890529e450
youssofOkiel <y.okiel@bimsaudi.com>
2022-11-14 11:38:55 +0200

    {removed}  bank wakala

--- app/Enums/MediaCollections/FinancingOrderMediaCollection.php
+++ app/Enums/MediaCollections/FinancingOrderMediaCollection.php
@@ -12,8 +12,6 @@ final class FinancingOrderMediaCollection extends Enum
 
     const LenderWakala = 'lender_wakala';
 
-    const BankWakala = 'bank_wakala';
-
     const PowerOfAttorney = 'power_of_attorney';
 
     const PromiseToPurchase = 'promise_to_purchase';
