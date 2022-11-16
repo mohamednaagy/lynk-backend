7711dada9c3955ba433a7b8ca1158423e53b2710
youssofOkiel <y.okiel@bimsaudi.com>
2022-11-16 15:19:38 +0200

    {updated} localization middleware

--- app/Http/Middleware/SetLocalization.php
+++ app/Http/Middleware/SetLocalization.php
@@ -3,29 +3,28 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SetLocalization
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse) $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param  Request  $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
-        $response = $next($request);
-
         if ($request->hasHeader('X-Locale')) {
             app()->setLocale($request->header('X-Locale'));
         } elseif ($request->user() && ! is_null($request->user()->locale)) {
             $locale = $request->user()->locale;
             app()->setLocale($locale);
-            $response->header('X-Locale', $locale);
         }
 
-        return $response;
+        return $next($request);
     }
 }
