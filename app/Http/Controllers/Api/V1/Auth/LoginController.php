430720e0799699abb01d512f09815448178b9b28
Ahmed Medhat <ahmed.medhat12@outlook.com>
2022-11-16 13:31:34 +0300

    adding changes

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -36,7 +36,6 @@ class LoginController extends Controller
         }
 
         // __REVIEW__: If company is null (not initialized), this should be reflected in the query to scope it company_id is null
-        // accounts
         $user = User::where('email', $request->safeInput('email'))->first();
 
         if ($user === null || ! Hash::check($request->safeInput('password'), $user->password)) {
