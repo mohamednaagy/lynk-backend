5011d0029445c0f3aeb7d8164c0a92276b769cbd
youssofOkiel <y.okiel@bimsaudi.com>
2022-11-17 10:25:45 +0200

    {updated} some changes

--- app/Http/Middleware/SetUserPreferredLocale.php
+++ app/Http/Middleware/SetUserPreferredLocale.php
@@ -7,7 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-class SetUserLocale
+class SetUserPreferredLocale
 {
     /**
      * Handle an incoming request.
