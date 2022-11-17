7b1adacec96e40f3e8b64b848fafd66444f2e8bd
Ahmed Medhat <ahmed.medhat12@outlook.com>
2022-11-17 13:44:16 +0300

    adding review

--- app/Http/Controllers/Api/V1/Lender/Auth/UpdateMyProfile.php
+++ app/Http/Controllers/Api/V1/Lender/Auth/UpdateMyProfile.php
@@ -17,6 +17,7 @@ class UpdateMyProfile extends Controller
      * @param  UpdateMyProfileRequest  $updateLenderRequest
      * @return \Illuminate\Http\JsonResponse
      */
+    // __REVIEW__ : Always Request/FormRquest comes first in the arguments
     public function __invoke(UpdateUser $updateUser, UpdateMyProfileRequest $updateLenderRequest)
     {
         $validated = $updateLenderRequest->validated();
