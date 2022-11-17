a604c0e6aa4aa896aaa2fced9daf2719ab31a3f8
Ahmed Medhat <ahmed.medhat12@outlook.com>
2022-11-17 10:58:36 +0300

    adding another changes

--- app/Settings/Classes/Areas/LenderSettings.php
+++ app/Settings/Classes/Areas/LenderSettings.php
@@ -6,12 +6,15 @@ use Spatie\LaravelSettings\Settings;
 
 class LenderSettings extends Settings
 {
+    // __REVIEW__ $default_company_registration_status
     public int $company_registration_status;
 
+    // __REVIEW__ $default_company_created_by_operation_status
     public int $company_created_by_operation_status;
 
     public bool $email_verification_enabled;
 
+    // __REVIEW__ $default_order_cost
     public float $order_cost;
 
     public static function group(): string
