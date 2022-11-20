e7448e852058b483b5f9b62fb4d3f89e0b6e2752
Ahmed Hamdi <ahmed.hamdi181@gmail.com>
2022-11-20 16:01:35 +0200

    send sms to client with order updates

--- app/Observers/FinancingOrderObserver.php
+++ app/Observers/FinancingOrderObserver.php
@@ -4,9 +4,9 @@ namespace App\Observers;
 
 use App\Enums\ClientMessage;
 use App\Enums\FinancingOrderStatus;
+use App\Enums\MediaCollections\FinancingOrderMediaCollection;
 use App\Models\FinancingOrder;
 use App\Support\Sms\Sms;
-use Illuminate\Support\Facades\Log;
 
 class FinancingOrderObserver
 {
@@ -16,25 +16,23 @@ class FinancingOrderObserver
      * @param  FinancingOrder  $financingOrder
      * @return void
      */
-    public function updated(FinancingOrder $financingOrder)
+    public function updated(FinancingOrder $financingOrder): void
     {
-        $res = match ($financingOrder->status->value) {
+        match ($financingOrder->status->value) {
             FinancingOrderStatus::CommoditySoldToCustomer => Sms::driver('msegat')->send(
                 __(ClientMessage::CommoditySoldToCustomer, [
-                    'product' => '',
-                    'quantity' => '',
+                    'product' => $financingOrder->activeTraderOrder()->first()->data->product,
+                    'quantity' => $financingOrder->activeTraderOrder()->first()->data->quantity,
                     'sellingPrice' => $financingOrder->getOriginal('selling_price'),
-                    'url' => '',
-                ]), $financingOrder->mobileDialingPhoneNumber),
+                    'url' => $financingOrder->getMedia(FinancingOrderMediaCollection::SellingCommodityToCustomer)->first(),
+                ], app()->getLocale()), ltrim($financingOrder->getPhoneNumber()->formatE164(), '+')),
             FinancingOrderStatus::MurabahaSaleCompleted => Sms::driver('msegat')->send(
                 __(ClientMessage::MurabahaSaleCompleted, [
-                    'product' => '',
-                    'quantity' => '',
+                    'product' => $financingOrder->activeTraderOrder()->first()->data->product,
+                    'quantity' => $financingOrder->activeTraderOrder()->first()->data->quantity,
                     'amount' => $financingOrder->getOriginal('selling_price'),
-                ]), $financingOrder->mobileDialingPhoneNumber),
+                ], app()->getLocale()), ltrim($financingOrder->getPhoneNumber()->formatE164(), '+')),
             default => new \ErrorException('Error found'),
         };
-
-        Log::info($res);
     }
 }
