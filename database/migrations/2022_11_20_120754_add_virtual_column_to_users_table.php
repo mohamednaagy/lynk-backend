8b9b11a1c12610072c40261d2752fb70d69023ac
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2022-11-20 15:12:30 +0200

    [Admin] Users unique constraint

---
+++ database/migrations/2022_11_20_120754_add_virtual_column_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('virtual_company_email')
+                ->virtualAs('concat_ws("",company_id,email)')
+                ->unique()
+                ->after('company_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('virtual_company_email');
+        });
+    }
+};
