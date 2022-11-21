080004bfaa94651333b52ccfe851c1f587ad31d5
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2022-11-21 15:16:05 +0200

    WIP

--- app/Http/Controllers/Api/V1/Lender/Orders/RejectOrder.php
+++ app/Http/Controllers/Api/V1/Lender/Orders/RejectOrder.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Lender\Orders\RejectOrderRequest;
 use App\Models\FinancingOrder;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class RejectOrder extends Controller
 {
@@ -22,16 +23,20 @@ class RejectOrder extends Controller
     public function __invoke(
         RejectOrderRequest $rejectOrderRequest,
         RejectOrderInterface $rejectOrder,
-        FinancingOrder $order
+        int $order
     ): JsonResponse {
-        if ($order->status->cantMoveTo(FinancingOrderStatus::Rejected)) {
-            return $this->errorResponse(
-                __('error.order_cannot_be_approved_because_it_is_approved')
-            );
-        }
+        return DB::transaction(function () use ($rejectOrderRequest, $rejectOrder, $order) {
+            $order = FinancingOrder::lockForUpdate()->findOrFail($order);
 
-        $rejectOrder->handle($order, $rejectOrderRequest->user(), $rejectOrderRequest->validated());
+            if ($order->status->cantMoveTo(FinancingOrderStatus::Rejected)) {
+                return $this->errorResponse(
+                    __('error.order_cannot_be_approved_because_it_is_approved')
+                );
+            }
 
-        return $this->successResponse();
+            $rejectOrder->handle($order, $rejectOrderRequest->user(), $rejectOrderRequest->validated());
+
+            return $this->successResponse();
+        });
     }
 }
