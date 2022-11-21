6689671a1c3d1e5a345ec1111443651c0e6a5d45
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2022-11-21 16:27:31 +0200

    WIP

--- database/migrations/2022_11_21_142608_drop_company_id_with_email_unique_from_users.php
+++ database/migrations/2022_11_21_142608_drop_company_id_with_email_unique_from_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,7 @@ return new class() extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('virtual_company_id_email')
-                ->virtualAs('concat_ws(":",company_id,email)')
-                ->unique()
-                ->after('company_id');
+            $table->dropUnique(['email', 'company_id']);
         });
     }
 
@@ -29,7 +26,7 @@ return new class() extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('virtual_company_email');
+            //
         });
     }
 };
