31576b7d8e2a3ad32ada84cb52c4138e1335cce6
Fady Ibrahim <fadysobhy111@gmail.com>
2022-11-22 10:32:17 +0200

    [Auth] Added the forgot password feature test

---
+++ tests/Feature/Endpoints/Api/V1/Auth/ForgotPasswordTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature\Endpoints\Api\V1\Auth;
+
+use App\Models\User;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Password;
+use Tests\TestCase;
+
+class ForgotPasswordTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Sends the password reset email when the user exists.
+     *
+     * @return void
+     */
+    public function testSendsPasswordResetEmail(): void
+    {
+        Notification::fake();
+
+        $user = User::factory()->create();
+
+        $response = $this->post('api/v1/auth/send-reset-password-link', [
+            'email' => $user->email,
+            'redirect_url' => 'http://localhost:8000/api/v1/reset-password',
+        ]);
+
+        $response->assertStatus(200);
+
+        Notification::assertSentTo($user, ResetPassword::class);
+    }
+
+    /**
+     * Does not send a password reset email when the user does not exist.
+     *
+     * @return void
+     */
+    public function testDoesNotSendPasswordResetEmail(): void
+    {
+        $this->doesntExpectJobs(ResetPassword::class);
+
+        $this->post('api/v1/auth/send-reset-password-link', ['email' => 'invalid@email.com']);
+    }
+
+    /**
+     * Allows a user to reset their password.
+     *
+     * @return void
+     */
+    public function testChangesAUsersPassword(): void
+    {
+        $user = User::factory()->create();
+
+        $token = Password::createToken($user);
+
+        $response = $this->post('api/v1/auth/reset-password', [
+            'token' => $token,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $this->assertTrue(Hash::check('password', $user->fresh()->password));
+    }
+}
