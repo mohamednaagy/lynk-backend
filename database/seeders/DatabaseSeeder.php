38590eddc1345d7f3583d532e61f27d469cabf73
youssofOkiel <y.okiel@bimsaudi.com>
2022-11-22 13:44:26 +0200

    {updated} add fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // UserSeeder::class,
-            FinancingOrderSeeder::class,
+            //             UserSeeder::class,
+            //            FinancingOrderSeeder::class,
         ]);
     }
 }
