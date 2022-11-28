256c9285bd2355942d4346ade279d7ea6a0e2f1d
Ahmed Hamdi <ahmed.hamdi181@gmail.com>
2022-11-29 00:21:36 +0200

    Update PR to fit pattern we agreed on

--- tests/Feature/Endpoints/Api/V1/Edaat/WebhookTest.php
+++ tests/Feature/Endpoints/Api/V1/Edaat/WebhookTest.php
@@ -3,23 +3,20 @@
 namespace Tests\Feature\Endpoints\Api\V1\Edaat;
 
 use App\Enums\Role;
-use App\Enums\WalletType;
 use App\Models\Company;
-use App\Models\EdaatInvoice;
 use App\Models\User;
-use Bavix\Wallet\Internal\Exceptions\ExceptionInterface;
 use Bavix\Wallet\Models\Transaction;
 use Bavix\Wallet\Models\Wallet;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Modules\Grantify\Facades\Grantify;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
+use Tests\Traits\InteractsWithLender;
 
 class WebhookTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, InteractsWithLender;
 
     private static Company $company;
 
@@ -31,58 +28,24 @@ class WebhookTest extends TestCase
 
     /**
      * @return void
-     *
-     * @throws ExceptionInterface
      */
     public function setUp(): void
     {
         parent::setUp();
 
-        self::$company = Company::factory()->create([
-            'first_name' => 'firstName',
-            'last_name' => 'lastName',
-            'phone_country_code' => 'SA',
-            'phone_number' => '503811000',
-            'email' => 'test@uselynk.test',
-            'password' => 'Qwer@1234',
-            'source' => 'Postman',
-            'company_name' => 'companyName',
-            'company_unique_name' => 'lynk05',
-            'company_cr' => '12345678910',
-        ]);
-
-        self::$wallet = self::$company->createWallet([
-            'name' => WalletType::CompanyWallet,
-            'slug' => WalletType::CompanyWallet,
-        ]);
-
-        self::$wallet->deposit(2000);
-
-        self::$userLender = User::factory()->create([
-            'email' => 'lender@bim.com',
-            'password' => bcrypt('12345678'),
-            'company_id' => self::$company->getOriginal('id'),
-        ]);
-
-        Grantify::assignRoleToModel(self::$userLender, Role::LenderAdmin);
-
-        self::$edaatInvoice = EdaatInvoice::query()->create([
-            'company_id' => self::$company->getOriginal('id'),
-            'creator_id' => self::$userLender->getOriginal('id'),
-            'invoice_number' => 1,
-            'amount' => 100,
-            'status' => 1,
-        ]);
+        [self::$company, self::$wallet] = $this->createCompany('2000', ['company_cr' => '12345678910']);
+        self::$userLender = $this->createLenderUser(self::$company->id, Role::LenderAdmin, 'lenderAdmin@bim.com');
+        self::$edaatInvoice = $this->createEdaatInvoice(self::$company->id, self::$userLender->id);
     }
 
-    public function testEdaatInvoicesWebhookWithPaidInvoiceSuccess()
+    public function test_edaat_invoices_webhook_with_paid_invoice_success()
     {
-        $this->withHeader('X-Company', self::$company->getOriginal('id'))
+        $this->withHeader('X-Company', self::$company->id)
             ->postJson('api/edaat/webhook/payment', [
                 [
                     'InvoiceNo' => '90510539184806',
                     'BillNo' => '9051053918479001',
-                    'InternalCode' => self::$edaatInvoice->getOriginal('id'),
+                    'InternalCode' => self::$edaatInvoice->id,
                     'PaymentAmount' => 3000,
                     'PaymentDate' => '2022-11-15T19:35:15.2960584+03:00',
                     'ProductIds' => [10559],
@@ -91,19 +54,19 @@ class WebhookTest extends TestCase
             ])->assertStatus(Response::HTTP_OK);
         $transactions = Transaction::all();
 
-        $this->assertEquals(self::$edaatInvoice->getOriginal('amount'), $transactions->last()->amount);
+        $this->assertEquals(self::$edaatInvoice->amount, $transactions->last()->amount);
         $this->assertEquals('deposit', $transactions->last()->type);
         $this->assertDatabaseCount(Transaction::class, 2);
     }
 
     public function testEdaatInvoicesWebhookWithUnPaidInvoiceFail()
     {
-        $this->withHeader('X-Company', self::$company->getOriginal('id'))
+        $this->withHeader('X-Company', self::$company->id)
             ->postJson('api/edaat/webhook/payment', [
                 [
                     'InvoiceNo' => '1',
                     'BillNo' => '1',
-                    'InternalCode' => self::$edaatInvoice->getOriginal('id'),
+                    'InternalCode' => self::$edaatInvoice->id,
                     'PaymentAmount' => 2,
                     'PaymentDate' => '2022-11-15T19:35:15.2960584+03:00',
                     'ProductIds' => [10559],
