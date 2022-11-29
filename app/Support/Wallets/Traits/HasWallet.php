c976de99a5ab32ddcb03f77702d44ff23cea0823
Ahmed Bally <ahmedbaly87@gmail.com>
2022-11-29 15:42:47 +0200

    enhancements

--- app/Support/Wallets/Traits/HasWallet.php
+++ app/Support/Wallets/Traits/HasWallet.php
@@ -4,52 +4,45 @@ namespace App\Support\Wallets\Traits;
 
 use App\Models\Wallet;
 use App\Support\Wallets\Contracts\WalletServiceInterface;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 trait HasWallet
 {
     public function getWallet(string $name, bool $lock = true): Wallet
     {
-        /** @var Model $this */
         return app(WalletServiceInterface::class)->findByName($this, $name, $lock);
     }
 
     public function getWalletOrFail(string $name, bool $lock = true)
     {
-        /** @var Model $this */
         return app(WalletServiceInterface::class)->findByNameOrFail($this, $name, $lock);
     }
 
     public function getWallets(string $name = null, bool $lock = true)
     {
-        /** @var Model $this */
         return app(WalletServiceInterface::class)->getWallets(model: $this, name: $name, lock: $lock);
     }
 
     public function hasWallet(string $name): bool
     {
-        /** @var Model $this */
         return (bool) app(WalletServiceInterface::class)->hasWallet($this, $name);
     }
 
-    public function createWallet(string $name, string $currency = null)
+    public function createWallet(string $name, string $currency): Wallet
     {
-        /** @var Model $this */
         return app(WalletServiceInterface::class)->create($this, [
             'name' => $name,
             'currency' => $currency,
         ]);
     }
 
-    public function transactions(string $walletName)
+    public function transactions(string $walletName): Builder
     {
-        /** @var Model $this */
         return app(WalletServiceInterface::class)->transactions($this, $walletName);
     }
 
     public function balance(string $walletName)
     {
-        /** @var Model $this */
         return app(WalletServiceInterface::class)->balance($this, $walletName);
     }
 }
