ef36cb32408b8285de29c74d6073b84121f789c7
Ahmed Medhat <ahmed.medhat12@outlook.com>
2022-11-30 11:51:28 +0300

    adding fixes to migration

--- database/migrations/2022_10_10_093100_add_company_id_to_users_table.php
+++ database/migrations/2022_10_10_093100_add_company_id_to_users_table.php
@@ -21,8 +21,12 @@ return new class extends Migration
                 ->constrained()
                 ->cascadeOnDelete();
 
+            $table->string('virtual_company_id_email')
+                ->virtualAs('concat_ws(":",company_id,email)')
+                ->unique()
+                ->after('company_id');
+
             $table->dropUnique(['email']);
-            $table->unique(['email', (new Company())->getForeignKey()]);
         });
     }
 
@@ -36,7 +40,8 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->dropForeignIdFor(Company::class);
             $table->unique('email');
-            $table->dropUnique(['email', (new Company())->getForeignKey()]);
+            $table->dropUnique(['virtual_company_id_email']);
+            $table->dropColumn('virtual_company_id_email');
         });
     }
 };
