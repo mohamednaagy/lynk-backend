8ddc531ac612575db5498898b2bc69b5080f5912
youssofOkiel <y.okiel@bimsaudi.com>
2022-12-01 16:10:48 +0200

    {updated} some fix

--- app/Http/Controllers/Api/V1/Lender/Orders/CreateOrderWithoutVerification.php
+++ app/Http/Controllers/Api/V1/Lender/Orders/CreateOrderWithoutVerification.php
@@ -45,6 +45,7 @@ class CreateOrderWithoutVerification extends Controller
                 $canCreateOrder->handle($company);
 
                 $financingOrder = $createFinancingOrder->handle(
+                    $company,
                     array_merge(
                         $request->validated(),
                         [
