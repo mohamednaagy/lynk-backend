f9cfe29e185016ea688a2e5ca45eae13fdb2a4db
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2022-12-02 03:41:54 +0200

    WIP

--- tests/Feature/Endpoints/Api/V1/Lender/FinancingOrders/RejectOrderControllerTest.php
+++ tests/Feature/Endpoints/Api/V1/Lender/FinancingOrders/RejectOrderControllerTest.php
@@ -29,13 +29,19 @@ class RejectOrderControllerTest extends TestCase
 
     private static User $userLenderAdmin;
 
-    private static User $customer;
+    private static User $userLenderBilling;
+
+    private static User $userLendersupervisor;
+
+    private static User $userLenderCreator;
 
     private static User $userLenderAdminWithoutEmailVerification;
 
     private static array $orderDetails;
 
     private static FinancingOrder $financingOrder;
+
+    private static FinancingOrder $rejectedOrder;
 
     private static FinancingOrder $pendingApprovalOrder;
 
@@ -49,15 +55,20 @@ class RejectOrderControllerTest extends TestCase
         [self::$secondCompany, self::$secondWallet] = $this->createCompany('3000', ['company_cr' => '12345678911']);
 
         self::$userLenderAdmin = $this->createLenderUser(self::$company->id, Role::LenderAdmin, 'lenderAdmin@bim.com');
+        self::$userLendersupervisor = $this->createLenderUser(self::$company->id, Role::LenderSupervisor, 'supervisor@bim.com');
+        self::$userLenderBilling = $this->createLenderUser(self::$company->id, Role::LenderBilling, 'userLenderBilling@bim.com');
+        self::$userLenderCreator = $this->createLenderUser(self::$company->id, Role::LenderOrderCreator, 'LenderOrderCreator@bim.com');
+
         self::$userLenderAdminWithoutEmailVerification = $this->createLenderUser(
             self::$company->id,
             Role::LenderAdmin,
             'userLenderAdminWithoutEmailVerification@bim.com',
             ['email_verified_at' => null]
         );
-        self::$customer = $this->createLenderUser(self::$company->id, Role::Customer, 'customer@bim.com');
+
         self::$financingOrder = $this->createOrder(self::$company->id, self::$userLenderAdmin->id, []);
         self::$pendingApprovalOrder = $this->createOrder(self::$company->id, self::$userLenderAdmin->id, ['status' => FinancingOrderStatus::PendingApproval]);
+        self::$rejectedOrder = $this->createOrder(self::$company->id, self::$userLenderAdmin->id, ['status' => FinancingOrderStatus::Rejected]);
         self::$statusReason = Str::random(80);
     }
 
@@ -80,8 +91,8 @@ class RejectOrderControllerTest extends TestCase
                 ['X-Company' => self::$company->id]
             )
             ->assertStatus(400)->assertJsonFragment([
-            'message' => __('error.order_cannot_be_approved_because_it_is_approved'),
-        ]);
+                'message' => __('error.order_cannot_be_approved_because_it_is_approved'),
+            ]);
     }
 
     public function test_other_company_can_not_reject_order()
@@ -105,14 +116,17 @@ class RejectOrderControllerTest extends TestCase
             ->assertStatus(404);
     }
 
-    public function test_customer_can_not_reject_order()
+    public function test_order_is_already_rejected()
     {
-        $this->actingAs(self::$customer)
+        $this->actingAs(self::$userLenderAdmin)
             ->putJson(
-                '/api/v1/lender/orders/'.self::$financingOrder->id.'/reject',
+                '/api/v1/lender/orders/'.self::$rejectedOrder->id.'/reject',
                 ['status_reason' => self::$statusReason],
                 ['X-Company' => self::$company->id]
-            )->assertStatus(403);
+            )
+            ->assertJsonFragment([
+                'message' => __('error.order_cannot_be_approved_because_it_is_approved'),
+            ]);
     }
 
     public function test_can_not_reject_order_without_email_verification()
@@ -122,9 +136,33 @@ class RejectOrderControllerTest extends TestCase
                 '/api/v1/lender/orders/'.self::$financingOrder->id.'/reject',
                 [],
                 ['X-Company' => self::$company->id]
-            )->assertStatus(403)->assertJsonFragment([
-            'message' => __('error.must_verify_email'),
-            'code' => ErrorCode::EMAIL_NOT_VERIFIED,
-        ]);
+            )
+            ->assertStatus(403)->assertJsonFragment([
+                'message' => __('error.must_verify_email'),
+                'code' => ErrorCode::EMAIL_NOT_VERIFIED,
+            ]);
+    }
+
+    public function test_lender_order_billing_can_not_reject_order()
+    {
+        $this->actingAs(self::$userLenderBilling);
+        // WIP billing has permissions
+        $this->putJson(
+            '/api/v1/lender/orders/'.self::$pendingApprovalOrder->id.'/reject',
+            ['status_reason' => self::$statusReason],
+            ['X-Company' => self::$company->id]
+        )
+            ->assertStatus(400);
+    }
+
+    public function test_lender_order_supervisor_can_reject_order()
+    {
+        $this->actingAs(self::$userLendersupervisor)
+            ->putJson(
+                '/api/v1/lender/orders/'.self::$pendingApprovalOrder->id.'/reject',
+                ['status_reason' => self::$statusReason],
+                ['X-Company' => self::$company->id]
+            )
+            ->assertStatus(200);
     }
 }
