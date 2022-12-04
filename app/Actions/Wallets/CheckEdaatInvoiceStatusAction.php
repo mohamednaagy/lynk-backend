0eb636469038e191a0722338e4b76c6a68a24b4d
Fady Ibrahim <fadysobhy111@gmail.com>
2022-12-04 10:45:12 +0200

    [Review][Admin] Added some changes

--- app/Actions/Wallets/CheckEdaatInvoiceStatusAction.php
+++ app/Actions/Wallets/CheckEdaatInvoiceStatusAction.php
@@ -21,14 +21,14 @@ class CheckEdaatInvoiceStatusAction implements CheckEdaatInvoiceStatus
     public function handle(EdaatInvoice $edaatInvoice): void
     {
         if (
-            $edaatInvoice->status !== EdaatInvoiceStatus::Paid() &&
+            $edaatInvoice->status === EdaatInvoiceStatus::Pending &&
             $this->edaatService->isPaidInvoice($edaatInvoice->invoice_number)
         ) {
             $edaatInvoice->update(['status' => EdaatInvoiceStatus::Paid]);
             $this->createTransactions->handle(
                 $edaatInvoice->company->getWallet(WalletType::CompanyWallet),
-                TransactionReason::DepositByEdaat,
                 $edaatInvoice->amount,
+                TransactionReason::DepositByEdaat,
                 [
                     'invoice_number' => $edaatInvoice->invoice_number,
                 ]
--- app/Http/Controllers/Api/V1/Lender/Wallets/CheckEdaatInvoiceStatus.php
+++ app/Http/Controllers/Api/V1/Lender/Wallets/CheckEdaatInvoiceStatus.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\Api\V1\Lender\Wallets;
 use App\Actions\Contracts\Wallets\CheckEdaatInvoiceStatus as CheckEdaatInvoiceStatusInterface;
 use App\Http\Controllers\Controller;
 use App\Models\EdaatInvoice;
-use Bavix\Wallet\Internal\Service\DatabaseServiceInterface;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class CheckEdaatInvoiceStatus extends Controller
 {
@@ -14,9 +14,7 @@ class CheckEdaatInvoiceStatus extends Controller
         CheckEdaatInvoiceStatusInterface $checkEdaatInvoiceStatus,
         int $invoice
     ): JsonResponse {
-        // According to https://bavix.github.io/laravel-wallet/#/transaction,
-        // DB::transaction() not working with Wallet version below 9.6, and we are using 9.5
-        app(DatabaseServiceInterface::class)->transaction(static function () use (
+        DB::multipleTransaction(function () use (
             $checkEdaatInvoiceStatus,
             $invoice
         ) {
