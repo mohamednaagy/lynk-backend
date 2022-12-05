75451d18460ceebe8a2ecacdf5da0d2bceed80ea
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2022-12-05 13:41:31 +0200

    WIP

--- tests/Feature/Endpoints/Api/V1/Lender/FinancingOrders/CreateOrderWithoutVerificationControllerTest.php
+++ tests/Feature/Endpoints/Api/V1/Lender/FinancingOrders/CreateOrderWithoutVerificationControllerTest.php
@@ -29,7 +29,17 @@ class CreateOrderWithoutVerificationControllerTest extends TestCase
 
     private static User $userLenderAdmin;
 
+    private static User $LenderApiUser;
+
+    private static User $userLenderSupervisor;
+
     private static User $userLenderAdminBelongsToCompanyHasEmptyWallet;
+
+    private static User $userLenderAdminWithoutEmailVerification;
+
+    private static User $userLenderOrderCreator;
+
+    private static User $userLenderBilling;
 
     private static array $orderDetails;
 
@@ -46,8 +56,20 @@ class CreateOrderWithoutVerificationControllerTest extends TestCase
         [self::$company, self::$wallet] = $this->createCompany('2000');
         [self::$companyWithEmptyWallet, self::$emptyWallet] = $this->createCompany('0', ['company_cr' => '12345678911']);
 
-        self::$userLenderAdmin = $this->createLenderUser(self::$company->id, Role::LenderAdmin, 'lenderAdmin@bim.com');
         self::$userLenderAdminBelongsToCompanyHasEmptyWallet = $this->createLenderUser(self::$companyWithEmptyWallet->id, Role::LenderAdmin, 'lenderAdmin2@bim.com');
+        self::$userLenderAdmin = $this->createLenderUser(self::$company->id, Role::LenderAdmin, 'LenderAdmin@bim.com');
+        self::$LenderApiUser = $this->createLenderUser(self::$company->id, Role::LenderApiUser, 'LenderApiUser@bim.com');
+        self::$userLenderOrderCreator = $this->createLenderUser(self::$company->id, Role::LenderOrderCreator, 'LenderOrderCreator@bim.com');
+        self::$userLenderSupervisor = $this->createLenderUser(self::$company->id, Role::LenderSupervisor, 'LenderSupervisor@bim.com');
+        self::$userLenderBilling = $this->createLenderUser(self::$company->id, Role::LenderBilling, 'LenderBilling@bim.com');
+        self::$userLenderAdminWithoutEmailVerification = $this->createLenderUser(
+            self::$company->id,
+            Role::LenderAdmin,
+            'userLenderAdminWithoutEmailVerification@bim.com',
+            [
+                'email_verified_at' => null,
+            ]
+        );
 
         self::$orderDetails = [
             'national_id' => '2553451234',
@@ -68,15 +90,13 @@ class CreateOrderWithoutVerificationControllerTest extends TestCase
 
     public function test_create_order_without_verification_does_not_work_if_wallet_is_empty()
     {
-        $this->actingAs(self::$userLenderAdminBelongsToCompanyHasEmptyWallet);
-
-        $response = $this->postJson(
-            '/api/v1/lender/orders/no-verification',
-            self::$orderDetails,
-            ['X-Company' => self::$companyWithEmptyWallet->id]
-        );
-
-        $response->assertStatus(Response::HTTP_BAD_REQUEST)->assertJsonFragment([
+        $this->actingAs(self::$userLenderAdminBelongsToCompanyHasEmptyWallet)
+            ->postJson(
+                '/api/v1/lender/orders/no-verification',
+                self::$orderDetails,
+                ['X-Company' => self::$companyWithEmptyWallet->id]
+            )
+            ->assertStatus(Response::HTTP_BAD_REQUEST)->assertJsonFragment([
             'message' => trans('error.no_enough_balance'),
             'code' => ErrorCode::BALANCE_NOT_ENOUGH,
         ]);
@@ -84,43 +104,82 @@ class CreateOrderWithoutVerificationControllerTest extends TestCase
 
     public function test_create_order_without_verification_does_not_work_when_national_id_is_not_valid()
     {
-        $this->actingAs(self::$userLenderAdmin);
-        $response = $this->postJson(
-            '/api/v1/lender/orders/no-verification',
-            self::$orderDetailsWithInvalidNationalID,
-            ['X-Company' => self::$company->id]
-        );
-
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->actingAs(self::$userLenderAdmin)
+            ->postJson(
+                '/api/v1/lender/orders/no-verification',
+                self::$orderDetailsWithInvalidNationalID,
+                ['X-Company' => self::$company->id]
+            )
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 
     public function test_create_order_without_verification_does_not_work_when_phone_number_is_not_valid()
     {
-        $this->actingAs(self::$userLenderAdmin);
-        $response = $this->postJson(
-            '/api/v1/lender/orders/no-verification',
-            array_merge(self::$orderDetails, ['phone_number' => '01010420399']),
-            ['X-Company' => self::$company->id]
-        );
-
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->actingAs(self::$userLenderAdmin)
+            ->postJson(
+                '/api/v1/lender/orders/no-verification',
+                array_merge(self::$orderDetails, ['phone_number' => '01010420399']),
+                ['X-Company' => self::$company->id]
+            )
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 
     public function test_create_order_without_verification_does_not_work_if_amount_is_greater_than_selling_price()
     {
         $amount = 10;
         $sellingPrice = 9;
-        $this->actingAs(self::$userLenderAdmin);
-        $response = $this->postJson(
-            '/api/v1/lender/orders/no-verification',
-            array_merge(self::$orderDetails, ['amount' => $amount, 'selling_price' => $sellingPrice]),
-            ['X-Company' => self::$company->id]
-        );
 
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->actingAs(self::$userLenderAdmin)
+            ->postJson(
+                '/api/v1/lender/orders/no-verification',
+                array_merge(self::$orderDetails, ['amount' => $amount, 'selling_price' => $sellingPrice]),
+                ['X-Company' => self::$company->id]
+            )
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 
-    public function test_create_order_without_verification_works_successfully()
+    public function test_create_order_without_verification_assert_balance_after_creating_order()
+    {
+        $wallet = self::$company->balance(WalletType::CompanyWallet);
+
+        $this->actingAs(self::$userLenderAdmin)
+            ->postJson(
+                '/api/v1/lender/orders/no-verification',
+                self::$orderDetails,
+                ['X-Company' => self::$company->id]
+            );
+
+        $walletAfterCreation = self::$company->balance(WalletType::CompanyWallet);
+        $orderCost = self::$company->order_cost->getMoney();
+        $this->assertTrue($wallet->subtract($orderCost)->equals($walletAfterCreation));
+    }
+
+    public function test_create_order_without_verification_lender_user_can_not_ceate_order_without_email_verification()
+    {
+        $this->actingAs(self::$userLenderAdminWithoutEmailVerification)
+            ->postJson(
+                '/api/v1/lender/orders/no-verification',
+                self::$orderDetails,
+                ['X-Company' => self::$company->id]
+            )
+            ->assertStatus(403)->assertJsonFragment([
+                'message' => __('error.must_verify_email'),
+                'code' => ErrorCode::EMAIL_NOT_VERIFIED,
+            ]);
+    }
+
+    public function test_create_order_without_verification_lender_api_user_can_create_order()
+    {
+        $this->actingAs(self::$LenderApiUser)
+            ->postJson(
+                '/api/v1/lender/orders/no-verification',
+                self::$orderDetails,
+                ['X-Company' => self::$company->id]
+            )
+            ->assertStatus(200);
+    }
+
+    public function test_create_order_without_verification_lender_admin_can_create_order()
     {
         $this->actingAs(self::$userLenderAdmin)
             ->postJson(
@@ -128,25 +187,39 @@ class CreateOrderWithoutVerificationControllerTest extends TestCase
                 self::$orderDetails,
                 ['X-Company' => self::$company->id]
             )
-            ->assertStatus(Response::HTTP_OK);
+            ->assertStatus(200);
     }
 
-    public function test_create_order_without_verification_assert_balance_after_creating_order()
+    public function test_create_order_without_verification_order_creator_user_can_create_order()
     {
-        $wallet = self::$company->balance(WalletType::CompanyWallet);
-        $balanceBeforeCreation = $wallet->getAmount();
+        $this->actingAs(self::$userLenderOrderCreator)
+            ->postJson(
+                '/api/v1/lender/orders/no-verification',
+                self::$orderDetails,
+                ['X-Company' => self::$company->id]
+            )
+            ->assertStatus(200);
+    }
 
-        $this->actingAs(self::$userLenderAdmin);
-        $this->postJson(
-            '/api/v1/lender/orders/no-verification',
-            self::$orderDetails,
-            ['X-Company' => self::$company->id]
-        );
+    public function test_create_order_without_verification_supervisor_user_can_create_order()
+    {
+        $this->actingAs(self::$userLenderSupervisor)
+            ->postJson(
+                '/api/v1/lender/orders/no-verification',
+                self::$orderDetails,
+                ['X-Company' => self::$company->id]
+            )
+            ->assertStatus(200);
+    }
 
-        $wallet = self::$company->balance(WalletType::CompanyWallet);
-        $balanceAfterCreation = $wallet->getAmount();
-        $orderCost = self::$company->order_cost->getMoney()->getAmount();
-
-        $this->assertTrue(((int) $balanceBeforeCreation - (int) $orderCost) == $balanceAfterCreation);
+    public function test_create_order_without_verification_billing_user_can_not_create_order()
+    {
+        $this->actingAs(self::$userLenderBilling)
+            ->postJson(
+                '/api/v1/lender/orders/no-verification',
+                self::$orderDetails,
+                ['X-Company' => self::$company->id]
+            )
+            ->assertStatus(403);
     }
 }
