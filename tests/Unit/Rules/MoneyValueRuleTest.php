0f719dc957ea012fc4e22f026da4d7d5f43a1792
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2022-12-05 13:51:40 +0200

    style

--- tests/Unit/Rules/MoneyValueRuleTest.php
+++ tests/Unit/Rules/MoneyValueRuleTest.php
@@ -24,64 +24,64 @@ class MoneyValueRuleTest extends TestCase
      *
      * @return void
      */
-    public function test_passes_return_bool()
+    public function test_money_value_rule_passes_method_return_bool()
     {
         $this->assertIsBool($this->rule->passes($this->attribute, 0));
     }
 
-    public function test_passes_return_false_when_value_is_zero()
+    public function test_money_value_rule_passes_method_return_false_when_value_is_zero()
     {
         $this->assertFalse($this->rule->passes($this->attribute, 0));
     }
 
-    public function test_passes_return_false_when_value_is_negative()
+    public function test_money_value_rule_passes_method_return_false_when_value_is_negative()
     {
         $this->assertFalse($this->rule->passes($this->attribute, -50));
     }
 
-    public function test_passes_return_true_when_value_is_postive()
+    public function test_money_value_rule_passes_method_return_true_when_value_is_postive()
     {
         $this->assertTrue($this->rule->passes($this->attribute, 1));
     }
 
-    public function test_passes_return_true_when_value_is_onley_decimal()
+    public function test_money_value_rule_passes_method_return_true_when_value_is_between_zero_and_one_without_zero_before_decimal_point()
     {
         $this->assertTrue($this->rule->passes($this->attribute, .20));
     }
 
-    public function test_passes_return_true_when_value_is_zero_with_decimal()
+    public function test_money_value_rule_passes_method_return_true_when_value_is_between_zero_and_one_with_zero_before_decimal_point()
     {
         $this->assertTrue($this->rule->passes($this->attribute, 0.20));
     }
 
-    public function test_passes_return_true_when_value_is_big_number_postive()
+    public function test_money_value_rule_passes_method_return_true_when_value_is_big_number_postive()
     {
         $this->assertTrue($this->rule->passes($this->attribute, 9999999));
     }
 
-    public function test_passes_return_true_when_value_is_decimal()
+    public function test_money_value_rule_passes_method_return_true_when_value_is_decimal()
     {
         $this->assertTrue($this->rule->passes($this->attribute, 101.50));
     }
 
-    public function test_passes_return_true_when_value_is_big_number_with_decimal()
+    public function test_money_value_rule_passes_method_return_true_when_value_is_big_number_with_decimal()
     {
         $this->assertTrue($this->rule->passes($this->attribute, 9999999.50));
     }
 
-    public function test_passes_return_false_when_more_than_two_decimal()
+    public function test_money_value_rule_passes_method_return_false_if_number_of_decimal_places_is_more_than_two()
     {
         $this->assertFalse($this->rule->passes($this->attribute, 101.501));
     }
 
-    public function test_decimal_could_customize()
+    public function test_money_value_rule_can_have_custom_number_of_decimal_places()
     {
         $rule = new MoneyValueRule(4);
 
         $this->assertTrue($rule->passes('amount', 999.9999));
     }
 
-    public function test_english_message()
+    public function test_money_value_rule_english_validatation_message()
     {
         $this->app->setLocale('en');
 
@@ -90,7 +90,7 @@ class MoneyValueRuleTest extends TestCase
         $this->assertSame($this->rule->message(), 'The :attribute format is invalid.');
     }
 
-    public function test_arabic_message()
+    public function test_money_value_rule_arabic_validation_message()
     {
         $this->app->setLocale('ar');
 
