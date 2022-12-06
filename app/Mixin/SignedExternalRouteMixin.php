257f95d1735417a2a81c64eaed7b6e369507aa35
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2022-12-06 15:45:33 +0200

    style

--- app/Mixin/SignedExternalRouteMixin.php
+++ app/Mixin/SignedExternalRouteMixin.php
@@ -17,6 +17,7 @@ class SignedExternalRouteMixin
             if (! filter_var($externalUrl, FILTER_VALIDATE_URL)) {
                 throw new InvalidArgumentException('Provided url not correct');
             }
+
             $externalUrl = preg_replace_callback('/:([a-zA-Z_]{1,})/', function ($matches) use ($parameters) {
                 return isset($parameters[$matches[1]]) && $parameters[$matches[1]] !== '' ? $parameters[$matches[1]] : $matches[0];
             }, $externalUrl);
