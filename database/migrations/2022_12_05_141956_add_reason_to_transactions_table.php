27a924c4e10abc5128d972e069870c8332438405
Ahmed Medhat <ahmed.medhat12@outlook.com>
2022-12-06 16:37:25 +0300

    changing after

--- database/migrations/2022_12_05_141956_add_reason_to_transactions_table.php
+++ database/migrations/2022_12_05_141956_add_reason_to_transactions_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('transactions', function (Blueprint $table) {
-            $table->unsignedTinyInteger('reason')->after('meta')->nullable();
+            $table->unsignedTinyInteger('reason')->after('reference_number')->nullable();
         });
     }
 
