b729802b97b1106686a087de8f6990b738abe063
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2022-12-08 10:59:32 +0200

    Test trait interacts with admin

---
+++ tests/Traits/InteractsWithAdmin.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Traits;
+
+use App\Enums\Role;
+use App\Models\User;
+use Modules\Grantify\Facades\Grantify;
+
+trait InteractsWithAdmin
+{
+    /**
+     * @param  int  $companyId
+     * @param  string  $role
+     * @param  string  $email
+     * @param  array  $data
+     * @return Collection|Model|mixed
+     */
+    public function createAdmin(
+        string $role = Role::Admin,
+        string $email = 'admin@bim.com',
+        array $data = []
+    ): mixed {
+        $admin = User::factory()->create(array_merge([
+            'email' => $email,
+            'password' => bcrypt('12345678'),
+        ], $data));
+
+        Grantify::assignRoleToModel($admin, $role);
+
+        return $admin;
+    }
+
+    /**
+     * Summary of createManager
+     *
+     * @param  string  $email
+     * @param  array  $data
+     * @param  string|array  $permissions
+     * @return mixed
+     */
+    public function createManager(
+        string $email = 'Manager@bim.com',
+        array $data = [],
+        string|array $permissions = []
+    ): mixed {
+        $manager = User::factory()->create(array_merge([
+            'email' => $email,
+            'password' => bcrypt('12345678'),
+        ], $data));
+
+        Grantify::assignRoleToModel($manager, Role::Manager);
+
+        if (! empty($permissions)) {
+            Grantify::assignPermissionToModel($manager, $permissions);
+        }
+
+        return $manager;
+    }
+}
