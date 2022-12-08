139873c9f7d5c1c551863c6585828f9f1ef5d6c3
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2022-12-08 18:16:13 +0200

    WIP

--- tests/Feature/Endpoints/Api/V1/Lender/Users/ResendInvitationTest.php
+++ tests/Feature/Endpoints/Api/V1/Lender/Users/ResendInvitationTest.php
@@ -22,7 +22,7 @@ class ResendInvitationTest extends TestCase
 
     private static Company $companyNotActive;
 
-    private static User $lender;
+    private static User $lenderAdminNotJoined;
 
     private static User $lenderBelongsToCompanyNotActive;
 
@@ -47,18 +47,21 @@ class ResendInvitationTest extends TestCase
         parent::setUp();
 
         self::$redirectUrl = 'http://localhost';
-        self::$company = $this->createCompany('2000')[0];
-        self::$companyNotActive = $this->createCompany('2000', [
-            'status' => CompanyStatus::Pending,
-            'company_cr' => '12345678999',
-        ])[0];
+        [self::$company] = $this->createCompany('2000');
+        [self::$companyNotActive] = $this->createCompany(
+            '2000',
+            [
+                'status' => CompanyStatus::Pending,
+                'company_cr' => '12345678999',
+            ]
+        );
 
         self::$LenderSupervisor = $this->createLenderUser(self::$company->id, Role::LenderSupervisor, 'LenderSupervisor@bim.com');
         self::$LenderBilling = $this->createLenderUser(self::$company->id, Role::LenderBilling, 'LenderBilling@bim.com');
         self::$lenerAdmin = $this->createLenderUser(self::$company->id, Role::LenderAdmin, 'LenderAdmin@bim.com');
         self::$lenderApiUser = $this->createLenderUser(self::$company->id, Role::LenderApiUser, 'LenderAdmin1@bim.com');
         self::$lenderCrearor = $this->createLenderUser(self::$company->id, Role::LenderOrderCreator, 'LenderAdmin1@bim.com');
-        self::$lender = $this->createLenderUser(self::$company->id, Role::LenderAdmin, 'LenderAdmin1@bim.com', ['password' => null]);
+        self::$lenderAdminNotJoined = $this->createLenderUser(self::$company->id, Role::LenderAdmin, 'LenderAdmin1@bim.com', ['password' => null]);
         self::$lenerAdminBelonsToCompanyNotActive = $this->createLenderUser(self::$companyNotActive->id, Role::LenderAdmin, 'LenderAdmin2@bim.com');
 
         self::$lenerAdminNotVerified = $this->createLenderUser(
@@ -87,7 +90,7 @@ class ResendInvitationTest extends TestCase
             )
             ->assertStatus(403)->assertJsonFragment([
                 'message' => __('error.company_not_active'),
-                'code' => ErrorCode::COMPANY_NOT_ACTIVE,
+                'code' => 1015,
             ]);
     }
 
@@ -96,7 +99,7 @@ class ResendInvitationTest extends TestCase
         $this->withHeader('X-Company', self::$company->id)
             ->actingAs(self::$lenerAdminNotVerified)
             ->postJson(
-                'api/v1/lender/users/'.self::$lender->id.'/resend-invitation',
+                'api/v1/lender/users/'.self::$lenderAdminNotJoined->id.'/resend-invitation',
                 [
                     'redirect_url' => self::$redirectUrl,
                 ]
@@ -113,12 +116,12 @@ class ResendInvitationTest extends TestCase
         $this->withHeader('X-Company', self::$company->id)
             ->actingAs(self::$LenderSupervisor)
             ->postJson(
-                'api/v1/lender/users/'.self::$lender->id.'/resend-invitation',
+                'api/v1/lender/users/'.self::$lenderAdminNotJoined->id.'/resend-invitation',
                 [
                     'redirect_url' => self::$redirectUrl,
                 ]
             )
-            ->assertStatus(200);
+            ->assertStatus(403);
     }
 
     public function test_resend_invitation_lender_admin_can_access()
@@ -126,7 +129,7 @@ class ResendInvitationTest extends TestCase
         $this->withHeader('X-Company', self::$company->id)
             ->actingAs(self::$lenerAdmin)
             ->postJson(
-                'api/v1/lender/users/'.self::$lender->id.'/resend-invitation',
+                'api/v1/lender/users/'.self::$lenderAdminNotJoined->id.'/resend-invitation',
                 [
                     'redirect_url' => self::$redirectUrl,
                 ]
@@ -139,12 +142,12 @@ class ResendInvitationTest extends TestCase
         $this->withHeader('X-Company', self::$company->id)
             ->actingAs(self::$LenderBilling)
             ->postJson(
-                'api/v1/lender/users/'.self::$lender->id.'/resend-invitation',
+                'api/v1/lender/users/'.self::$lenderAdminNotJoined->id.'/resend-invitation',
                 [
                     'redirect_url' => self::$redirectUrl,
                 ]
             )
-            ->assertStatus(200);
+            ->assertStatus(403);
     }
 
     public function test_resend_invitation_lender_api_user_can_access()
@@ -152,12 +155,12 @@ class ResendInvitationTest extends TestCase
         $this->withHeader('X-Company', self::$company->id)
             ->actingAs(self::$lenderApiUser)
             ->postJson(
-                'api/v1/lender/users/'.self::$lender->id.'/resend-invitation',
+                'api/v1/lender/users/'.self::$lenderAdminNotJoined->id.'/resend-invitation',
                 [
                     'redirect_url' => self::$redirectUrl,
                 ]
             )
-            ->assertStatus(200);
+            ->assertStatus(403);
     }
 
     public function test_resend_invitation_lender_order_creator_can_access()
@@ -165,33 +168,33 @@ class ResendInvitationTest extends TestCase
         $this->withHeader('X-Company', self::$company->id)
             ->actingAs(self::$lenderCrearor)
             ->postJson(
-                'api/v1/lender/users/'.self::$lender->id.'/resend-invitation',
+                'api/v1/lender/users/'.self::$lenderAdminNotJoined->id.'/resend-invitation',
                 [
                     'redirect_url' => self::$redirectUrl,
                 ]
             )
-            ->assertStatus(200);
+            ->assertStatus(403);
     }
 
     public function test_resend_invitation_email_is_sent()
     {
+        Mail::fake();
         $this->withHeader('X-Company', self::$company->id)
             ->actingAs(self::$lenerAdmin)
             ->postJson(
-                'api/v1/lender/users/'.self::$lender->id.'/resend-invitation',
+                'api/v1/lender/users/'.self::$lenderAdminNotJoined->id.'/resend-invitation',
                 [
                     'redirect_url' => self::$redirectUrl,
                 ]
             )
             ->assertStatus(200);
 
-        Mail::fake();
-        Mail::send(new CompleteRegisterInvitation(self::$lender, self::$redirectUrl));
         Mail::assertSent(CompleteRegisterInvitation::class, function ($mail) {
-            $this->assertInstanceOf(User::class, $mail->user);
-            $this->assertTrue($mail->user->id == self::$lender->id);
+            return $mail->user instanceof User;
+        });
 
-            return true;
+        Mail::assertSent(CompleteRegisterInvitation::class, function ($mail) {
+            return $mail->user->id == self::$lenderAdminNotJoined->id;
         });
     }
 }
