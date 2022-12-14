352d9e9113e38ab26945b43bca831efd45b33f69
Ahmed Bally <ahmedbaly87@gmail.com>
2022-12-14 06:19:30 +0200

    test has wallet trait

---
+++ tests/Unit/Wallets/HasWalletTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Tests\Unit\Wallets;
+
+use App\Actions\Contracts\Wallets\CreateTransactions;
+use App\Enums\TransactionReason;
+use App\Models\Company;
+use App\Models\Transaction;
+use App\Support\Money\Money;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Tests\Traits\InteractsWithLender;
+
+class HasWalletTest extends TestCase
+{
+    use RefreshDatabase, InteractsWithLender;
+
+    private static Company $company;
+
+    /**
+     * @throws BindingResolutionException
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        self::$company = Company::factory()->create(array_merge([
+            'first_name' => 'firstName',
+            'last_name' => 'lastName',
+            'phone_country_code' => 'SA',
+            'phone_number' => '503811000',
+            'email' => 'test@uselynk.test',
+            'password' => 'Qwer@1234',
+            'source' => 'Postman',
+            'company_name' => 'companyName',
+            'company_unique_name' => 'lynk05',
+            'company_cr' => '12345678910',
+        ]));
+    }
+
+    public function test_has_wallet_get_wallet()
+    {
+        $wallet = self::$company->getWallet('test-wallet');
+        $this->assertNull($wallet);
+
+        self::$company->createWallet('test-wallet', Money::getDefaultCurrency());
+        $this->assertNotNull(self::$company->getWallet('test-wallet'));
+    }
+
+    public function test_has_wallet_get_wallet_or_fail()
+    {
+        $this->expectException(ModelNotFoundException::class);
+        self::$company->getWalletOrFail('test-wallet');
+
+        self::$company->createWallet('test-wallet', Money::getDefaultCurrency());
+        $this->assertNotNull(self::$company->getWalletOrFail('test-wallet'));
+    }
+
+    public function test_has_wallet_get_wallets()
+    {
+        $wallets = self::$company->getWallets();
+        $this->assertCount(0, $wallets);
+
+        self::$company->createWallet('test-wallet', Money::getDefaultCurrency());
+        $wallets = self::$company->getWallets('test-wallet');
+        $this->assertCount(1, $wallets);
+
+        self::$company->createWallet('test-wallet1', Money::getDefaultCurrency());
+        $wallets = self::$company->getWallets();
+        $this->assertCount(2, $wallets);
+    }
+
+    public function test_has_wallet_create_wallet_and_has_wallet()
+    {
+        self::$company->createWallet('test-wallet', Money::getDefaultCurrency());
+        $this->assertTrue(self::$company->hasWallet('test-wallet'));
+    }
+
+    public function test_has_wallet_transactions()
+    {
+        $createTransaction = app(CreateTransactions::class);
+        $wallet = self::$company->createWallet('test-wallet', Money::getDefaultCurrency());
+
+        $this->assertEquals(0, self::$company->transactions('test-wallet')->count());
+
+        $createTransaction->handle($wallet, TransactionReason::DepositByEdaat, new Money(1500), []);
+
+        $transaction = self::$company->transactions('test-wallet');
+        $this->assertEquals(1, $transaction->count());
+        $this->assertInstanceOf(Transaction::class, $transaction->first());
+    }
+
+    public function test_has_wallet_balance()
+    {
+        $wallet = self::$company->createWallet('test-wallet', Money::getDefaultCurrency());
+        $this->assertTrue(self::$company->balance('test-wallet')->isZero());
+
+        $wallet->deposit(new Money(1500), 1, []);
+        $this->assertTrue(self::$company->balance('test-wallet')->equals(new Money(1500)));
+    }
+}
