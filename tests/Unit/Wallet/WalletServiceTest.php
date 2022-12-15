b0f5e68d57db0e7ce6c9c370ea16400b6558af4b
Ahmed Hamdi <ahmed.hamdi181@gmail.com>
2022-12-15 17:50:26 +0200

    Apply changes

--- tests/Unit/Wallet/WalletServiceTest.php
+++ tests/Unit/Wallet/WalletServiceTest.php
@@ -16,12 +16,11 @@ use Tests\Traits\InteractsWithLender;
 
 class WalletServiceTest extends TestCase
 {
-    use RefreshDatabase;
-    use InteractsWithLender;
+    use RefreshDatabase, InteractsWithLender;
 
     private static WalletService $walletService;
 
-    private static Company $model;
+    private static Company $company;
 
     private static wallet $wallet;
 
@@ -36,12 +35,12 @@ class WalletServiceTest extends TestCase
             'currency' => 'SAR',
         ];
         self::$walletService = new WalletService();
-        [self::$model, self::$wallet] = $this->createCompany(2000);
+        [self::$company, self::$wallet] = $this->createCompany(2000);
     }
 
     public function test_wallet_service_create_method_return_wallet_model()
     {
-        $wallet = self::$walletService->create(self::$model, self::$walletInformation);
+        $wallet = self::$walletService->create(self::$company, self::$walletInformation);
         $this->assertInstanceOf(Wallet::class, $wallet);
         $this->assertSame($wallet->name, self::$walletInformation['name']);
     }
@@ -62,7 +61,7 @@ class WalletServiceTest extends TestCase
 
     public function test_wallet_service_find_by_name_method_return_wallet_model()
     {
-        $wallet = self::$walletService->findByName(self::$model, self::$wallet->name);
+        $wallet = self::$walletService->findByName(self::$company, self::$wallet->name);
         $this->assertInstanceOf(Wallet::class, $wallet);
         $this->assertSame($wallet->name, self::$wallet->name);
     }
@@ -83,7 +82,7 @@ class WalletServiceTest extends TestCase
 
     public function test_wallet_service_find_by_name_or_fail_method_return_wallet_model()
     {
-        $wallet = self::$walletService->findByNameOrFail(self::$model, self::$wallet->name);
+        $wallet = self::$walletService->findByNameOrFail(self::$company, self::$wallet->name);
         $this->assertInstanceOf(Wallet::class, $wallet);
         $this->assertSame($wallet->name, self::$wallet->name);
     }
@@ -91,25 +90,25 @@ class WalletServiceTest extends TestCase
     public function test_wallet_service_find_by_id_method_return_null_when_model_not_found()
     {
         $wallet = self::$walletService->findById(404);
-        $this->assertEmpty($wallet);
+        $this->assertNull($wallet);
     }
 
     public function test_wallet_service_find_by_uuid_method_return_null_when_model_not_found()
     {
         $wallet = self::$walletService->findByUuid(404);
-        $this->assertEmpty($wallet);
+        $this->assertNull($wallet);
     }
 
     public function test_wallet_service_find_by_name_method_return_null_when_model_not_found()
     {
-        $wallet = self::$walletService->findByName(self::$model, 'not exists name');
-        $this->assertEmpty($wallet);
+        $wallet = self::$walletService->findByName(self::$company, 'not exists name');
+        $this->assertNull($wallet);
     }
 
     public function test_wallet_service_find_by_name_or_fail_method_throw_exception_if_model_not_found()
     {
         $this->expectException(ModelNotFoundException::class);
-        self::$walletService->findByNameOrFail(self::$model, 'not exiosts name');
+        self::$walletService->findByNameOrFail(self::$company, 'not exists name');
     }
 
     public function test_wallet_service_find_by_uuid_or_fail_method_throw_exception_if_model_not_found()
@@ -126,24 +125,24 @@ class WalletServiceTest extends TestCase
 
     public function test_wallet_service_get_wallets_method_return_collection()
     {
-        $this->assertInstanceOf(Collection::class, self::$walletService->getWallets(self::$model));
+        $this->assertInstanceOf(Collection::class, self::$walletService->getWallets(self::$company));
     }
 
     public function test_wallet_service_get_wallets_method_return_his_wallets_collection()
     {
-        $this->assertEquals(1, self::$walletService->getWallets(self::$model)->count());
+        $this->assertEquals(1, self::$walletService->getWallets(self::$company)->count());
     }
 
     public function test_wallet_service_has_wallet_method_return_bool()
     {
-        $transactions = self::$walletService->hasWallet(self::$model, WalletType::CompanyWallet);
+        $transactions = self::$walletService->hasWallet(self::$company, WalletType::CompanyWallet);
 
         $this->assertIsBool($transactions);
     }
 
     public function test_wallet_service_transactions_method_return_query_builder()
     {
-        $transactions = self::$walletService->transactions(self::$model, WalletType::CompanyWallet);
+        $transactions = self::$walletService->transactions(self::$company, WalletType::CompanyWallet);
 
         $this->assertInstanceOf(Builder::class, $transactions);
     }
@@ -154,19 +153,19 @@ class WalletServiceTest extends TestCase
 
         $this->assertEquals(
             3,
-            self::$walletService->transactions(self::$model, self::$wallet->name)->count()
+            self::$walletService->transactions(self::$company, self::$wallet->name)->count()
         );
     }
 
     public function test_wallet_service_balance_method_return_wallet_balance()
     {
-        $balance = self::$walletService->balance(self::$model, WalletType::CompanyWallet);
+        $balance = self::$walletService->balance(self::$company, WalletType::CompanyWallet);
         $this->assertEquals(2000, $balance->getAmount());
     }
 
     public function test_wallet_service_balance_method_throw_exception_if_wallet_not_found()
     {
         $this->expectException(ModelNotFoundException::class);
-        self::$walletService->balance(self::$model, 'not exists wallet');
+        self::$walletService->balance(self::$company, 'not exists wallet');
     }
 }
