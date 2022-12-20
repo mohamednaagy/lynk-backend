9fb1c5cf645d2439e1d70cbff956f37285f62272
youssofOkiel <y.okiel@bimsaudi.com>
2022-12-20 12:58:52 +0200

    {updated} refactor notification structure

--- app/Http/Controllers/Api/V1/Lender/Orders/OrderController.php
+++ app/Http/Controllers/Api/V1/Lender/Orders/OrderController.php
@@ -16,16 +16,14 @@ use App\Enums\Subject;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Lender\Orders\StoreOrderRequest;
 use App\Http\Requests\V1\Lender\Orders\UpdateOrderRequest;
-use App\Mail\OrderCreated;
+use App\Jobs\FinancingOrders\NotifyAdminsAboutOrderCreated;
 use App\Models\FinancingOrder;
-use App\Models\User;
 use App\Transformers\FinancingOrderTransformer;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
 
 class OrderController extends Controller
 {
@@ -53,6 +51,7 @@ class OrderController extends Controller
     }
 
     /**
+     * @param  Request  $request
      * @param  GetPaginatedFinancingOrder  $getPaginatedOrders
      * @return JsonResponse
      */
@@ -154,10 +153,8 @@ class OrderController extends Controller
                 // deduct the cost from the wallet
                 $deductOrderCreationFee->handle($financingOrder);
 
-                $notification = 'notification model';
-                $admins = User::role([Role::LenderAdmin, Role::LenderSupervisor])->get();
-
-                Mail::cc($admins)->queue(new OrderCreated($notification));
+                $user = auth()->user();
+                dispatch(new NotifyAdminsAboutOrderCreated($financingOrder, $user));
 
                 return fractal($financingOrder, new FinancingOrderTransformer())
                     ->parseIncludes([
---
+++ lang/en/emails/order-created.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'subject' => 'Order #:order_id Requires Approval',
+    'body' => 'Order #:order_id is awaiting your approval',
+    'action' => 'View Order',
+];
