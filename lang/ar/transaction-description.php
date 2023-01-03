958ebe35fe16f5afdbc645e7155481d3f776f2af
Ahmed Hamdi <ahmed.hamdi181@gmail.com>
2023-01-03 11:03:36 +0200

    [LYNKB-321][Transactions] Edaat invoice description not correct in arabic

--- lang/ar/transaction-description.php
+++ lang/ar/transaction-description.php
@@ -2,7 +2,7 @@
 
 return [
     'order_creation_fee' => ':order_number رسوم إنشاء طلب',
-    'deposit_by_edaat' => ':invoice_number شحن رصيد بواسطة سداد لفاتورة رقم',
+    'deposit_by_edaat' => 'invoice_number: شحن رصيد بواسطة سداد لفاتورة رقم',
     'vat_percentage' => 'ضريبة القيمة المضافة (vat_percentage:%) للطلب رقم order_id:',
     'manual_deposit' => 'شحن رصيد',
 ];
