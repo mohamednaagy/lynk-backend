dba294f0613171ed0e83d489f0657518a5fe3711
Ahmed Bally <ahmedbaly87@gmail.com>
2023-01-10 09:30:10 +0200

    testing

---
+++ tests/Feature/Endpoints/Api/V1/Trader/Auth/GetAuthUserTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Endpoints\Api\V1\Trader\Auth;
+
+use App\Enums\Area;
+use App\Enums\CompanyType;
+use App\Enums\Role;
+use App\Models\Company;
+use App\Models\User;
+use App\Models\Wallet;
+use App\Transformers\UserTransformer;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+use Tests\Traits\InteractsWithLender;
+
+class GetAuthUserTest extends TestCase
+{
+    use RefreshDatabase, InteractsWithLender;
+
+    private static Company $company;
+
+    private static Wallet $wallet;
+
+    private static User $userTrader;
+
+    /**
+     * @return void
+     *
+     * @throws BindingResolutionException
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        [self::$company, self::$wallet] = $this->createCompany('2000', ['company_cr' => '12345678910', 'type' => CompanyType::Trader]);
+        self::$userTrader = $this->createLenderUser(self::$company->id, Role::TraderAdmin, 'traderAdmin@bim.com');
+    }
+
+    /**
+     * @return void
+     */
+    public function test_that_un_auth_user_cant_fetch_his_details(): void
+    {
+        $this->withHeader('X-Company', self::$company->id)
+            ->getJson('api/v1/trader/auth')
+            ->assertStatus(Response::HTTP_UNAUTHORIZED)
+            ->assertExactJson([
+                'message' => 'Unauthenticated.',
+            ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_that_trader_can_fetch_his_details(): void
+    {
+        $data = $this->actingAs(self::$userTrader)
+            ->withHeader('X-Company', self::$company->id)
+            ->getJson('api/v1/trader/auth')
+            ->assertStatus(Response::HTTP_OK)
+            ->assertExactJson(
+                fractal(self::$userTrader->load(['roles']), new UserTransformer(Area::Trader))
+                    ->parseIncludes([
+                        'id',
+                        'first_name',
+                        'last_name',
+                        'email',
+                        'is_email_verified',
+                        'role',
+                        'company.id',
+                        'company.name',
+                        'company.public_status_comment',
+                        'company.status',
+                        'permissions',
+                        'locale',
+                        'phone_number',
+                        'phone_country_code',
+                        'formatted_phone_number',
+                    ])->respond()->getData(true)
+            );
+    }
+}
