d19a8c32319ae386ec30ef25915a071bdd983400
Ahmed Hamdi <ahmed.hamdi181@gmail.com>
2023-01-15 10:00:07 +0200

    Update usage of attach doc

--- app/Jobs/Dmcc/ProcessDmccRespondedToPtpOrder.php
+++ app/Jobs/Dmcc/ProcessDmccRespondedToPtpOrder.php
@@ -7,6 +7,7 @@ use App\Enums\FinancingOrderStatus;
 use App\Enums\MediaCollections\FinancingOrderMediaCollection;
 use App\Models\FinancingOrder;
 use App\Support\Traders\Facades\Trader;
+use App\Support\Traders\TraderHelper;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -17,7 +18,7 @@ use Illuminate\Support\Facades\DB;
 
 class ProcessDmccRespondedToPtpOrder implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, TraderHelper;
 
     protected mixed $financingOrder;
 
@@ -64,7 +65,7 @@ class ProcessDmccRespondedToPtpOrder implements ShouldQueue
                 FinancingOrderHistory::GetPtpDocument
             );
 
-            $trader->attachDocumentToOrder(
+            $this->attachDocumentToOrder(
                 $lastTraderOrder,
                 $ptpDocument,
                 FinancingOrderMediaCollection::PromiseToPurchase,
@@ -86,7 +87,7 @@ class ProcessDmccRespondedToPtpOrder implements ShouldQueue
                 FinancingOrderHistory::GetTtiHoldingCertificateDocument
             );
 
-            $trader->attachDocumentToOrder(
+            $this->attachDocumentToOrder(
                 $lastTraderOrder,
                 $ttiDocument,
                 FinancingOrderMediaCollection::TtiHoldingCertificate,
