d3f1770d179ba4bd9d4b446ed4aac9f5c5ff8912
Ahmedd-Ibrahim <0ahmedibrahem@gmail.com>
2023-01-15 10:36:14 +0200

    [LYNKB-353][Unit Test][Jobs] ProcessAskClientForWakala

---
+++ tests/Unit/Jobs/Dmcc/ProcessAskClientForWakalaTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Unit\Jobs\Dmcc;
+
+use App\Enums\FinancingOrderStatus;
+use App\Enums\Role;
+use App\Jobs\Dmcc\ProcessAskClientForWakala;
+use App\Models\Company;
+use App\Models\FinancingOrder;
+use App\Models\User;
+use App\Support\Sms\Events\SmsSent;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+use Tests\Traits\InteractsWithLender;
+
+class ProcessAskClientForWakalaTest extends TestCase
+{
+    use RefreshDatabase, InteractsWithLender;
+
+    protected static Company $company;
+
+    protected static User $lender;
+
+    protected static FinancingOrder $order;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        [self::$company] = $this->createCompany();
+        self::$lender = $this->createLenderUser(self::$company->id, Role::LenderAdmin);
+        self::$order = $this->createOrder(self::$company->id, self::$lender->id, [
+            'status' => FinancingOrderStatus::Approved,
+        ]);
+    }
+
+    public function test_process_ask_client_for_wakala_status_moved_to_waiting_client_wakala_successfully()
+    {
+        $processOrder = new ProcessAskClientForWakala(self::$order->id);
+
+        $processOrder->handle();
+        self::$order = self::$order->fresh();
+
+        $this->assertTrue(self::$order->status->is(FinancingOrderStatus::WaitingClientWakala));
+    }
+
+    public function test_process_ask_client_for_wakala_sms_sent_successfully()
+    {
+        Event::fake([
+            SmsSent::class,
+        ]);
+
+        $processOrder = new ProcessAskClientForWakala(self::$order->id);
+
+        $processOrder->handle();
+
+        Event::assertDispatched(SmsSent::class);
+    }
+}
