9a80b23055f0fa6396b7fb5550c32b42c224502e
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-01-22 17:21:52 +0300

    adding fixes

--- tests/Unit/Trader/Facade/TraderFacadeTest.php
+++ tests/Unit/Trader/Facade/TraderFacadeTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Trader\Facade;
 
 use App\Support\Traders\Facades\Trader;
+use App\Support\Traders\TraderManager;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -18,9 +19,10 @@ class TraderFacadeTest extends TestCase
     /**
      * @return void
      */
-    public function test_underlying_resolved_instance_TraderManager_class(): void
+    public function test_underlying_resolved_instance_trader_manager_class(): void
     {
-        $this->assertInstanceOf('App\Support\Traders\TraderManager',
+        $this->assertInstanceOf(
+            TraderManager::class,
             Trader::getFacadeRoot()
         );
     }
