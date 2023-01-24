c5d5214610e0d43973e361912739baa79205a09b
Ahmedd-Ibrahim <0ahmedibrahem@gmail.com>
2023-01-24 12:25:35 +0200

    tests

--- tests/Feature/Endpoints/Api/V1/Admin/Traders/TraderCompanyControllerUpdateTest.php
+++ tests/Feature/Endpoints/Api/V1/Admin/Traders/TraderCompanyControllerUpdateTest.php
@@ -36,7 +36,6 @@ class TraderCompanyControllerUpdateTest extends TestCase
         self::$companyDetails = [
             'name' => 'testCompany',
             'unique_name' => 'companyUniqueName',
-            'company_cr' => '1234567891',
             'driver' => 'dmcc',
         ];
 
@@ -72,7 +71,6 @@ class TraderCompanyControllerUpdateTest extends TestCase
         $this->actingAs(self::$superAdmin)
             ->putJson('api/v1/admin/traders/'.self::$company->id, [
                 'unique_name' => 'companyUniqueName',
-                'company_cr' => '1234567891',
                 'driver' => 'dmcc',
             ])
             ->assertJsonValidationErrorFor('name');
@@ -83,21 +81,9 @@ class TraderCompanyControllerUpdateTest extends TestCase
         $this->actingAs(self::$superAdmin)
             ->putJson('api/v1/admin/traders/'.self::$company->id, [
                 'name' => 'name',
-                'company_cr' => '1234567891',
                 'driver' => 'dmcc',
             ])
             ->assertJsonValidationErrorFor('unique_name');
-    }
-
-    public function test_trader_company_controller_update_without_company_cr_unsuccessful()
-    {
-        $this->actingAs(self::$superAdmin)
-            ->putJson('api/v1/admin/traders/'.self::$company->id, [
-                'name' => 'testCompany',
-                'unique_name' => 'companyUniqueName',
-                'driver' => 'dmcc',
-            ])
-            ->assertJsonValidationErrorFor('company_cr');
     }
 
     public function test_trader_company_controller_update_driver_should_be_in_fake_dmcc_unsuccessful()
@@ -106,7 +92,6 @@ class TraderCompanyControllerUpdateTest extends TestCase
             ->putJson('api/v1/admin/traders/'.self::$company->id, [
                 'name' => 'testCompany',
                 'unique_name' => 'companyUniqueName',
-                'company_cr' => '1234567891',
                 'driver' => 'random',
             ])
             ->assertJsonValidationErrorFor('driver');
