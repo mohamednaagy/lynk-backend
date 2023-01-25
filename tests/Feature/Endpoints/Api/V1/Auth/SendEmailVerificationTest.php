0ad0e168275bdd3be5428a32e049c3515bc9b7b9
Ahmedd-Ibrahim <0ahmedibrahem@gmail.com>
2023-01-25 10:50:54 +0200

    change lender to lender user

--- tests/Feature/Endpoints/Api/V1/Auth/SendEmailVerificationTest.php
+++ tests/Feature/Endpoints/Api/V1/Auth/SendEmailVerificationTest.php
@@ -15,9 +15,9 @@ class SendEmailVerificationTest extends TestCase
 {
     use RefreshDatabase, InteractsWithUser, InteractsWithCompany;
 
-    private static User $lender;
+    private static User $lenderUser;
 
-    private static User $secondLender;
+    private static User $secondLenderUSer;
 
     /**
      * @return void
@@ -26,42 +26,42 @@ class SendEmailVerificationTest extends TestCase
     {
         parent::setUp();
         [$company] = $this->createLenderCompany();
-        self::$lender = $this->createLenderUser($company->id);
-        self::$secondLender = $this->createLenderUser($company->id);
+        self::$lenderUser = $this->createLenderUser($company->id);
+        self::$secondLenderUSer = $this->createLenderUser($company->id);
     }
 
     public function test_send_email_verification_redirect_url_input_is_required()
     {
-        $this->actingAs(self::$lender)
+        $this->actingAs(self::$lenderUser)
             ->postJson('api/v1/auth/send-email-verification')
             ->assertJsonValidationErrorFor('redirect_url');
     }
 
     public function test_send_email_verification_redirect_url_input_should_be_valid_url()
     {
-        $this->actingAs(self::$lender)
+        $this->actingAs(self::$lenderUser)
             ->postJson('api/v1/auth/send-email-verification', ['redirect_url' => 'wrong://localhost'])
             ->assertJsonValidationErrorFor('redirect_url');
     }
 
     public function test_send_email_verification_redirect_url_should_be_in_app_white_list()
     {
-        $this->actingAs(self::$lender)
+        $this->actingAs(self::$lenderUser)
             ->postJson('api/v1/auth/send-email-verification', ['redirect_url' => 'http://wrong-website'])
             ->assertJsonValidationErrorFor('redirect_url');
     }
 
     public function test_send_email_verification_email_input_should_be_unique()
     {
-        $this->actingAs(self::$lender)
-            ->postJson('api/v1/auth/send-email-verification', ['email' => self::$secondLender->email])
+        $this->actingAs(self::$lenderUser)
+            ->postJson('api/v1/auth/send-email-verification', ['email' => self::$secondLenderUSer->email])
             ->assertJsonValidationErrorFor('email');
     }
 
     public function test_send_email_verification_update_email_if_the_user_provide_new_email()
     {
         $newEmail = 'newEmail@email.com';
-        $this->actingAs(self::$lender)
+        $this->actingAs(self::$lenderUser)
             ->postJson(
                 'api/v1/auth/send-email-verification',
                 [
@@ -71,16 +71,16 @@ class SendEmailVerificationTest extends TestCase
             )
             ->assertStatus(Response::HTTP_OK);
 
-        self::$lender->refresh();
+        self::$lenderUser->refresh();
 
-        $this->assertTrue(self::$lender->email == $newEmail);
+        $this->assertTrue(self::$lenderUser->email == $newEmail);
     }
 
     public function test_send_email_verification_mail_sent_successfully()
     {
         Mail::fake();
 
-        $this->actingAs(self::$lender)
+        $this->actingAs(self::$lenderUser)
             ->postJson(
                 'api/v1/auth/send-email-verification',
                 [
@@ -90,7 +90,7 @@ class SendEmailVerificationTest extends TestCase
             ->assertStatus(200);
 
         Mail::assertQueued(VerifyEmail::class, function ($mail) {
-            return $mail->to(self::$lender->email);
+            return $mail->to(self::$lenderUser->email);
         });
     }
 }
