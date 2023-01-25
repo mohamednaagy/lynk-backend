1f7a353f5d08a27d2102c689c42902c5e6646f21
Ahmedd-Ibrahim <0ahmedibrahem@gmail.com>
2023-01-25 13:46:23 +0200

    other roles test

--- tests/Unit/Providers/GateBeforeTest.php
+++ tests/Unit/Providers/GateBeforeTest.php
@@ -19,7 +19,7 @@ class GateBeforeTest extends TestCase
 
     private static User $userLenderOrderCreator;
 
-    private static User $admin;
+    private static User $superAdmin;
 
     private static User $traderAdmin;
 
@@ -32,11 +32,12 @@ class GateBeforeTest extends TestCase
         parent::setUp();
 
         [self::$company] = $this->createLenderCompany();
-        self::$admin = $this->createSuperAdminUser();
+        self::$superAdmin = $this->createSuperAdminUser();
         self::$traderAdmin = $this->createTraderUser(self::$company->id);
         self::$userLenderAdmin = $this->createLenderUser(self::$company->id, Role::LenderAdmin);
         self::$userLenderOrderCreator = $this->createLenderUser(self::$company->id, Role::LenderOrderCreator);
         self::$order = $this->createOrder(self::$company->id, self::$userLenderAdmin->id);
+        $this->withoutMiddleware([\Spatie\Permission\Middlewares\RoleMiddleware::class]);
     }
 
     public function test_gate_before_order_in_trader_area_only_lender_admin_can_access()
@@ -46,7 +47,7 @@ class GateBeforeTest extends TestCase
             ->getJson('api/v1/lender/orders/'.self::$order->id)
             ->assertStatus(Response::HTTP_OK);
 
-        $this->actingAs(self::$admin)
+        $this->actingAs(self::$superAdmin)
             ->withHeader('X-Company', self::$company->id)
             ->getJson('api/v1/lender/orders/'.self::$order->id)
             ->assertStatus(Response::HTTP_FORBIDDEN);
@@ -55,5 +56,38 @@ class GateBeforeTest extends TestCase
             ->withHeader('X-Company', self::$company->id)
             ->getJson('api/v1/lender/orders/'.self::$order->id)
             ->assertStatus(Response::HTTP_FORBIDDEN);
+    }
+
+    public function test_gate_before_users_in_trader_area_only_trader_admin_can_access()
+    {
+        $this->actingAs(self::$superAdmin)
+            ->withHeader('X-Company', self::$company->id)
+            ->getJson('api/v1/trader/users')
+            ->assertStatus(Response::HTTP_FORBIDDEN);
+
+        $this->actingAs(self::$userLenderAdmin)
+            ->withHeader('X-Company', self::$company->id)
+            ->getJson('api/v1/trader/users')
+            ->assertStatus(Response::HTTP_FORBIDDEN);
+
+        $this->actingAs(self::$traderAdmin)
+            ->withHeader('X-Company', self::$company->id)
+            ->getJson('api/v1/trader/users')
+            ->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_gate_before_lenders_in_admin_area_only_super_admin_can_access()
+    {
+        $this->actingAs(self::$userLenderAdmin)
+            ->getJson('api/v1/admin/lenders')
+            ->assertStatus(Response::HTTP_FORBIDDEN);
+
+        $this->actingAs(self::$traderAdmin)
+            ->getJson('api/v1/admin/lenders')
+            ->assertStatus(Response::HTTP_FORBIDDEN);
+
+        $this->actingAs(self::$superAdmin)
+            ->getJson('api/v1/admin/lenders')
+            ->assertStatus(Response::HTTP_OK);
     }
 }
