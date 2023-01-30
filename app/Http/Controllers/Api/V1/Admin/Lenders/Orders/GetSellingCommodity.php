44004799237420a45480064a46400e3396d26873
youssofOkiel <y.okiel@bimsaudi.com>
2023-01-30 10:46:03 +0200

    [LYNKB104][Backend] Fetch data of selling commodity endpoint

---
+++ app/Http/Controllers/Api/V1/Admin/Lenders/Orders/GetSellingCommodity.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin\Lenders\Orders;
+
+use App\Enums\Action;
+use App\Enums\Area;
+use App\Enums\MediaCollections\FinancingOrderMediaCollection;
+use App\Enums\Subject;
+use App\Http\Controllers\Controller;
+use App\Models\Company;
+use App\Models\FinancingOrder;
+use Illuminate\Http\JsonResponse;
+
+class GetSellingCommodity extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(
+            'permission:'.
+            perm(Area::SuperAdmin, [Subject::FinancingOrders, Action::Show, Action::Manage])
+        );
+    }
+
+    /**
+     * @param  Company  $lender
+     * @param  FinancingOrder  $order
+     * @return JsonResponse
+     */
+    public function __invoke(Company $lender, FinancingOrder $order): JsonResponse
+    {
+        $url = $this->fileUrl($order->getMedia(FinancingOrderMediaCollection::SellingCommodityToCustomer)->first());
+
+        return $this->successResponse([$url]);
+    }
+
+    public function fileUrl($media)
+    {
+        if ($media) {
+            return route('api.v1.media.download', ['media' => $media->uuid]);
+        }
+
+        return null;
+    }
+}
