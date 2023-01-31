af91a5a771db87a5ccff55cc38b6a445a0695b96
KhuloodBatis <k.batis@bimventures.com>
2023-01-31 15:25:34 +0300

    LYNK-77-backend-delete-user-endpoint

--- app/Http/Controllers/Api/V1/Trader/Users/UserController.php
+++ app/Http/Controllers/Api/V1/Trader/Users/UserController.php
@@ -154,11 +154,13 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  User  $user
+     * @return JsonResponse
      */
-    public function destroy($id)
+    public function destroy(User $user): JsonResponse
     {
-        //
+        $user->delete();
+
+        return $this->successResponse();
     }
 }
