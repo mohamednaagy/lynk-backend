d62a543999439ab9cae3d8e45222b4d16a119bb7
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-01-31 14:42:58 +0300

    adding improvements

--- app/Http/Requests/V1/Admin/Lender/Orders/MurabahaPurchaseOffer/UpdateDocumentRequest.php
+++ app/Http/Requests/V1/Admin/Lender/Orders/MurabahaPurchaseOffer/UpdateDocumentRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\V1\Admin\Lender\Orders\MurabahaPurchase;
+namespace App\Http\Requests\V1\Admin\Lender\Orders\MurabahaPurchaseOffer;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -24,7 +24,7 @@ class UpdateDocumentRequest extends FormRequest
     public function rules()
     {
         return [
-            'document' => ['file'],
+            'document' => ['required', 'file', 'mimes:pdf'],
         ];
     }
 }
