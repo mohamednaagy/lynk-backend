465e11b5ebceb8088029ece3d19c8963f194a01f
Fady Ibrahim <fadysobhy111@gmail.com>
2023-02-07 10:35:15 +0200

    [LYNK-132][Backend] Added some changes

--- app/Http/Controllers/Api/V1/Trader/FinancingOrders/TraderOrders/MurabhaCompleteDocument/UpdateMurabhaCompleteDocument.php
+++ app/Http/Controllers/Api/V1/Trader/FinancingOrders/TraderOrders/MurabhaCompleteDocument/UpdateMurabhaCompleteDocument.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\Api\V1\Trader\FinancingOrders\TraderOrders\MurabhaCompleteDocument;
 
+use App\Actions\Contracts\Orders\TraderOrders\ProceedMurabhaCompleteDocument;
 use App\Actions\Contracts\Orders\TraderOrders\UpdateMurabhaCompleteDocument as UpdateMurabhaCompleteDocumentInterface;
 use App\Enums\Action;
 use App\Enums\Area;
+use App\Enums\FinancingOrderStatus;
 use App\Enums\Subject;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\V1\Trader\Orders\MurabhaCompleteDocument\UpdateDocumentRequest;
+use App\Http\Requests\V1\Trader\Orders\MurabhaCompleteDocument\UpdateMurabhaCompleteDocumentRequest;
 use App\Models\TraderOrder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
@@ -18,25 +20,29 @@ class UpdateMurabhaCompleteDocument extends Controller
     {
         $this->middleware(
             'permission:'.
-            perm(Area::Trader, [Subject::FinancingOrders, Action::Show, Action::Manage])
+            perm(Area::Trader, [Subject::FinancingOrders, Action::Edit, Action::Manage])
         );
     }
 
     /**
      * Handle the incoming request.
      *
-     * @param  UpdateDocumentRequest  $request
+     * @param  UpdateMurabhaCompleteDocumentRequest  $request
      * @param  int  $order
      * @param  TraderOrder  $traderOrder
      * @return JsonResponse
      */
     public function __invoke(
-        UpdateDocumentRequest $request,
+        UpdateMurabhaCompleteDocumentRequest $request,
         int $order,
         TraderOrder $traderOrder
     ): JsonResponse {
         return DB::transaction(function () use ($request, $order, $traderOrder) {
-            app(UpdateMurabhaCompleteDocumentInterface::class)->handle($request, $order, $traderOrder);
+            if ($traderOrder->checkOrderStepComplete(FinancingOrderStatus::MurabahaSaleCompleted)) {
+                app(UpdateMurabhaCompleteDocumentInterface::class)->handle($request, $traderOrder);
+            } else {
+                app(ProceedMurabhaCompleteDocument::class)->handle($request, $order, $traderOrder);
+            }
 
             return $this->successResponse();
         });
--- app/Http/Requests/V1/Trader/Orders/MurabhaCompleteDocument/UpdateMurabhaCompleteDocumentRequest.php
+++ app/Http/Requests/V1/Trader/Orders/MurabhaCompleteDocument/UpdateMurabhaCompleteDocumentRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\V1\Trader\Orders\MurabhaCompleteDocument;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateDocumentRequest extends FormRequest
+class UpdateMurabhaCompleteDocumentRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
