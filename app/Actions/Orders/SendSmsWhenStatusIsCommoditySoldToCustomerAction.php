581cd9ce9e86e10c56e3f3d1c70d04f268fcacad
Ahmedd-Ibrahim <0ahmedibrahem@gmail.com>
2023-02-07 11:35:24 +0200

    [LYNK-148][backend][SMS] Selling commodity to client on behalf of the bank

--- app/Actions/Orders/SendSmsWhenStatusIsCommoditySoldToCustomerAction.php
+++ app/Actions/Orders/SendSmsWhenStatusIsCommoditySoldToCustomerAction.php
@@ -4,28 +4,46 @@ namespace App\Actions\Orders;
 
 use App\Actions\Contracts\Orders\SendSmsWhenStatusIsCommoditySoldToCustomer;
 use App\Enums\ClientMessage;
-use App\Enums\MediaCollections\TraderOrderMediaCollection;
 use App\Models\FinancingOrder;
 use App\Support\Sms\Sms;
+use Illuminate\Support\Facades\Config;
 
 class SendSmsWhenStatusIsCommoditySoldToCustomerAction implements SendSmsWhenStatusIsCommoditySoldToCustomer
 {
     public function handle(FinancingOrder $financingOrder, string $product, string $quantity): void
     {
-        $sellingPrice = $financingOrder->selling_price ?? '';
-        $url = $financingOrder->activeTraderOrder()
-            ->first()
-            ->getFirstMedia(TraderOrderMediaCollection::SellingCommodityToCustomer) ?? '';
         $phoneNumber = ltrim($financingOrder->getPhoneNumber()->formatE164(), '+');
-        $locale = app()->getLocale();
+        $message = $this->smsMessageDependsOnOrderVerification($financingOrder, $product, $quantity);
 
         Sms::send(
-            __(ClientMessage::CommoditySoldToCustomer, [
+            $message,
+            $phoneNumber
+        );
+    }
+
+    private function smsMessageDependsOnOrderVerification(FinancingOrder $financingOrder, $product, $quantity)
+    {
+        $sellingPrice = optional($financingOrder->selling_price)->getAmount() ?? '';
+        $url = Config::get('app.url');
+        $locale = app()->getLocale();
+
+        if ($financingOrder->is_verification_required) {
+            return __(ClientMessage::CommoditySoldToCustomer, [
                 'product' => $product,
+                'orderId' => $financingOrder->id,
+                'companyName' => $financingOrder->company->name,
                 'quantity' => $quantity,
                 'sellingPrice' => $sellingPrice,
-                'url' => $url,
-            ], $locale), $phoneNumber
-        );
+                'url' => $url.'/'.$financingOrder->id,
+            ], $locale);
+        }
+
+        return __(ClientMessage::CommoditySoldToCustomerWithoutVerification, [
+            'product' => $product,
+            'orderId' => $financingOrder->id,
+            'companyName' => $financingOrder->company->name,
+            'quantity' => $quantity,
+            'sellingPrice' => $sellingPrice,
+        ], $locale);
     }
 }
