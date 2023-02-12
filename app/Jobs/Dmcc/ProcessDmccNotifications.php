228caa3712de2698a3182658a00325b33a1fc9a0
youssofOkiel <y.okiel@bimsaudi.com>
2023-02-12 17:53:49 +0200

    [LYNK-146][Trader][Orders] Update/process purchasing commodity step some fixes

--- app/Jobs/Dmcc/ProcessDmccNotifications.php
+++ app/Jobs/Dmcc/ProcessDmccNotifications.php
@@ -8,7 +8,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
 
 class ProcessDmccNotifications implements ShouldQueue
 {
@@ -31,7 +30,6 @@ class ProcessDmccNotifications implements ShouldQueue
         collect(
             $trader->fetchNotifications('ACTIONABLE')
         )->each(function ($notification) {
-            Log::debug('not', [$notification]);
             try {
                 if (
                     $notification->notificationHeaderAndEntity->notification
