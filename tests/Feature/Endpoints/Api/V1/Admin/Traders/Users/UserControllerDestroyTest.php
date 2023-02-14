d1e22d1c98c7bb86d6caba2b037673b2fb2f1719
KhuloodBatis <k.batis@bimventures.com>
2023-02-14 14:18:13 +0300

    test admin cannot delete user that doesnot have trader role && test manager cannot access delete unless has the right permissions

--- tests/Feature/Endpoints/Api/V1/Admin/Traders/Users/UserControllerDestroyTest.php
+++ tests/Feature/Endpoints/Api/V1/Admin/Traders/Users/UserControllerDestroyTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature\Endpoints\Api\V1\Admin\Traders\Users;
 
+use App\Enums\Role;
 use App\Models\Company;
 use App\Models\User;
 use App\Models\Wallet;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Modules\Grantify\Facades\Grantify;
 use Tests\TestCase;
 use Tests\Traits\AssertsAccessByRoleAndArea;
 use Tests\Traits\InteractsWithCompany;
@@ -22,6 +25,8 @@ class UserControllerDestroyTest extends TestCase
 
     private static User $userTraderAdmin;
 
+    private static User $managerAdminUser;
+
     private static string $endPoint;
 
     /**
@@ -32,6 +37,7 @@ class UserControllerDestroyTest extends TestCase
         parent::setUp();
 
         self::$userAdmin = $this->createSuperAdminUser();
+        self::$managerAdminUser = $this->createSuperAdminUser(Role::Manager);
         [self::$company, self::$wallet] = $this->createTraderCompany('2000', ['company_cr' => '12345678910']);
         self::$userTraderAdmin = $this->createTraderUser(self::$company->id);
         self::$endPoint = 'api/v1/admin/traders/'.self::$company->id.'/users/'.self::$userTraderAdmin->id;
@@ -65,5 +71,31 @@ class UserControllerDestroyTest extends TestCase
 
         $newTradersUserCount = User::query()->count();
         $this->assertEquals($newTradersUserCount, $tradersUserCount - 1);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_admin_manager_user_cant_delete_trader_user_unsuccessful(): void
+    {
+        Grantify::syncPermissionToModel(self::$managerAdminUser, []);
+
+        $this->actingAs(self::$managerAdminUser)
+            ->deleteJson(self::$endPoint)
+            ->assertStatus(Response::HTTP_FORBIDDEN)
+            ->assertJsonPath('message', __('User does not have the right permissions.'));
+    }
+
+    /**
+     * @return void
+     */
+    public function test_admin_user_cant_delete_trader_user_witout_trader_role_unsuccessful(): void
+    {
+        Grantify::syncRoleToModel(self::$userTraderAdmin, Role::LenderSupervisor);
+
+        $this->actingAs(self::$userAdmin)
+            ->deleteJson(self::$endPoint)
+            ->assertStatus(Response::HTTP_FORBIDDEN)
+            ->assertJsonPath('message', __('This action is unauthorized.'));
     }
 }
