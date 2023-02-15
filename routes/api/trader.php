c638642e30eb5ae99575f80d32944bfd09f3894f
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-02-15 15:22:06 +0300

    fixing routes for register and naming style

--- routes/api/trader.php
+++ routes/api/trader.php
@@ -49,5 +49,5 @@ Route::prefix('v1/trader')->name('api.v1.trader.')->group(function () {
         });
     });
 
-    Route::post('{user}/sign-up', CompleteRegister::class)->name('trader.complete-register');
+    Route::post('{user}/sign-up', CompleteRegister::class)->name('complete-register');
 });
