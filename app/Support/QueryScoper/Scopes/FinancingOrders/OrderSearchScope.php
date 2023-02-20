0cdda6b90e030cd8b675e71bf3e5b62c64d10b5e
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-02-20 14:53:04 +0300

    adding fixes to status scope

--- app/Support/QueryScoper/Scopes/FinancingOrders/OrderSearchScope.php
+++ app/Support/QueryScoper/Scopes/FinancingOrders/OrderSearchScope.php
@@ -32,7 +32,7 @@ class OrderSearchScope extends QueryScoper
         return Validator::make(
             $data,
             [
-                'search' => ['nullable', 'string'],
+                'search' => ['required', 'string'],
             ]
         );
     }
--- app/Support/QueryScoper/Scopes/FinancingOrders/OrderStatusScope.php
+++ app/Support/QueryScoper/Scopes/FinancingOrders/OrderStatusScope.php
@@ -4,10 +4,10 @@ namespace App\Support\QueryScoper\Scopes\FinancingOrders;
 
 use App\Enums\FinancingOrderStatus;
 use App\Support\QueryScoper\QueryScoper;
+use BenSampo\Enum\Rules\EnumValue;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
 
 class OrderStatusScope extends QueryScoper
 {
@@ -38,7 +38,8 @@ class OrderStatusScope extends QueryScoper
         return Validator::make(
             $data,
             [
-                'status' => ['nullable', 'array', Rule::in(FinancingOrderStatus::getValues())],
+                'status' => ['required', 'array'],
+                'status.*' => ['required', new EnumValue(FinancingOrderStatus::class, false)],
             ]
         );
     }
