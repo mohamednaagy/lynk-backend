c170c0f84f7c48da94e9cb851afa721a51db38c6
youssofOkiel <y.okiel@bimsaudi.com>
2023-02-21 12:59:01 +0200

    [LYNK-240][Trader+Admin] Update commodity purchased code logic On Admin

--- tests/Feature/Endpoints/Api/V1/Admin/Lenders/Orders/TraderOrders/MurabhaCompleteDocument/GetMurabhaCompleteDocumentTest.php
+++ tests/Feature/Endpoints/Api/V1/Admin/Lenders/Orders/TraderOrders/MurabhaCompleteDocument/GetMurabhaCompleteDocumentTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Endpoints\Api\V1\Admin\Lenders\Orders\TraderOrders;
+namespace Tests\Feature\Endpoints\Api\V1\Admin\Lenders\Orders\TraderOrders\MurabhaCompleteDocument;
 
 use App\Enums\Area;
 use App\Enums\FinancingOrderStatus;
