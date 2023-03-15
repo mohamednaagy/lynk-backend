4227617301f7757fb950fd154a5757a86cd5c49a
Ahmed Bally <ahmedbaly87@gmail.com>
2023-03-15 17:11:16 +0200

    fix edaat webhook test

--- tests/Unit/Edaat/EdaatServiceTest.php
+++ tests/Unit/Edaat/EdaatServiceTest.php
@@ -51,6 +51,17 @@ class EdaatServiceTest extends TestCase
      */
     public function test_that_create_invoice_fail_if_id_is_not_unique(): void
     {
+        Http::fakeSequence()
+            ->push([
+                'access_token' => 'token',
+            ])
+            ->push([
+                'Status' => ['Success' => true, 'Code' => 'E000'],
+                'Body' => ['InvoiceNo' => '1'],
+            ])->push([
+                'Status' => ['Success' => false],
+            ]);
+
         $response = self::$edaatService->createInvoice(self::$randomString, self::$amount);
         $this->assertNotNull($response);
         $this->assertIsString($response);
@@ -65,6 +76,18 @@ class EdaatServiceTest extends TestCase
      */
     public function test_that_invoice_is_not_paid(): void
     {
+        Http::fakeSequence()
+            ->push([
+                'access_token' => 'token',
+            ])
+            ->push([
+                'Status' => ['Success' => true, 'Code' => 'E000'],
+                'Body' => ['InvoiceNo' => '1'],
+            ])->push([
+                'Status' => ['Success' => true, 'Code' => 'E000'],
+                'Body' => ['StatusEn' => 'Unpaid'],
+            ]);
+
         $response = self::$edaatService->createInvoice(self::$randomString, self::$amount);
         $this->assertNotNull($response);
         $this->assertIsString($response);
@@ -79,6 +102,12 @@ class EdaatServiceTest extends TestCase
      */
     public function test_that_register_webhook_return_success(): void
     {
+        Http::fake(function () {
+            return Http::response([
+                'Status' => ['Success' => true, 'Code' => 'E000'],
+            ]);
+        });
+
         $response = self::$edaatService->registerWebhook(
             config('edaat.payment_url'),
             config('edaat.bill_url'),
