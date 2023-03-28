b078c2ee6b8f29e1561e1700efa2d9de3d449dbd
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-03-28 13:42:05 +0300

    adding random string

--- app/Support/Sms/Drivers/MsegatDriver.php
+++ app/Support/Sms/Drivers/MsegatDriver.php
@@ -9,6 +9,7 @@ use App\Exceptions\MSGDuplicatedException;
 use App\Support\Sms\Events\SmsSent;
 use App\Support\Sms\SmsDriverInterface;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class MsegatDriver implements SmsDriverInterface
 {
@@ -42,7 +43,7 @@ class MsegatDriver implements SmsDriverInterface
             'numbers' => $phoneNumber,
             'userSender' => config('sms.msegat.sender_name'),
             'apiKey' => $this->apiKey,
-            'msg' => $message,
+            'msg' => $message.'\n\n'.Str::radom(6),
         ];
 
         $response = Http::post($this->baseUrl, $body);
