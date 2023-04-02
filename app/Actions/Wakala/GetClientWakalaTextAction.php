1838eb18a8b2e0b14da488acce8cb09869acb5ac
KhuloodBatis <k.batis@bimventures.com>
2023-04-02 23:32:19 +0300

    k.batis/LYNK-331

--- app/Actions/Wakala/GetClientWakalaTextAction.php
+++ app/Actions/Wakala/GetClientWakalaTextAction.php
@@ -17,7 +17,6 @@ class GetClientWakalaTextAction implements GetClientWakalaText
         $commodityNumber = $traderOrder->reference;
         $commodity = collect($traderOrder->products)->pluck('product')->implode(' و ') ?? '';
         $commodityPrice = $financingOrder->amount->formatByDecimal();
-        $orderNumber = $financingOrder->id;
         $orderDate = $financingOrder->created_at->format('Y-m-d');
         $clientName = $financingOrder->customer_name;
         $clientNationalId = $financingOrder->national_id;
@@ -32,7 +31,6 @@ class GetClientWakalaTextAction implements GetClientWakalaText
                 '{{commodityPrice}}',
                 '{{clientName}}',
                 '{{clientNationalId}}',
-                '{{orderNumber}}',
                 '{{orderDate}}',
             ],
             [
@@ -44,7 +42,6 @@ class GetClientWakalaTextAction implements GetClientWakalaText
                 $commodityPrice,
                 $clientName,
                 $clientNationalId,
-                $orderNumber,
                 $orderDate,
             ],
             $clientTemplate
