f10b90061a9edecddee87bac9a38baa96b0abd35
خلود عبد الله باتيس <50932099+KhuloodBatis@users.noreply.github.com>
2023-04-12 01:06:45 +0300

    Update StoreUserRequest.php

--- app/Http/Requests/V1/Admin/Companies/Users/StoreUserRequest.php
+++ app/Http/Requests/V1/Admin/Companies/Users/StoreUserRequest.php
@@ -14,6 +14,8 @@ class StoreUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     *
+     * @return bool
      */
     public function authorize(): bool
     {
