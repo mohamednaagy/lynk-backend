2e9d17533dbb6567e68805af6e3572086165812a
youssofOkiel <youssofokiel3@gmail.com>
2023-04-19 06:46:23 +0200

    {updated} comment overwritten methods in dmccV2

--- app/Support/Traders/Drivers/Dmcc/Strategies/DmccV2Driver.php
+++ app/Support/Traders/Drivers/Dmcc/Strategies/DmccV2Driver.php
@@ -6,26 +6,26 @@ use App\Models\FinancingOrder;
 
 class DmccV2Driver extends DmccV1Driver
 {
-    public function createTraderOrder(FinancingOrder $financingOrder): string
-    {
-        // TODO: Implement createTraderOrder() method.
-
-        return '';
-    }
-
-    public function fetchOrderResult(string $type): ?array
-    {
-        // TODO: Implement fetchNotifications() method.
-        return [];
-    }
-
-    public function ownershipToCustomer(FinancingOrder $financingOrder)
-    {
-        // TODO: Implement ownershipToCustomer() method.
-    }
-
-    public function sellingCommodity(FinancingOrder $financingOrder)
-    {
-        // TODO: Implement sellingCommodity() method.
-    }
+//    public function createTraderOrder(FinancingOrder $financingOrder): string
+//    {
+//        // TODO: Implement createTraderOrder() method.
+//
+//        return '';
+//    }
+//
+//    public function fetchOrderResult(string $type): ?array
+//    {
+//        // TODO: Implement fetchNotifications() method.
+//        return [];
+//    }
+//
+//    public function ownershipToCustomer(FinancingOrder $financingOrder)
+//    {
+//        // TODO: Implement ownershipToCustomer() method.
+//    }
+//
+//    public function sellingCommodity(FinancingOrder $financingOrder)
+//    {
+//        // TODO: Implement sellingCommodity() method.
+//    }
 }
