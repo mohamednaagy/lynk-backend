33acb154e8d13c6669dd06c00cbde38d7dda128e
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-05-11 00:26:23 +0300

    adding fix

--- app/Support/Traders/Traits/StopsTraderOrderOnJobFailure.php
+++ app/Support/Traders/Traits/StopsTraderOrderOnJobFailure.php
@@ -3,19 +3,13 @@
 namespace App\Support\Traders\Traits;
 
 use App\Enums\TraderOrderStatus;
-use App\Models\TraderOrder;
-use Illuminate\Support\Facades\DB;
 
 trait StopsTraderOrderOnJobFailure
 {
     public function failed($exception)
     {
-        $traderOrder = TraderOrder::query()->lockForUpdate()->findOrFail($this->traderOrder);
-
-        DB::transaction(function () use ($traderOrder) {
-            $traderOrder->update([
-                'status' => TraderOrderStatus::FailureToProgress,
-            ]);
-        });
+        $this->traderOrder->update([
+            'status' => TraderOrderStatus::FailureToProgress,
+        ]);
     }
 }
