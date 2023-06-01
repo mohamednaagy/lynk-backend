fbb35022ce8b40d8507b375e1c3fff3d39d3577f
Khulood Batis <50932099+KhuloodBatis@users.noreply.github.com>
2023-06-01 10:51:23 +0300

    Update CancelOrder.php after remove @pram

--- app/Http/Controllers/Api/V1/Admin/Lenders/Orders/TraderOrders/CancelOrder.php
+++ app/Http/Controllers/Api/V1/Admin/Lenders/Orders/TraderOrders/CancelOrder.php
@@ -28,8 +28,10 @@ class CancelOrder extends Controller
 
     /**
      * Handle the incoming request.
-     *
+    * @param  CancelOrderRequest  $request
      * @param  CancelOrderInterface  $cancelOrder ,
+     * @param  int  $order
+     * @return JsonResponse
      */
     public function __invoke(
         CancelOrderRequest $request,
