425a4032561bd47747cd5d58203bef5c1efb462b
youssofOkiel <youssofokiel3@gmail.com>
2023-06-06 09:50:32 +0300

    {updated} some fixes

--- app/Http/Controllers/Api/V1/Lender/Orders/ApproveOrder.php
+++ app/Http/Controllers/Api/V1/Lender/Orders/ApproveOrder.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers\Api\V1\Lender\Orders;
 
 use App\Actions\Contracts\Orders\ApproveOrder as ApproveOrderInterface;
-use App\Actions\Contracts\Wallets\DeductOrderCreationFee;
-use App\Actions\Contracts\Wallets\DeductVatPercentage;
-use App\Actions\Contracts\Wallets\GenerateZatcaInvoice;
 use App\Enums\Action;
 use App\Enums\Area;
 use App\Enums\ErrorCode;
@@ -34,18 +31,13 @@ class ApproveOrder extends Controller
     public function __invoke(
         Request $request,
         ApproveOrderInterface $approveOrder,
-        int $order,
-        DeductOrderCreationFee $deductOrderCreationFee,
-        DeductVatPercentage $deductVatPercentage,
-        GenerateZatcaInvoice $generateFatoura): JsonResponse
+        int $order): JsonResponse
     {
         return DB::transaction(function () use (
             $request,
             $approveOrder,
-            $order,
-            $deductOrderCreationFee,
-            $deductVatPercentage,
-            $generateFatoura) {
+            $order
+        ) {
             $order = FinancingOrder::lockForUpdate()->findOrFail($order);
 
             if ($order->status->cantMoveTo(FinancingOrderStatus::Approved)) {
@@ -55,15 +47,6 @@ class ApproveOrder extends Controller
                     ErrorCode::ORDER_ALREADY_APPROVED
                 );
             }
-
-            // deduct the cost from the wallet
-            $creationFeeTransaction = $deductOrderCreationFee->handle($order);
-            $deductVatPercentage->handle($order, $creationFeeTransaction, tenant());
-
-            $generateFatoura->handel(
-                $order,
-                creationFeeTransaction: $creationFeeTransaction
-            );
 
             $approveOrder->handle($order, $request->user());
 
