461a5012c29ec5491a6853d1e492c3b7be8aba33
Khuloodbatis <khuloodbatis@gmail.com>
2023-06-15 18:25:16 +0300

    add trader order delay time on GeneralSetting

---
+++ database/settings/2023_06_15_132114_add_trader_order_delay_time_to_general_settings.php
@@ -0,0 +1,11 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+return new class extends SettingsMigration
+{
+    public function up(): void
+    {
+        $this->migrator->add('general.trader_order_delay_time', 1);
+    }
+};
