eaa34b7417c622c5d852b950b5defd1686e10114
Ahmed Bally <ahmedbaly87@gmail.com>
2023-07-09 18:10:02 +0300

    test

--- resources/views/bursam-templates/bid-certificate-template.blade.php
+++ resources/views/bursam-templates/bid-certificate-template.blade.php
@@ -99,14 +99,14 @@
                 <td>Total Value (in MYR):</td>
                 <td>{{ $total_value_myr_equivalent }}</td>
             </tr>
-            <tr>
-                <td>Product Price:</td>
-                <td>{{ $price }} {{ $currency }}</td>
-            </tr>
-            <tr>
-                <td>Product Price (MYR Equivalent):</td>
-                <td>{{ $price_myr_equivalent }} MYR</td>
-            </tr>
+{{--            <tr>--}}
+{{--                <td>Product Price:</td>--}}
+{{--                <td>{{ $price }} {{ $currency }}</td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <td>Product Price (MYR Equivalent):</td>--}}
+{{--                <td>{{ $price_myr_equivalent }} MYR</td>--}}
+{{--            </tr>--}}
             <tr>
                 <td>Purchase Time/Date:</td>
                 <td>{{ $purchase_time_date }}</td>
