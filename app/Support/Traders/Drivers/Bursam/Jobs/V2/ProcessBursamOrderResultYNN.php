de0e404db95b07608df26328e6e385d5350ddd2c
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-08-13 12:17:46 +0300

    adding logs

--- app/Support/Traders/Drivers/Bursam/Jobs/V2/ProcessBursamOrderResultYNN.php
+++ app/Support/Traders/Drivers/Bursam/Jobs/V2/ProcessBursamOrderResultYNN.php
@@ -76,6 +76,7 @@ class ProcessBursamOrderResultYNN implements ShouldQueue, ShouldBeUnique
 
     public function failed($exception)
     {
+        logs()->debug('failed-exc');
         if ($exception instanceof TraderException) {
             DB::transaction(function () use ($exception) {
                 $traderOrder = TraderOrder::query()
