7de73ed1c47443a271fe8aa25e1c8efee613ee78
youssofOkiel <youssofokiel3@gmail.com>
2023-08-14 17:02:09 +0300

    {added} some fixes

---
+++ app/Jobs/General/ProcessRetrieveOrderCertificates.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Jobs\General;
+
+use App\Enums\MediaCollections\TraderOrderMediaCollection;
+use App\Enums\TraderOrderStatus;
+use App\Models\TraderOrder;
+use App\Support\Traders\Facades\Trader;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Throwable;
+
+class ProcessRetrieveOrderCertificates implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(protected int $traderOrderId)
+    {
+    }
+
+    /**
+     * Execute the job.
+     *
+     *
+     * @throws Throwable
+     */
+    public function handle(): void
+    {
+        $traderOrder = TraderOrder::query()->findOrFail($this->traderOrderId);
+        $trader = Trader::driver($traderOrder->provider, $traderOrder->version);
+
+        if ($traderOrder->status->isNot(TraderOrderStatus::Completed)) {
+            return;
+        }
+
+        if (! $traderOrder->getFirstMedia(TraderOrderMediaCollection::TtiHoldingCertificate)) {
+            $trader->getBidCertificateDetails($traderOrder);
+        }
+
+        if (! $traderOrder->getFirstMedia(TraderOrderMediaCollection::BursamSellingCommodityToCustomer)) {
+            $trader->getOtcCertificateDetails($traderOrder);
+        }
+
+        if (! $traderOrder->getFirstMedia(TraderOrderMediaCollection::BursamTtiHoldingCertificate)) {
+            $trader->getStbCertificateDetails($traderOrder);
+        }
+    }
+}
