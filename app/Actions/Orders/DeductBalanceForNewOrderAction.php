cf04635b04460b2b40c6ac8ca71a3ba9bf8dba02
youssofOkiel <youssofokiel3@gmail.com>
2023-08-22 09:22:00 +0300

    WIP

--- app/Actions/Orders/DeductBalanceForNewOrderAction.php
+++ app/Actions/Orders/DeductBalanceForNewOrderAction.php
@@ -4,35 +4,18 @@ namespace App\Actions\Orders;
 
 use App\Actions\Contracts\Orders\DeductBalanceForNewOrder;
 use App\Actions\Contracts\Wallets\DeductOrderCreationFee;
-use App\Actions\Contracts\Wallets\DeductTraderOrderVatPercentage;
-use App\Actions\Contracts\Wallets\GenerateTraderOrderInvoice;
 use App\Models\TraderOrder;
 
 class DeductBalanceForNewOrderAction implements DeductBalanceForNewOrder
 {
     public function __construct(
-        protected DeductOrderCreationFee $deductOrderCreationFee,
-        protected DeductTraderOrderVatPercentage $deductVatPercentage,
-        protected GenerateTraderOrderInvoice $generateZatcaInvoice
+        protected DeductOrderCreationFee $deductOrderCreationFee
     ) {
     }
 
     public function handle(TraderOrder $traderOrder): void
     {
-        $financingOrder = $traderOrder->order;
-
         // deduct the cost from the wallet
         $creationFeeTransaction = $this->deductOrderCreationFee->handle($traderOrder);
-
-        $this->deductVatPercentage->handle(
-            $traderOrder,
-            $creationFeeTransaction,
-            $financingOrder->company()->withTrashed()->first()
-        );
-
-        $this->generateZatcaInvoice->handle(
-            $traderOrder,
-            creationFeeTransaction: $creationFeeTransaction
-        );
     }
 }
--- app/Actions/Wallets/DeductOrderCreationFeeAction.php
+++ app/Actions/Wallets/DeductOrderCreationFeeAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Wallets;
 
+use App\Actions\Contracts\ProjectSettings\GetProjectSettings;
 use App\Actions\Contracts\Wallets\CreateTransactions;
 use App\Actions\Contracts\Wallets\DeductOrderCreationFee;
 use App\Enums\TransactionReason;
@@ -10,8 +11,10 @@ use App\Models\TraderOrder;
 
 class DeductOrderCreationFeeAction implements DeductOrderCreationFee
 {
-    public function __construct(protected CreateTransactions $createTransactions)
-    {
+    public function __construct(
+        protected CreateTransactions $createTransactions,
+        protected GetProjectSettings $getProjectSettings
+    ) {
     }
 
     public function handle(TraderOrder $traderOrder)
@@ -19,11 +22,13 @@ class DeductOrderCreationFeeAction implements DeductOrderCreationFee
         $financingOrder = $traderOrder->order;
         $company = $financingOrder->company()->withTrashed()->first();
         $wallet = $company->getWallet(WalletType::CompanyWallet);
+        $vatRate = $this->getProjectSettings->handle()->getVatRate();
+        $vatPercentageFee = $company->order_cost->multiply($vatRate);
 
         return $this->createTransactions->handle(
             $wallet,
             TransactionReason::OrderCreationFee,
-            $company->order_cost,
+            $company->order_cost->add($vatPercentageFee),
             [
                 'financing_order_id' => $financingOrder->id,
                 'trader_order_id' => $traderOrder->id,
