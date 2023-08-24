19537f85a75b52b171771c6891cd09fc5ca5662d
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-08-24 16:25:52 +0300

    adding fix for can create order

--- app/Actions/Orders/CanCreateOrderAction.php
+++ app/Actions/Orders/CanCreateOrderAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Orders;
 
+use App\Actions\Contracts\Companies\CalculateVatAmount;
 use App\Actions\Contracts\Orders\CanCreateOrder;
 use App\Enums\WalletType;
 use App\Exceptions\BalanceIsNotEnoughException;
@@ -9,11 +10,21 @@ use App\Models\Company;
 
 class CanCreateOrderAction implements CanCreateOrder
 {
+    public function __construct(protected CalculateVatAmount $calculateVatAmount)
+    {
+    }
+
     public function handle(Company $company): bool
     {
         $wallet = $company->getWallet(WalletType::CompanyWallet);
+        [$vatAmount] = $this->calculateVatAmount
+            ->setAmount($company->order_cost)
+            ->setIsVatIncludedInAmount(false)
+            ->handle();
 
-        if ($wallet->balance->greaterThanOrEqual($company->order_cost)) {
+        if ($wallet->balance->greaterThanOrEqual(
+            $company->order_cost->add($vatAmount)
+        )) {
             return true;
         }
 
