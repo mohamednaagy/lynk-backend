f31cc334402b768815c25eb365713121fef18a08
youssofOkiel <youssofokiel3@gmail.com>
2023-09-03 10:56:10 +0300

    WIP add new transaction type handlers tests

---
+++ tests/Unit/Wallets/Transactions/TransactionTypeHandlers/RefundOrderCreationFeeTypeTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Unit\Wallets\Transactions\TransactionTypeHandlers;
+
+use App\Enums\TransactionReason;
+use App\Models\Company;
+use App\Models\Transaction;
+use App\Models\Wallet;
+use App\Support\Wallets\Contracts\TransactionServiceInterface;
+use App\Support\Wallets\Contracts\TransactionTypeHandlerInterface;
+use App\Support\Wallets\Transactions\TransactionTypeHandlers\RefundOrderCreationFeeType;
+use Cknow\Money\Money;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Tests\Traits\InteractsWithCompany;
+
+class RefundOrderCreationFeeTypeTest extends TestCase
+{
+    use RefreshDatabase, InteractsWithCompany;
+
+    private static TransactionTypeHandlerInterface $transactionTypeHandler;
+
+    private static Transaction $depositTransaction;
+
+    private static Company $company;
+
+    private static Wallet $wallet;
+
+    private static array $messages = [
+        'ar' => 'استعادة رسوم لطلب المرابحة #123456',
+        'en' => 'Refund for trading request #123456',
+    ];
+
+    /**
+     * @throws BindingResolutionException
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        self::$transactionTypeHandler = new RefundOrderCreationFeeType();
+        [self::$company, self::$wallet] = $this->createCompany();
+        self::$depositTransaction = app()->make(TransactionServiceInterface::class)
+            ->deposit(
+                self::$wallet,
+                Money::parseByDecimal(115, 'SAR'),
+                TransactionReason::RefundOrderCreationFee,
+                null,
+                [
+                    'is_vat_included' => true,
+                    'financing_order_id' => '123456',
+                ]
+            );
+    }
+
+    public function test_refund_order_creation_fee_handler_implements_transaction_type_handler_interface_instance()
+    {
+        $this->assertInstanceOf(TransactionTypeHandlerInterface::class, self::$transactionTypeHandler);
+    }
+
+    public function test_refund_order_creation_fee_generate_message_method_with_all_available_locales_return_string()
+    {
+        foreach (self::$messages as $locale => $message) {
+            $transactionDescription = self::$transactionTypeHandler->generateMessage(self::$depositTransaction, $locale);
+            $this->assertEquals($message, $transactionDescription);
+        }
+    }
+
+    public function test_refund_order_creation_fee_process_method_return_transaction_model_instance()
+    {
+        $transaction = self::$transactionTypeHandler->process(
+            self::$wallet,
+            Money::parseByDecimal(100, 'SAR'),
+            TransactionReason::OrderCreationFee,
+            null,
+            []
+        );
+
+        $this->assertInstanceOf(Transaction::class, $transaction);
+    }
+}
