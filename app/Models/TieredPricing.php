5b3b2a36f050b440356ef7cc8bdba0874f33e639
youssofOkiel <youssofokiel3@gmail.com>
2023-09-18 16:14:25 +0300

    fix proration tier pricing

--- app/Models/TieredPricing.php
+++ app/Models/TieredPricing.php
@@ -44,7 +44,7 @@ class TieredPricing extends Model
 
         if ($pricing->fee_type->is(OrderFeeType::Proration)) {
             return $pricing->order_cost_without_vat->multiply(
-                $orderValue->divide($pricing->proration_amount)
+                $orderValue->getAmount() / $pricing->proration_amount->getAmount()
             );
         }
 
