be0fefc6721d7ccc1dda62f16eadafc295e49d92
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-09-27 08:17:31 +0300

    adding chunk by id

--- database/migrations/2023_08_28_102056_migrate_old_order_cost_in_comapnies_table_to_assocciate_with_tiered_table.php
+++ database/migrations/2023_08_28_102056_migrate_old_order_cost_in_comapnies_table_to_assocciate_with_tiered_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     public function up()
     {
         Company::doesntHave('tieredPricing')
-            ->chunk(20, function (Collection $companies) {
+            ->chunkById(20, function (Collection $companies) {
                 $companies->each(function (Company $company) {
 
                     $orderCostWithoutVat = money($company->order_cost, $company->order_cost_currency);
