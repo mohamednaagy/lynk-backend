c685b2bb2a9de8f64d396508f654f8eb4be4d4e6
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-10-12 10:04:37 +0300

    adding fixes to transfer ownership

--- resources/views/transfer-ownership-to-lender.blade.php
+++ resources/views/transfer-ownership-to-lender.blade.php
@@ -713,63 +713,6 @@ Constrain images and videos to the parent width and preserve their intrinsic asp
         <img class="h-20" src="{{ asset('color-logo.svg') }}" alt="">
     </div>
     <div class="divide-y divide-gray-300/50">
-        <div class="py-8 space-y-6 text-base leading-7">
-            <p class="text-black">التاريخ: {{ $date ?? '' }}</p>
-            <p class="text-black">الوقت: {{ $time ?? '' }}</p>
-            <p class="text-xl text-center text-black">شهادة حيازة</p>
-            <p class="text-center text-black">نؤكد نحن لينك أن السلع المشار لها
-                @if ($products->first()?->getWarehouse())
-                    في الموقع أدناه
-                @endif
-                ؛ في حيازتنا بالنيابة
-                عن {{ $company_name }} من
-                تاريخ وتوقيت هذه الشهادة، وقد تم شراؤها من {{ $previous_owner }} بناء على طلب الشركة مقابل مبلغ
-                وقدره {{ $amount }} ريال سعودي
-            </p>
-            <p class="text-lg font-semibold text-center text-black">بيانات السلع/ـة</p>
-            @foreach ($products ?? [] as $product)
-                <table class="min-w-full mt-4">
-                    <tbody>
-                        @if (isset($trader_order_reference))
-                            <tr>
-                                <td class="w-1/2 px-4 text-right border border-black">رقم الشهادة</td>
-                                <td class="w-1/2 border border-black">{{ $trader_order_reference }}</td>
-                            </tr>
-                        @endif
-                        <tr>
-                            <td class="w-1/2 px-4 text-right border border-black">نوع السلعة</td>
-                            <td class="w-1/2 border border-black">{{ $product->getProduct() }}</td>
-                        </tr>
-                        <tr>
-                            <td class="w-1/2 px-4 text-right border border-black">الكمية</td>
-                            <td class="w-1/2 border border-black">{{ $product->getQuantity() }} {{ $product->getUom() }}
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="w-1/2 px-4 text-right border border-black">قيمة السلعة</td>
-                            <td class="w-1/2 border border-black">{{ $product->getAmount() }} ريال سعودي</td>
-                        </tr>
-                        @if ($product->getWarehouse())
-                            <tr>
-                                <td class="w-1/2 px-4 text-right border border-black">موقع السلعة</td>
-                                <td class="w-1/2 border border-black">{{ $product->getWarehouse() }}</td>
-                            </tr>
-                        @endif
-                    </tbody>
-                </table>
-            @endforeach
-            <p class="text-lg text-center text-black">سيتم حفظ السلعة
-                @if ($products->first()?->getWarehouse())
-                    بالموقع المشار له أعلاه
-                @endif
-                ، بالنيابة عن {{ $company_name }}
-                إلى أن يتم إشعارنا بالتصرف.
-            </p>
-            <p class="text-lg text-center text-black">{{ $company_name }} سيكون مسؤولًا عن رسوم التخزين والحفظ إذا تم
-                الاحتفاظ
-                بـ\{{ $product_name }} لأكثر من يوم عمل</p>
-            <p class="pt-2 text-center text-black">توقيع المخول بالتوقيع نيابة عن ( لينك)</p>
-        </div>
         <div class="divide-y divide-gray-300/50">
             <div class="py-8 space-y-6 text-base leading-7">
                 <p class="text-black">التاريخ: {{ $date ?? '' }}</p>
@@ -841,7 +784,6 @@ Constrain images and videos to the parent width and preserve their intrinsic asp
                 <p class="text-right text-[11px] text-cyan-600">شركة تقنيات صلة المالية</p>
             </div>
         </div>
-
 </body>
 
 </html>
