75e3e5c75aeddf3ffadf98d4b6b8e6ae5788ced5
Ahmed Bally <ahmedbaly87@gmail.com>
2023-10-14 13:27:54 +0300

    migrate to new currency

---
+++ database/migrations/2023_10_12_031222_migrate_money_to_new_currency.php
@@ -0,0 +1,135 @@
+<?php
+
+use App\Actions\Contracts\GetSettingsClassInstance;
+use App\Enums\Area;
+use Cknow\Money\Money;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::table('edaat_invoices')
+            ->update([
+                'currency' => config('app.currency'),
+                'amount' => DB::raw('CONCAT(`amount`, "00")'),
+            ]);
+
+        DB::table('financing_orders')
+            ->update([
+                'currency' => config('app.currency'),
+                'amount' => DB::raw('CONCAT(`amount`, "00")'),
+                'selling_price' => DB::raw('CONCAT(`selling_price`, "00")'),
+            ]);
+
+        DB::table('wallet_notifications')
+            ->update([
+                'value' => DB::raw('CONCAT(`value`, "00")'),
+            ]);
+
+        DB::table('tiered_pricing')
+            ->update([
+                'order_value_start' => DB::raw('CONCAT(`order_value_start`, "00")'),
+                'order_value_end' => DB::raw('CONCAT(`order_value_end`, "00")'),
+                'order_cost_without_vat' => DB::raw('CONCAT(`order_cost_without_vat`, "00")'),
+                'proration_amount' => DB::raw('CONCAT(`order_value_end`, "00")'),
+            ]);
+
+        $settingInstance = app(GetSettingsClassInstance::class)->handle(Area::Lender);
+        $settingInstance->default_order_cost .= '00';
+        $settingInstance->save();
+
+        $settingInstance = app(GetSettingsClassInstance::class)->handle(Area::Trader);
+        $settingInstance->default_order_cost .= '00';
+        $settingInstance->save();
+
+        DB::table('trader_orders')
+            ->select(['id', 'data'])
+            ->chunkById(50, function ($traderOrders) {
+                foreach ($traderOrders as $traderOrder) {
+                    $data = json_decode($traderOrder->data, true);
+                    DB::table('trader_orders')
+                        ->where('id', $traderOrder->id)
+                        ->update([
+                            'data' => $this->fixJsonColumns($data),
+                        ]);
+                }
+            });
+
+        DB::table('notifications')
+            ->select(['id', 'data'])
+            ->chunkById(50, function ($notifications) {
+                foreach ($notifications as $notification) {
+                    $data = json_decode($notification->data, true);
+                    DB::table('notifications')
+                        ->where('id', $notification->id)
+                        ->update([
+                            'data' => $this->fixJsonColumns($data),
+                        ]);
+                }
+            });
+
+        DB::connection('wallet')
+            ->table('transactions')
+            ->update([
+                'currency' => config('app.currency'),
+                'amount' => DB::raw('CONCAT(`amount`, "00")'),
+            ]);
+
+        DB::connection('wallet')
+            ->table('transactions')
+            ->select(['id', 'meta'])
+            ->chunkById(50, function ($transactions) {
+                foreach ($transactions as $transaction) {
+                    $data = json_decode($transaction->meta, true);
+                    DB::connection('wallet')
+                        ->table('transactions')
+                        ->where('id', $transaction->id)
+                        ->update([
+                            'meta' => $this->fixJsonColumns($data),
+                        ]);
+                }
+            });
+
+        DB::connection('wallet')
+            ->table('transfers')
+            ->update([
+                'currency' => config('app.currency'),
+                'amount' => DB::raw('CONCAT(`amount`, "00")'),
+            ]);
+    }
+
+    public function down(): void
+    {
+        //
+    }
+
+    private function fixJsonColumns(array|string &$data): ?array
+    {
+        foreach ($data as $key => $value) {
+            if (! is_array($value)) {
+                continue;
+            }
+
+            if ($this->isMoneyObject($value)) {
+                if ($value['amount'] !== '0') {
+                    $data[$key]['amount'] .= '00';
+                }
+                $data[$key]['currency'] = config('app.currency');
+
+                $data[$key]['formatted'] = Money::parse($data[$key]['amount'], $data[$key]['currency'])->formatByIntl();
+
+                continue;
+            }
+
+            $this->fixJsonColumns($value);
+        }
+
+        return $data;
+    }
+
+    private function isMoneyObject(array $data): bool
+    {
+        return array_key_exists('amount', $data) && array_key_exists('currency', $data) && array_key_exists('formatted', $data);
+    }
+};
