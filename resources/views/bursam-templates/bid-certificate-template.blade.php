a0bbe79239e9ef34d76afc49f9f7ef35eaf0efac
Ahmed Bally <ahmedbaly87@gmail.com>
2023-10-19 01:06:42 +0300

    enhance

--- resources/views/bursam-templates/bid-certificate-template.blade.php
+++ resources/views/bursam-templates/bid-certificate-template.blade.php
@@ -93,11 +93,11 @@
             </tr>
             <tr>
                 <td>Total Value (in {{$currency}}):</td>
-                <td>{{ $total_value }}</td>
+                <td>{{ number_format($total_value, 2) }}</td>
             </tr>
             <tr>
                 <td>Total Value (in MYR):</td>
-                <td>{{ $total_value_myr_equivalent }}</td>
+                <td>{{ number_format($total_value_myr_equivalent, 2) }}</td>
             </tr>
 {{--            <tr>--}}
 {{--                <td>Product Price:</td>--}}
