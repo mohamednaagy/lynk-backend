98117c7678ddbbd2ffd544d52c25eead3acbfa5f
Ahmed Medhat <ahmed.medhat12@outlook.com>
2023-11-13 14:46:53 +0300

    adding fixes to production

--- lang/en/emails/trader-order-stopped.php
+++ lang/en/emails/trader-order-stopped.php
@@ -2,5 +2,6 @@
 
 return [
     'subject' => 'Order #:order_id Not Making Progress',
-    'body' => 'Order #:order_id is not making any progress and has stopped at the step :next_step',
+    'body_with_step' => 'Order #:order_id is not making any progress and has stopped at the step :next_step',
+    'body_without_step' => 'Order #:order_id is not making any progress',
 ];
