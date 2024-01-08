b32e802815923189aa436f6619bd2606e29b1894
Ahmed Medhat <ahmed.medhat12@outlook.com>
2024-01-08 21:34:54 +0300

    reverting back commenting out

--- database/migrations/2023_10_12_031222_migrate_money_to_new_currency.php
+++ database/migrations/2023_10_12_031222_migrate_money_to_new_currency.php
@@ -7,65 +7,65 @@ return new class extends Migration
 {
     public function up(): void
     {
-        // DB::table('edaat_invoices')
-        //     ->update([
-        //         'currency' => config('app.currency'),
-        //         'amount' => DB::raw('CONCAT(`amount`, "00")'),
-        //     ]);
+        DB::table('edaat_invoices')
+            ->update([
+                'currency' => config('app.currency'),
+                'amount' => DB::raw('CONCAT(`amount`, "00")'),
+            ]);
 
-        // DB::table('financing_orders')
-        //     ->update([
-        //         'currency' => config('app.currency'),
-        //         'amount' => DB::raw('CONCAT(`amount`, "00")'),
-        //         'selling_price' => DB::raw('CONCAT(`selling_price`, "00")'),
-        //     ]);
+        DB::table('financing_orders')
+            ->update([
+                'currency' => config('app.currency'),
+                'amount' => DB::raw('CONCAT(`amount`, "00")'),
+                'selling_price' => DB::raw('CONCAT(`selling_price`, "00")'),
+            ]);
 
-        // DB::table('wallet_notifications')
-        //     ->update([
-        //         'value' => DB::raw('CONCAT(`value`, "00")'),
-        //     ]);
+        DB::table('wallet_notifications')
+            ->update([
+                'value' => DB::raw('CONCAT(`value`, "00")'),
+            ]);
 
-        // DB::table('tiered_pricing')
-        //     ->update([
-        //         'order_value_start' => DB::raw('CONCAT(`order_value_start`, "00")'),
-        //         'order_value_end' => DB::raw('CONCAT(`order_value_end`, "00")'),
-        //         'order_cost_without_vat' => DB::raw('CONCAT(`order_cost_without_vat`, "00")'),
-        //         'proration_amount' => DB::raw('CONCAT(`order_value_end`, "00")'),
-        //     ]);
+        DB::table('tiered_pricing')
+            ->update([
+                'order_value_start' => DB::raw('CONCAT(`order_value_start`, "00")'),
+                'order_value_end' => DB::raw('CONCAT(`order_value_end`, "00")'),
+                'order_cost_without_vat' => DB::raw('CONCAT(`order_cost_without_vat`, "00")'),
+                'proration_amount' => DB::raw('CONCAT(`order_value_end`, "00")'),
+            ]);
 
-        // DB::table('trader_orders')
-        //     ->select(['id', 'data'])
-        //     ->chunkById(50, function ($traderOrders) {
-        //         foreach ($traderOrders as $traderOrder) {
-        //             if (is_null($traderOrder->data)) {
-        //                 continue;
-        //             }
+        DB::table('trader_orders')
+            ->select(['id', 'data'])
+            ->chunkById(50, function ($traderOrders) {
+                foreach ($traderOrders as $traderOrder) {
+                    if (is_null($traderOrder->data)) {
+                        continue;
+                    }
 
-        //             $data = json_decode($traderOrder->data, true);
-        //             DB::table('trader_orders')
-        //                 ->where('id', $traderOrder->id)
-        //                 ->update([
-        //                     'data' => $this->fixJsonColumnsIfMoneyObjectExists($data),
-        //                 ]);
-        //         }
-        //     });
+                    $data = json_decode($traderOrder->data, true);
+                    DB::table('trader_orders')
+                        ->where('id', $traderOrder->id)
+                        ->update([
+                            'data' => $this->fixJsonColumnsIfMoneyObjectExists($data),
+                        ]);
+                }
+            });
 
-        // DB::table('notifications')
-        //     ->select(['id', 'data'])
-        //     ->chunkById(50, function ($notifications) {
-        //         foreach ($notifications as $notification) {
-        //             if (is_null($notification->data)) {
-        //                 continue;
-        //             }
+        DB::table('notifications')
+            ->select(['id', 'data'])
+            ->chunkById(50, function ($notifications) {
+                foreach ($notifications as $notification) {
+                    if (is_null($notification->data)) {
+                        continue;
+                    }
 
-        //             $data = json_decode($notification->data, true);
-        //             DB::table('notifications')
-        //                 ->where('id', $notification->id)
-        //                 ->update([
-        //                     'data' => $this->fixJsonColumnsIfMoneyObjectExists($data),
-        //                 ]);
-        //         }
-        //     });
+                    $data = json_decode($notification->data, true);
+                    DB::table('notifications')
+                        ->where('id', $notification->id)
+                        ->update([
+                            'data' => $this->fixJsonColumnsIfMoneyObjectExists($data),
+                        ]);
+                }
+            });
 
         DB::connection('wallet')
             ->table('transactions')
