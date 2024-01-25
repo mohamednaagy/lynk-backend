4d86cd02e725e772455b6f6d1ce87437b75d7c44
.nagy <m.nagy@lynk.sa>
2024-01-25 15:26:06 +0200

    175-[AdminLYNK/Lender][Wallet] fix calculate of numbers

--- app/Actions/Lenders/CalcAmountWithoutVatAndOrdersCountAction.php
+++ app/Actions/Lenders/CalcAmountWithoutVatAndOrdersCountAction.php
@@ -31,7 +31,7 @@ class CalcAmountWithoutVatAndOrdersCountAction implements CalcAmountWithoutVatAn
 
         $chargeAmountWithoutVat = $chargeAmountWithVat->subtract($vatOfChargeAmount);
 
-        $ordersCount = $this->calcOrdersCount($company, $chargeAmountWithVat);
+        $ordersCount = round($this->calcOrdersCount($company, $chargeAmountWithVat));
 
         return [
             $chargeAmountWithoutVat,
