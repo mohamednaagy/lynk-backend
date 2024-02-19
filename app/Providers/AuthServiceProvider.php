e6761ad0da533f1b96d37f28629ac03763d9ab99
.nagy <m.nagy@lynk.sa>
2024-02-19 14:17:02 +0200

    add gate to LenderApiUser Role

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -51,6 +51,7 @@ class AuthServiceProvider extends ServiceProvider
             return match ($this->getAreaFromRequestPath()) {
                 RouteArea::Admin => $user->hasRole([Role::Admin]) ?: null,
                 RouteArea::Lender => $user->hasRole([Role::LenderAdmin]) ?: null,
+                RouteArea::Lender => $user->hasRole([Role::LenderApiUser]) ?: null,
                 RouteArea::Trader => $user->hasRole([Role::TraderAdmin]) ?: null,
                 default => null
             };
