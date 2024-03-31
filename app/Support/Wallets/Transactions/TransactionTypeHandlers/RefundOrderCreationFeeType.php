e2ade5ff5bd3cb827530045a6b0be7f423cdfafd
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2024-04-01 01:08:58 +0200

    select trader_order_id from arr::only

--- app/Support/Wallets/Transactions/TransactionTypeHandlers/RefundOrderCreationFeeType.php
+++ app/Support/Wallets/Transactions/TransactionTypeHandlers/RefundOrderCreationFeeType.php
@@ -12,7 +12,7 @@ class RefundOrderCreationFeeType implements TransactionTypeHandlerInterface
 {
     public function generateMessage(Transaction $transaction, $locale): string
     {
-        $items = Arr::only($transaction->meta, ['type', 'financing_order_id']);
+        $items = Arr::only($transaction->meta, ['type', 'financing_order_id', 'trader_order_id']);
 
         return __('transaction-description.refund_order_creation_fee', [
             'order_number' => $items['financing_order_id'] ?? '',
