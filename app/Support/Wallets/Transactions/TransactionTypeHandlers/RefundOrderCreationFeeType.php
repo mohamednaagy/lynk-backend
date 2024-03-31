f67a877bd9564330b65c686bfed7e7dfeb9c068a
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2024-04-01 01:04:20 +0200

    add trader_order_number parmater

--- app/Support/Wallets/Transactions/TransactionTypeHandlers/RefundOrderCreationFeeType.php
+++ app/Support/Wallets/Transactions/TransactionTypeHandlers/RefundOrderCreationFeeType.php
@@ -16,6 +16,7 @@ class RefundOrderCreationFeeType implements TransactionTypeHandlerInterface
 
         return __('transaction-description.refund_order_creation_fee', [
             'order_number' => $items['financing_order_id'] ?? '',
+            'trader_order_number' => $items['trader_order_id'] ?? '',
         ], $locale);
     }
 
