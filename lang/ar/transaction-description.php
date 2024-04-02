851bb3e49cc22d74be88bd3f62700edd175cc0d0
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2024-04-02 14:19:52 +0200

    return refrence number not id

--- lang/ar/transaction-description.php
+++ lang/ar/transaction-description.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'order_creation_fee' => 'رسوم إنشاء طلب #:order_number / عملية تداول #:trader_order_number',
+    'order_creation_fee' => 'رسوم إنشاء طلب #:order_number / عملية تداول #:trader_order_reference_number',
     'vat_percentage' => 'ضريبة القيمة المضافة (:vat_percentage%) للطلب #:order_id',
     'deposit_by_edaat' => 'شحن رصيد بواسطة سداد لفاتورة رقم :invoice_number',
     'manual_deposit' => 'شحن رصيد',
-    'refund_order_creation_fee' => 'استعادة رسوم طلب #:order_number / عملية تداول #:trader_order_number',
+    'refund_order_creation_fee' => 'استعادة رسوم طلب #:order_number / عملية تداول #:trader_order_reference_number',
     'refund_vat_percentage' => 'استعادة الرسوم الضريبية لطلب المرابحة #:order_id',
     'vat_percentage_recharge' => 'ضريبة القيمة المضافة (:vat_percentage%) لإعادة شحن الرصيد',
 ];
