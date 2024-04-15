a20063a9fb4ee8aca477b5e4859e108ad710db42
mohamednaagy <m.nagy@lynk.sa>
2024-04-15 15:58:09 +0200

    LYNKMRBHA-337 <fix message of email>

--- lang/en/emails/change-email.php
+++ lang/en/emails/change-email.php
@@ -2,6 +2,6 @@
 
 return [
     'dear' => 'Dear :name',
-    'the_email_you_use_to_login_to_lynk_has_been_changed_by_an_administrator' => 'The Email You Use To Login To :app_name Has Been Changed By An Administrator',
-    'please_sign_in_again_using_the_button_below_to_confirm' => 'Please sign in again to the platform to confirm this change',
+    'the_email_you_use_to_login_to_lynk_has_been_changed_by_an_administrator' => 'The email you use to login to :app_name has been changed by an administrator.',
+    'please_sign_in_again_using_the_button_below_to_confirm' => 'Please sign in again to the platform to confirm this change.',
 ];
