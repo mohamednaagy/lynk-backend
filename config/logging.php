862e3df7ce2cf4c28557cccea797858408969a67
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2024-04-15 17:44:36 +0200

    create daily log files

--- config/logging.php
+++ config/logging.php
@@ -55,7 +55,7 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
+            'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
@@ -64,7 +64,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => 30,
         ],
 
         'slack' => [
