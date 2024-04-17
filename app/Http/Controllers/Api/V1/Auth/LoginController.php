710c62c5a7a55ed1a47d18769b4c2064c1e9baa0
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2024-04-17 14:04:59 +0200

    fix issue

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -16,7 +16,6 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 use Jenssegers\Agent\Facades\Agent;
 use Stancl\Tenancy\Exceptions\TenantCouldNotBeIdentifiedById;
@@ -38,8 +37,6 @@ class LoginController extends Controller
      */
     public function authenticate(LoginRequest $request, LoginUser $loginUser, SendOtp $sendOtp)
     {
-        Log::channel('bursam')->error('check bursam log working as expected ');
-
         $companyUniqueName = $request->validated('unique_name');
         $company = null;
 
