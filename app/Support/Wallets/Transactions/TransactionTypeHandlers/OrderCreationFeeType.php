9c14688ab0650c063db22a6141d5d98853106301
mohamednaagy <m.nagy@lynk.sa>
2024-05-01 14:11:59 +0300

    return trander order if if not exist refernece

--- app/Support/Wallets/Transactions/TransactionTypeHandlers/OrderCreationFeeType.php
+++ app/Support/Wallets/Transactions/TransactionTypeHandlers/OrderCreationFeeType.php
@@ -15,8 +15,11 @@ class OrderCreationFeeType implements TransactionTypeHandlerInterface
     {
 
         $items = Arr::only($transaction->meta, ['type', 'financing_order_id', 'trader_order_id']);
-        $traderOrder = TraderOrder::find($items['trader_order_id']);
-        $traderReferenceNumber = $traderOrder->reference ?? $items['trader_order_id'];
+        $traderOrder = '';
+        if (isset($items['trader_order_id'])) {
+            $traderOrder = TraderOrder::find($items['trader_order_id']);
+        }
+        $traderReferenceNumber = $traderOrder ? ($traderOrder->reference ? $traderOrder->reference : $traderOrder->id) : '';
 
         return __('transaction-description.order_creation_fee', [
             'order_number' => $items['financing_order_id'] ?? '',
--- app/Support/Wallets/Transactions/TransactionTypeHandlers/RefundOrderCreationFeeType.php
+++ app/Support/Wallets/Transactions/TransactionTypeHandlers/RefundOrderCreationFeeType.php
@@ -14,8 +14,11 @@ class RefundOrderCreationFeeType implements TransactionTypeHandlerInterface
     public function generateMessage(Transaction $transaction, $locale): string
     {
         $items = Arr::only($transaction->meta, ['type', 'financing_order_id', 'trader_order_id']);
-        $traderOrder = TraderOrder::find($items['trader_order_id']);
-        $traderReferenceNumber = $traderOrder->reference ?? $items['trader_order_id'];
+        $traderOrder = '';
+        if (isset($items['trader_order_id'])) {
+            $traderOrder = TraderOrder::find($items['trader_order_id']);
+        }
+        $traderReferenceNumber = $traderOrder ? ($traderOrder->reference ? $traderOrder->reference : $traderOrder->id) : '';
 
         return __('transaction-description.refund_order_creation_fee', [
             'order_number' => $items['financing_order_id'] ?? '',
