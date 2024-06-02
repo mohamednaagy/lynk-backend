b087cd16922153cd3253549f8d97b9595712c2c0
aadell365 <a.mahmoud@lynk.sa>
2024-06-03 02:30:11 +0300

    add listing Inventories

---
+++ app/Actions/Supplier/CommodityItem/Inventory/GetPaginatedCommodityInventoriesAction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Actions\Supplier\CommodityItem\Inventory;
+
+use App\Actions\Contracts\Supplier\CommodityItem\Inventory\GetPaginatedCommodityInventories;
+use App\Models\Inventory;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class GetPaginatedCommodityInventoriesAction implements GetPaginatedCommodityInventories
+{
+
+    public function handle($supplier, $item): LengthAwarePaginator
+    {
+        return Inventory::query()
+            ->where('company_id', $supplier->id)
+            ->where('commodity_item_id', $item->id)
+            ->paginate();
+    }
+}
