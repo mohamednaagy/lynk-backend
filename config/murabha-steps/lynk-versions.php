90c144fe531180dae11a102bff64e4a3edccb2b1
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2024-06-04 12:30:53 +0300

    adding a new version steps

---
+++ config/murabha-steps/lynk-versions.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Enums\FinancingOrderHistory;
+use App\Enums\MediaCollections\TraderOrderMediaCollection;
+use App\Enums\MurabhaStep;
+
+return [
+    'v1' => [
+        MurabhaStep::TraderOrderCreated => [
+            FinancingOrderHistory::GetTtiId => null,
+        ],
+        MurabhaStep::PurchasingCommodity => [
+            FinancingOrderHistory::GetTtiHoldingCertificateDocument => null,
+            FinancingOrderHistory::AttachTtiHoldingCertificateDocument => [
+                'collection' => TraderOrderMediaCollection::TtiHoldingCertificate,
+                'file' => 'original_holding_certificate',
+            ],
+            FinancingOrderHistory::CreateTransferOwnershipToLenderDocument => null,
+        ],
+        MurabhaStep::ContractSigned => [
+            FinancingOrderHistory::ContractSigned => null,
+        ],
+        MurabhaStep::ClientWakala => [
+            FinancingOrderHistory::WaitingClientWakala => null,
+            FinancingOrderHistory::ClientWakalaAccepted => null,
+        ],
+        MurabhaStep::CommoditySoldToCustomer => [
+            FinancingOrderHistory::CreateSellingCommodityToCustomerDocument => null,
+        ],
+        MurabhaStep::MurabhaOfferIssued => [
+            FinancingOrderHistory::IssueMurabahaOffer => null,
+            FinancingOrderHistory::GetMurabahaPurchaseOfferDocument => null,
+            FinancingOrderHistory::AttachMpoDocument => [
+                'collection' => TraderOrderMediaCollection::MurabahaPurchaseOrder,
+                'file' => 'document',
+            ],
+        ],
+        MurabhaStep::MurabahaSaleCompleted => [
+            FinancingOrderHistory::GetWarrantAmendmentExceptWarrantNoDocument => null,
+            FinancingOrderHistory::AttachWarrantAmendmentExceptWarrantNoDocument => [
+                'collection' => TraderOrderMediaCollection::WarrantAmendmentExceptWarrantNo,
+                'file' => 'document',
+            ],
+            FinancingOrderHistory::MurabahaSaleCompleted => null,
+        ],
+    ],
+];
