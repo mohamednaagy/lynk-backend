ff53b275b76183665b0d2b1f9bc827fb1267eb1d
aadell365 <a.mahmoud@lynk.sa>
2024-06-09 13:54:44 +0300

    fix issue for update item on inventory when update the item

---
+++ app/Observers/CommodityItemObserver.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\CommodityItem;
+use App\Models\Inventory;
+
+class CommodityItemObserver
+{
+    public $afterCommit = true;
+
+    /**
+     * Handle the CommodityItem "updated" event.
+     *
+     * @return void
+     */
+    public function updated(CommodityItem $item)
+    {        
+        if ($item->isDirty('min_price') || $item->isDirty('max_price')) {
+            Inventory::where('commodity_item_id', $item->id)->update([
+                'min_price' => $item->min_price,
+                'max_price' => $item->max_price,
+            ]);
+        }
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,10 +4,12 @@ namespace App\Providers;
 
 use App\Events\TraderOrderCancelled;
 use App\Listeners\RefundOrderCost;
+use App\Models\CommodityItem;
 use App\Models\FinancingOrder;
 use App\Models\TraderHistory;
 use App\Models\TraderOrder;
 use App\Models\Transaction;
+use App\Observers\CommodityItemObserver;
 use App\Observers\FinancingOrderObserver;
 use App\Observers\TraderHistoryObserver;
 use App\Observers\TraderOrderObserver;
@@ -37,6 +39,7 @@ class EventServiceProvider extends ServiceProvider
         TraderHistory::class => [TraderHistoryObserver::class],
         TraderOrder::class => [TraderOrderObserver::class],
         Transaction::class => [TransactionObserver::class],
+        CommodityItem::class => [CommodityItemObserver::class],
     ];
 
     /**
