e261a2b0a77c8e26ac1ed8727f5402984323cfd7
mohamednaagy <m.nagy@lynk.sa>
2024-06-09 15:37:07 +0300

    fix bugs LYNKMRBHA-638

--- app/Http/Requests/V1/Supplier/Locations/StoreLocationRequest.php
+++ app/Http/Requests/V1/Supplier/Locations/StoreLocationRequest.php
@@ -31,7 +31,7 @@ class StoreLocationRequest extends FormRequest
                 'string',
                 'min:3',
                 'max:16',
-                Rule::unique(SupplierLocation::class, 'unique_identifier')->where('company_id', Auth()->user()->company_id),
+                Rule::unique(SupplierLocation::class, 'unique_identifier')->where('company_id', tenant()->id),
             ],
             'name' => ['required', 'string',  'max:32'],
             'description' => ['nullable', 'string', 'max:256'],
