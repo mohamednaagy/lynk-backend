c6c132b61c5a341561b707cd65858edf138dd787
aadell365 <a.mahmoud@lynk.sa>
2024-06-10 10:40:03 +0300

    fix pr

--- app/Observers/CommodityItemObserver.php
+++ app/Observers/CommodityItemObserver.php
@@ -18,8 +18,9 @@ class CommodityItemObserver
     {        
         if ($item->isDirty('min_price') || $item->isDirty('max_price')) {
             Inventory::where('commodity_item_id', $item->id)->update([
-                'min_price' => $item->min_price,
-                'max_price' => $item->max_price,
+                'min_price'         => $item->min_price,
+                'max_price'         => $item->max_price,
+                'commodity_type_id' => $item->commodity_type_id,
             ]);
         }
     }
