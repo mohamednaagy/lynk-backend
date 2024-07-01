c174f87cc44766983d6d06bfd8f0d8d9e383662f
aadell365 <a.mahmoud@lynk.sa>
2024-07-01 16:32:42 +0300

    add available / reserved units to LocalMarket CommodityItems

--- app/Models/CommodityItem.php
+++ app/Models/CommodityItem.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
@@ -48,5 +49,29 @@ class CommodityItem extends Model
     public function measurement()
     {
         return $this->belongsTo(Measurement::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function inventories() :HasMany
+    {
+        return $this->hasMany(LocalMarketInventory::class);
+    }
+
+    /**
+     * @return int
+     */
+    public function getAvailableUnitsAttribute() :int
+    {
+        return $this->inventories()->sum('available_quantity');
+    }
+
+    /**
+     * @return int
+     */
+    public function getReservedUnitsAttribute() :int
+    {
+        return $this->inventories()->sum('reserved_items');
     }
 }
