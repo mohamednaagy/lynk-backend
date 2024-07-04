fc941fb547192557b7eed9cb4257592dccadcb21
aadell365 <a.mahmoud@lynk.sa>
2024-07-04 15:53:19 +0300

    add

--- app/Services/LocalMarketService.php
+++ app/Services/LocalMarketService.php
@@ -102,7 +102,7 @@ class LocalMarketService
     }
     public function getSuitableUnits($companyId, $inventory, $loan, $needToCheckOwnerShip = false, $usedUnits, $rotations = 0)
     {
-        $numberOfNeededUnits = $loan / $inventory->max_price;
+        $numberOfNeededUnits = floor($loan / $inventory->max_price);
         dd($numberOfNeededUnits);
         $whereClause = '';
 
