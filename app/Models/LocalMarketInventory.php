f79ac497edb7ccf14c7d5f2f0def523cab9b5154
aadell365 <a.mahmoud@lynk.sa>
2024-07-14 09:38:14 +0300

    fix issue with localmarketinventory file

--- app/Models/LocalMarketInventory.php
+++ app/Models/LocalMarketInventory.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use App\Enums\LocalMarketInventoryStatus;
 use App\Enums\LocalMarketInventoryUnitsStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class LocalMarketInventory extends Model
 {
