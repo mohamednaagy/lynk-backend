3b0a130d18b11b53a606e4dd9d9b873c287b018e
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2024-07-16 17:12:15 +0300

    remove duplicate code in localmarketinventoryobserver

--- app/Observers/LocalMarketInventoryObserver.php
+++ app/Observers/LocalMarketInventoryObserver.php
@@ -4,7 +4,6 @@ namespace App\Observers;
 
 use App\Jobs\LocalMarket\UpdateInventoryStock;
 use App\Models\LocalMarketInventory;
-use Illuminate\Support\Facades\Log;
 
 class LocalMarketInventoryObserver
 {
@@ -27,12 +26,6 @@ class LocalMarketInventoryObserver
      */
     public function updating(LocalMarketInventory $inventory)
     {
-        // Log the current state for debugging
-        Log::info("Before update: {$inventory->getOriginal('total_items')} - total_items: {$inventory->total_items}, isDirty: {$inventory->isDirty('total_items')}");
-        $currentTotalItems = $inventory->total_items;
-        $originalTotalItems = $inventory->getOriginal('total_items');
-        if ($currentTotalItems !== $originalTotalItems) {
-            UpdateInventoryStock::dispatch($inventory, $originalTotalItems)->onQueue('unit-inventory');
-        }
+
     }
 }
