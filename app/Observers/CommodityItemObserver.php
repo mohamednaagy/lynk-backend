89e63f30d3cc61401f52ae4f82e2a23046efbff0
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2024-07-21 02:02:42 +0300

    complete refactoring from inventory model to LocalMarketInventory

--- app/Observers/CommodityItemObserver.php
+++ app/Observers/CommodityItemObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\CommodityItem;
-use App\Models\Inventory;
+use App\Models\LocalMarketInventory;
 
 class CommodityItemObserver
 {
@@ -17,7 +17,7 @@ class CommodityItemObserver
     public function updated(CommodityItem $item)
     {
         if ($item->isDirty('min_price') || $item->isDirty('max_price') || $item->isDirty('commodity_type_id')) {
-            Inventory::where('commodity_item_id', $item->id)->update([
+            LocalMarketInventory::where('commodity_item_id', $item->id)->update([
                 'min_price' => $item->min_price,
                 'max_price' => $item->max_price,
                 'commodity_type_id' => $item->commodity_type_id,
