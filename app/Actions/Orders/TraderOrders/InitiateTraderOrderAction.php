8dfa3ef25f83e4f310e75cf9fb44cf64f1677db4
aadell365 <a.mahmoud@lynk.sa>
2024-07-29 18:09:05 +0300

    delete condition

--- app/Actions/Orders/TraderOrders/InitiateTraderOrderAction.php
+++ app/Actions/Orders/TraderOrders/InitiateTraderOrderAction.php
@@ -26,10 +26,6 @@ class InitiateTraderOrderAction implements InitiateTraderOrder
             throw new OrderAlreadyHasActiveTraderOrderException;
         }
 
-        // if (! is_bursam_service_available()) {
-        //     throw new CommodityMarketIsUnavailableException;
-        // }
-
         $driver = $financingOrder->company->getPreferredTrader();
 
         $trader = Trader::driver($driver, get_latest_version_of_trader($driver));
