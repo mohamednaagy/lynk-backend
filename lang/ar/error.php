a7e6a845cb6426fccd51b159bf8b54d2d14836e6
aadell365 <a.mahmoud@lynk.sa>
2024-08-01 14:16:58 +0300

    enhance transaltions

--- lang/ar/error.php
+++ lang/ar/error.php
@@ -42,4 +42,6 @@ return [
     'inventory_cannot_be_updated' => 'عدد الوحدات المحجوزة أكبر من العدد الإجمالي الجديد للوحدات',
     'inventory_units_not_changed' => 'لم يتم تغيير عدد الوحدات',
     'failed_to_delete_inventory' => 'حدث خطأ أثناء محاولة حذف المخزن. يرجى المحاولة مرة أخرى لاحقاً.',
+    'inventory_cannot_be_deleted' => 'الوحدات المحجوزة أكبر من 0. لا يمكن حذف المخزن.',
+
 ];
