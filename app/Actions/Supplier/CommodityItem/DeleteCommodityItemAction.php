c65d1a603f97d7f9c93c69cd7a35a7a6e28bf3a1
aadell365 <a.mahmoud@lynk.sa>
2024-08-05 15:56:03 +0300

    enhancement the code transaction and rollback

--- app/Actions/Supplier/CommodityItem/DeleteCommodityItemAction.php
+++ app/Actions/Supplier/CommodityItem/DeleteCommodityItemAction.php
@@ -3,42 +3,14 @@
 namespace App\Actions\Supplier\CommodityItem;
 
 use App\Actions\Contracts\Supplier\CommodityItem\DeleteCommodityItem;
-use App\Actions\Supplier\CommodityItem\Inventory\DeleteCommodityInventoryAction;
+use App\Jobs\LocalMarket\DeleteCommodityItem as DeleteCommodityItemJob;
 use App\Models\CommodityItem;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class DeleteCommodityItemAction implements DeleteCommodityItem
 {
-    protected $deleteCommodityInventoryAction;
-
-    public function __construct(DeleteCommodityInventoryAction $deleteCommodityInventoryAction)
-    {
-        $this->deleteCommodityInventoryAction = $deleteCommodityInventoryAction;
-    }
-
     public function handle(CommodityItem $commodityItem): CommodityItem
     {
-        try {
-            DB::beginTransaction();
-            // Delete each related inventory and dispatch the job to delete its units
-            foreach ($commodityItem->inventories as $inventory) {
-                $this->deleteCommodityInventoryAction->handle($inventory);
-            }
-
-            // Delete the commodity item itself
-            $commodityItem->delete();
-
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();
-            Log::error('Failed to delete commodity item and its inventories.', [
-                'commodity_item_id' => $commodityItem->id,
-                'exception' => $e->getMessage(),
-                'trace' => $e->getTraceAsString()
-            ]);
-        }
-
+        DeleteCommodityItemJob::dispatch($commodityItem);
         return $commodityItem;
     }
 }
--- app/Actions/Supplier/CommodityItem/Inventory/DeleteCommodityInventoryAction.php
+++ app/Actions/Supplier/CommodityItem/Inventory/DeleteCommodityInventoryAction.php
@@ -3,14 +3,14 @@
 namespace App\Actions\Supplier\CommodityItem\Inventory;
 
 use App\Actions\Contracts\Supplier\CommodityItem\Inventory\DeleteCommodityInventory;
-use App\Jobs\LocalMarket\DeleteInventoryStock;
+use App\Jobs\LocalMarket\DeleteInventory;
 use App\Models\LocalMarketInventory;
 
 class DeleteCommodityInventoryAction implements DeleteCommodityInventory
 {
     public function handle(LocalMarketInventory $inventory): LocalMarketInventory
     {
-        DeleteInventoryStock::dispatch($inventory);
+        DeleteInventory::dispatch($inventory);
         return $inventory;
     }
 }
