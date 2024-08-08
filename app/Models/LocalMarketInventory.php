6826bcdf44515a2341cd2e65288a7c80b615aa21
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2024-08-08 13:34:51 +0300

    fix isdeletable() duplication

--- app/Models/LocalMarketInventory.php
+++ app/Models/LocalMarketInventory.php
@@ -106,14 +106,4 @@ class LocalMarketInventory extends Model
     {
         return $this->reserved_items == 0;
     }
-
-    /**
-     * Determine if the item is deletable.
-     * An item is considered deletable if there are no reserved items.
-     * @return bool
-     */
-    public function getIsDeletableAttribute(): bool
-    {
-        return $this->reserved_items == 0;
-    }
 }
