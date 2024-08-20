886049c50ae74e1e63da13854c4c5fdcafa8b6b5
aadell365 <a.mahmoud@lynk.sa>
2024-08-20 17:04:16 +0300

    update regular expersion

--- app/Rules/SpecialCharValidation.php
+++ app/Rules/SpecialCharValidation.php
@@ -26,8 +26,7 @@ class SpecialCharValidation implements Rule
     public function passes($attribute, $value)
     {
         // Define the regular expression pattern to allow the specified special characters
-        $pattern = '/[\/\\\\?<>|“\'.:;~,\sA-Za-z0-9]+/';
-
+        $pattern = '/^[a-zA-Z0-9\s\/\\\\\?<>|!@#\$%\^\&*\(\)\-+=._"“\'\:\;~\,]*$/';  
 
         // Check if the value matches the pattern
         return preg_match($pattern, $value);
