314ace2ab8a4210eddd8f21cf5f8ad3fc6f95aba
aadell365 <a.mahmoud@lynk.sa>
2024-08-28 14:51:23 +0300

    add new column has problems and fix issue with wrong data

--- database/migrations/2024_08_26_221436_update_charged_trader_orders_count_in_financing_orders.php
+++ database/migrations/2024_08_26_221436_update_charged_trader_orders_count_in_financing_orders.php
@@ -3,7 +3,8 @@
 use App\Models\FinancingOrder;
 use App\Models\Transaction;
 use Illuminate\Database\Migrations\Migration;
-
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
@@ -13,6 +14,9 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::table('financing_orders', function (Blueprint $table) {
+            $table->boolean('has_problems')->default(false);
+        });
         // Process transactions in chunks
         Transaction::chunkById(1000, function ($transactions) {
             foreach ($transactions as $transaction) {
@@ -33,12 +37,15 @@ return new class extends Migration
 
     private function updateFinancingOrderCount(Transaction $transaction): void
     {
+        $financingOrder = FinancingOrder::where('id', $transaction->financing_order_id);
         if ($transaction->amount->isNegative()) {
-            FinancingOrder::where('id', $transaction->financing_order_id)
-                ->increment('charged_trader_orders_count');
+            $financingOrder->increment('charged_trader_orders_count');
         } elseif ($transaction->amount->isPositive()) {
-            FinancingOrder::where('id', $transaction->financing_order_id)
-                ->decrement('charged_trader_orders_count');
+            if ($financingOrder->first()?->charged_trader_orders_count == 0) {
+                $financingOrder->update(['has_problems' => true]);
+            } else {
+                $financingOrder->decrement('charged_trader_orders_count');
+            }
         }
     }
 };
