e665ca073f75939df75f94ec28c30d8face3c77f
mohamednaagy <m.nagy@lynk.sa>
2024-09-05 10:47:24 +0300

    remove abort

--- app/Actions/LocalMarket/FindEligibleCommoditiesAction.php
+++ app/Actions/LocalMarket/FindEligibleCommoditiesAction.php
@@ -18,7 +18,6 @@ class FindEligibleCommoditiesAction implements FindEligibleCommodities
     public function handle(LocalMarketOrder $localMarketOrder): void
     {
         try {
-            abort(500);
             // TODO naser amount is not real amount ouble check it
             $startTime = microtime(true);
             $eligibleCommodities = $this->LoanService->getCommoditiesForLoan(
