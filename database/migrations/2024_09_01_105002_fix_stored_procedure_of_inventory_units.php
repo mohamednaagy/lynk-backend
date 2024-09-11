ba1b84c320df4f3dd1b07c5ac50d663b40355c35
mohamednaagy <m.nagy@lynk.sa>
2024-09-11 15:46:55 +0300

    fix conflict

--- database/migrations/2024_09_01_105002_fix_stored_procedure_of_inventory_units.php
+++ database/migrations/2024_09_01_105002_fix_stored_procedure_of_inventory_units.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Enums\LocalMarket\OwnershipTypes;
 use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
@@ -12,20 +11,27 @@ return new class extends Migration
      */
     public function up()
     {
-        \Illuminate\Support\Facades\DB::unprepared('DROP PROCEDURE IF EXISTS `GenerateRandomQRCodesOptimized`;');
-        \Illuminate\Support\Facades\DB::unprepared('DROP PROCEDURE IF EXISTS `GenerateRandomInventoryUnitsQRCode`;');
         \Illuminate\Support\Facades\DB::unprepared('
             CREATE DEFINER=`root`@`localhost` PROCEDURE `GenerateRandomInventoryUnitsQRCode`(
                 IN `p_local_market_inventory_id` BIGINT UNSIGNED,
                 IN `p_commodity_item_id` BIGINT UNSIGNED,
                 IN `p_number_of_units` INT UNSIGNED,
-                IN `p_current_owner` BIGINT UNSIGNED
+                IN `p_current_owner` BIGINT UNSIGNED,
+                IN `p_current_owner_type` BIGINT UNSIGNED,
+                IN `p_status` INT,
+                IN `p_qrBaseName` VARCHAR(10)
             )
             BEGIN
                 DECLARE time_now DATETIME;
                 DECLARE current_batch INT UNSIGNED DEFAULT 0;
-                DECLARE batch_size INT UNSIGNED DEFAULT 100000; -- Adjust batch size based on your system capacity
+                DECLARE batch_size INT UNSIGNED DEFAULT 1000; -- Adjust batch size based on your system capacity
                 DECLARE total_units_remaining INT UNSIGNED;
+                DECLARE EXIT HANDLER FOR SQLEXCEPTION
+                BEGIN
+                    -- Log and rollback on error
+                    ROLLBACK;
+                    SIGNAL SQLSTATE \'45000\' SET MESSAGE_TEXT = \'An error occurred. Rolling back transaction.\';
+                END;
 
                 -- Ensure we don\'t exceed two million units
                 IF p_number_of_units > 2000000 THEN
@@ -34,7 +40,7 @@ return new class extends Migration
 
                 SET time_now = NOW();
                 SET total_units_remaining = p_number_of_units;
-
+                START TRANSACTION;
                 -- Disable foreign key checks and unique checks for performance
                 SET FOREIGN_KEY_CHECKS = 0;
                 SET UNIQUE_CHECKS = 0;
@@ -56,22 +62,8 @@ return new class extends Migration
 
                     -- Insert a batch of data into the temporary table
                     INSERT INTO temp_units (qr_code)
-                    SELECT CONCAT(\'asd\', LPAD(@row := @row + 1, 12, \'0\'))
-                    FROM (
-                        SELECT @row := 0
-                    ) r, (
-                        SELECT 0 UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4
-                        UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9
-                    ) t1,
-                    (
-                        SELECT 0 UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4
-                        UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9
-                    ) t2,
-                    (
-                        SELECT 0 UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4
-                        UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9
-                    ) t3
-                    LIMIT current_batch;
+                    SELECT CONCAT(p_qrBaseName, \'-\', UUID())
+                    FROM (SELECT @row := 0) r, (SELECT 0 UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) t1, (SELECT 0 UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) t2, (SELECT 0 UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) t3 LIMIT current_batch;
 
                     -- Insert from the temporary table to the main table in bulk
                     INSERT INTO local_market_inventory_units (
@@ -88,9 +80,9 @@ return new class extends Migration
                         p_local_market_inventory_id,
                         p_commodity_item_id,
                         qr_code,
-                        0,
+                        p_status,
                         p_current_owner,
-                        '.OwnershipTypes::OriginalSupplier.',
+                        p_current_owner_type,
                         time_now,
                         time_now
                     FROM temp_units;
@@ -109,11 +101,7 @@ return new class extends Migration
                 SET FOREIGN_KEY_CHECKS = 1;
                 SET UNIQUE_CHECKS = 1;
                 SET SQL_MODE = DEFAULT;
-
                 COMMIT;
-
-                -- Final debug information
-                SELECT CONCAT(\'Total inserted: \', p_number_of_units, \' units\') AS final_result;
             END;
         ');
 
@@ -126,6 +114,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        \Illuminate\Support\Facades\DB::unprepared('DROP PROCEDURE IF EXISTS GenerateRandomInventoryUnitsQRCode');
     }
 };
