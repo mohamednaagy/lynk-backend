7eae5dc074c97708030aaffc17877f15d09ea64e
aadell365 <a.mahmoud@lynk.sa>
2024-09-16 15:41:59 +0300

    fix duplicate function conflict with LYNKMRBHA-1516-BE-Fix-the-issue

--- app/Models/LocalMarketInventory.php
+++ app/Models/LocalMarketInventory.php
@@ -144,14 +144,4 @@ class LocalMarketInventory extends Model
 
         $this->save();
     }
-
-    /**
-     * Determine if the item is deletable.
-     * An item is considered deletable if there are no reserved items.
-     * @return bool
-     */
-    public function getIsDeletableAttribute(): bool
-    {
-        return $this->reserved_items == 0;
-    }
 }
