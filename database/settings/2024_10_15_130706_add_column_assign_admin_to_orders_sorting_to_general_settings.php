52dae547898d2d9f056a4399597fe50820fc17f2
aadell365 <a.mahmoud@lynk.sa>
2024-10-16 13:10:01 +0300

    add new column to settings

---
+++ database/settings/2024_10_15_130706_add_column_assign_admin_to_orders_sorting_to_general_settings.php
@@ -0,0 +1,11 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+return new class extends SettingsMigration
+{
+    public function up(): void
+    {
+        $this->migrator->add('general.assign_admin_to_orders_sorting');
+    }
+};
