45c42a1e1fce18f2ba9d269a8a0197d300c8f7fb
aadell365 <a.mahmoud@lynk.sa>
2024-10-24 09:47:59 +0300

    remove default value from check

--- app/Http/Middleware/ThrottleManager.php
+++ app/Http/Middleware/ThrottleManager.php
@@ -18,7 +18,7 @@ class ThrottleManager extends ThrottleRequests
     public function handle($request, Closure $next, $maxAttempts = 60, $decayMinutes = 1, $prefix = '')
     {
         // Check if throttling is enabled
-        if (config('app.api_throttle_enabled', 'true')) {
+        if (config('app.api_throttle_enabled')) {
             return parent::handleRequest($request, $next,
             [
                 (object) [
