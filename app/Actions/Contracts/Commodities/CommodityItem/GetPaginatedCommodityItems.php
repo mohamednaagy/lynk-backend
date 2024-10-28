0f867069bd3d8a4601ae11ae29886ea0f655d969
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2024-10-28 15:58:10 +0300

    Feat: add new filters to commodity items listing endpoint

--- app/Actions/Contracts/Commodities/CommodityItem/GetPaginatedCommodityItems.php
+++ app/Actions/Contracts/Commodities/CommodityItem/GetPaginatedCommodityItems.php
@@ -7,4 +7,16 @@ use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 interface GetPaginatedCommodityItems
 {
     public function handle(): LengthAwarePaginator;
+
+    public function setuniqueName(?string $uniqueName): self;
+
+    public function setName(?string $name): self;
+
+    public function setSuppliers(?array $suppliers): self;
+
+    public function setCommodityTypes(?array $commodityTypes): self;
+
+    public function setDirection(string $direction = 'asc'): self;
+
+    public function setSort(string $sort = 'id'): self;
 }
