bc2996fd161409aceb6b54fe4508e9d4786cf942
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2024-10-30 17:13:38 +0300

    Feat: add new endpoint to list CommodityTypesLiteList

---
+++ app/Http/Controllers/Api/V1/Supplier/CommodityType/CommodityTypesLiteList.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Supplier\CommodityType;
+
+use App\Actions\Contracts\Commodities\CommodityType\BuildPaginatedCommodityTypeQuery;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use App\Transformers\CommodityTypeTransformer;
+use App\Enums\Area;
+use App\Enums\Action;
+use App\Enums\Subject;
+
+class CommodityTypesLiteList extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware(
+            'permission:'.
+                perm(Area::CommoditySupplier, [Subject::CommodityMarketCommodityTypes, Action::Index, Action::Manage])
+        );
+    }
+
+
+    /**
+     * Handle the incoming request to list commodity types.
+     *
+     * @param BuildPaginatedCommodityTypeQuery $buildPaginatedCommodityTypeQuery
+     * @return JsonResponse
+     */
+    public function __invoke(BuildPaginatedCommodityTypeQuery $buildPaginatedCommodityTypeQuery): JsonResponse
+    {
+        $commidityTypes = $buildPaginatedCommodityTypeQuery
+            ->setName(request('search'))
+            ->handle()
+            ->get(['id', 'name']);
+
+        return fractal($commidityTypes, new CommodityTypeTransformer())
+            ->parseIncludes(['id', 'name'])
+            ->respond();
+    }
+}
