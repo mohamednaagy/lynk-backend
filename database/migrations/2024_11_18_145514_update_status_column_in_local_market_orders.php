5b84ad5f376f8a41c56a05cf271a9754684c310d
aadell365 <a.mahmoud@lynk.sa>
2024-11-18 17:22:06 +0200

    fix migrations

---
+++ database/migrations/2024_11_18_145514_update_status_column_in_local_market_orders.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enums\LocalMarketOrderStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('local_market_orders', function (Blueprint $table) {
+            $table->unsignedSmallInteger('status')
+                  ->default(LocalMarketOrderStatus::initiate)
+                  ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('local_market_orders', function (Blueprint $table) {
+            $table->unsignedSmallInteger('status')
+                  ->default(null)
+                  ->change();
+        });
+    }
+};
