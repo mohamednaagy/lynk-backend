9cbe49ea865ae141a44ce9f361a459eb8ace40f1
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2024-12-05 14:08:58 +0200

    Feat: encapsulates time config into service

--- app/Services/TraderOrder/TimeLimitService.php
+++ app/Services/TraderOrder/TimeLimitService.php
@@ -32,9 +32,15 @@ class TimeLimitService
      * @param  string  $effectiveAt  - Format: 'Y-m-d H:i:s', timezone: UTC
      * @param  int  $defaultValue  - minutes of hours
      */
-    public function setContractSignTimeLimit(TraderOrder $traderOrder, string $effectiveAt, int $defaultValue): void
+    public function setContractSignTimeLimit(TraderOrder $traderOrder): void
     {
-        $this->setTimeLimit($traderOrder, TraderOrderTimeLimitType::ContractSignTimeLimit, $effectiveAt, $defaultValue);
+        $config = $this->getConfirmDeliveryTimeConfig();
+        $this->setTimeLimit(
+            $traderOrder,
+            TraderOrderTimeLimitType::ContractSignTimeLimit,
+            $config['effective_at'],
+            $config['default_value']
+        );
     }
 
 
@@ -72,6 +78,26 @@ class TimeLimitService
     private function getConfirmDeliveryTimeConfig()
     {
         $defaultValue = app(LocalMurabahaSettings::class)->default_customer_delivery_confirmation_time_limit;
+        $effectiveAt = Carbon::now()
+            ->timezone('UTC')
+            ->addHours($defaultValue)
+            ->format('Y-m-d H:i:s');
+        return ['default_value' => $defaultValue, 'effective_at' => $effectiveAt];
+    }
+
+    /**
+     * Get the contract sign time limit configuration.
+     *
+     * Fetches the default contract sign time limit from the local Murabaha settings,
+     * calculates the effective contract sign time by adding the default time limit to the current time in UTC,
+     * and returns these values in an associative array.
+     *
+     * @return array An associative array containing 'default_value' (the default contract sign time limit in hours)
+     *               and 'effective_at' (the calculated effective contract sign time as a string in 'Y-m-d H:i:s' format).
+     */
+    private function getContractSigneLimitTimeConfig()
+    {
+        $defaultValue = app(LocalMurabahaSettings::class)->default_contract_sign_time_limit;
         $effectiveAt = Carbon::now()
             ->timezone('UTC')
             ->addHours($defaultValue)
