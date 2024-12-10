8c001b3ba47f2bb5087a2328ee1240a90f603178
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2024-12-10 16:03:54 +0200

    Update FinancingOrderHistory.php

--- app/Enums/FinancingOrderHistory.php
+++ app/Enums/FinancingOrderHistory.php
@@ -69,8 +69,6 @@ final class FinancingOrderHistory extends Enum implements LocalizedEnum
 
     const FailedCancelled = 32;
 
-    const PendingDelivery = 33;
-
     public static array $notCancellableActions = [
         self::GetMurabahaPurchaseOfferDocument,
         self::AttachMpoDocument,
