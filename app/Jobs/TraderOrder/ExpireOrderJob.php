604c4ebd7985a3155414bc5ef5aaff7de5e3f7b7
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2024-12-12 08:33:24 +0200

    Feat: remove skip

--- app/Jobs/TraderOrder/ExpireOrderJob.php
+++ app/Jobs/TraderOrder/ExpireOrderJob.php
@@ -2,18 +2,15 @@
 
 namespace App\Jobs\TraderOrder;
 
-use App\Models\TraderOrder;
 use App\Enums\TraderOrderCancelReason;
+use App\Models\TraderOrder;
 use App\Models\TraderOrderTimeLimit;
-use App\Services\TraderOrder\TimeLimitService;
+use App\Support\Traders\Facades\Trader;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use App\Support\Traders\Facades\Trader;
-use Illuminate\Queue\Middleware\Skip;
-use App\Enums\TraderOrderTimeLimitStatus;
 use Illuminate\Support\Facades\Log;
 
 class ExpireOrderJob implements ShouldQueue
@@ -21,12 +18,13 @@ class ExpireOrderJob implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     private TraderOrderTimeLimit $traderOrderTimeLimit;
+
     private string $jobUniqueId;
 
     public function __construct(TraderOrderTimeLimit $traderOrderTimeLimit)
     {
         $this->traderOrderTimeLimit = $traderOrderTimeLimit;
-        $this->jobUniqueId = 'expire_trader_order_' . $traderOrderTimeLimit->trader_order_id;
+        $this->jobUniqueId = 'expire_trader_order_'.$traderOrderTimeLimit->trader_order_id;
         $this->onQueue('expire_trader_order');
     }
 
@@ -35,8 +33,9 @@ class ExpireOrderJob implements ShouldQueue
         try {
             $traderOrder = TraderOrder::find($this->traderOrderTimeLimit->trader_order_id);
 
-            if (!$traderOrder) {
+            if (! $traderOrder) {
                 $this->traderOrderTimeLimit->fail();
+
                 return;
             }
 
@@ -58,16 +57,5 @@ class ExpireOrderJob implements ShouldQueue
     public function getJobUniqueId(): string
     {
         return $this->jobUniqueId;
-    }
-
-    public function middleware(): array
-    {
-        return [
-            // Skip the job if the status of the TraderOrderTimeLimit is Canceled or Expired
-            Skip::when(in_array($this->traderOrderTimeLimit->status, [
-                TraderOrderTimeLimitStatus::Canceled,
-                TraderOrderTimeLimitStatus::Expired,
-            ])),
-        ];
     }
 }
