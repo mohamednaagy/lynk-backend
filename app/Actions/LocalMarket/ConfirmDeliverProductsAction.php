b8c7187256d2e288167e13e67ef4e554391d82ed
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2024-12-16 11:27:07 +0200

    Feat: change ownership in request delivery step not confirm delivery

--- app/Actions/LocalMarket/ConfirmDeliverProductsAction.php
+++ app/Actions/LocalMarket/ConfirmDeliverProductsAction.php
@@ -3,12 +3,9 @@
 namespace App\Actions\LocalMarket;
 
 use App\Actions\Contracts\LocalMarket\ConfirmDeliverProducts;
-use App\Enums\LocalMarket\OwnershipTypes;
-use App\Enums\LocalMarket\UnitOwnershipAction;
 use App\Enums\LocalMarketOrderStatus;
 use App\Models\LocalMarketOrder;
 use App\Services\LocalMarket\InventoryService;
-use App\Services\LocalMarket\UnitService;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -16,24 +13,15 @@ class ConfirmDeliverProductsAction implements ConfirmDeliverProducts
 {
     private InventoryService $inventoryService;
 
-    private UnitService $unitService;
-
     public function __construct(
     ) {
         $this->inventoryService = app(InventoryService::class);
-        $this->unitService = app(UnitService::class);
     }
 
     public function handle(LocalMarketOrder $localMarketOrder): void
     {
         DB::beginTransaction();
         try {
-            $this->unitService->changeOrderUnitsOwnershipTo(
-                $localMarketOrder,
-                OwnershipTypes::Customer,
-                $localMarketOrder->customer_name,
-                UnitOwnershipAction::ConfirmedDelivery
-            );
             $this->inventoryService->confirmDeliverOrderUnits($localMarketOrder);
             $localMarketOrder->changeStatusTo(LocalMarketOrderStatus::Delivered);
             DB::commit();
