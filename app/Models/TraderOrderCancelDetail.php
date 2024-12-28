179ed94668169c38e198b7eda4b6be82267eac4e
Mohamed Nagy <m.nagy@lynk.sa>
2024-12-28 21:51:18 +0200

    enhance code

--- app/Models/TraderOrderCancelDetail.php
+++ app/Models/TraderOrderCancelDetail.php
@@ -29,8 +29,8 @@ class TraderOrderCancelDetail extends Model
 
     public function shouldNotifyProvider(): bool
     {
-        if ($this->cancelReason->key === TraderOrderCancelReason::FailureToPurchase ||
-            $this->cancelReason->key === TraderOrderCancelReason::NoEligibleCommoditiesAvailable) {
+        if ($this->cancelReason === TraderOrderCancelReason::FailureToPurchase ||
+            $this->cancelReason === TraderOrderCancelReason::NoEligibleCommoditiesAvailable) {
             return false;
         }
 
