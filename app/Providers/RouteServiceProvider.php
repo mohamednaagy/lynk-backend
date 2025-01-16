9849e27b7ec33f6f3b77672b6ba8f8ee7fbb02f6
noureddinbadran-lynk <n.badran@lynk.sa>
2025-01-16 12:26:34 +0200

    Implementing a new algorithm to ehnace purchase eligibility and add an API to test it

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -71,6 +71,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api/supplier.php'));
 
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api/local_market.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
--- routes/api/local_market.php
+++ routes/api/local_market.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\V1\LocalMarket\LocalMarketController;
+use App\Http\Controllers\Api\V1\LocalMarket\Test\LoanController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1/local_markets')->name('api.v1.')->group(function () {
-    Route::post('buy', [LocalMarketController::class, 'buy']);
+    Route::post('loan-coverage', [LoanController::class, 'calculateLoanCoverage']);
 });
