3097c4db3afd45664ccb83724143b4d3af91d14c
Mohamed Nagy <m.nagy@lynk.sa>
2025-01-20 21:13:01 +0200

    enhancment name of functions

--- app/Actions/Orders/TraderOrders/ProceedAction/ProceedContractSignedAction.php
+++ app/Actions/Orders/TraderOrders/ProceedAction/ProceedContractSignedAction.php
@@ -35,7 +35,7 @@ class ProceedContractSignedAction implements ProceedContractSigned
 
         $this->createTraderOrderHistory($traderOrder, FinancingOrderHistory::ContractSigned);
         // use it to complete sequence of steps ( use it in ProcessFinancingOrders)
-        $traderOrder->update(['can_continue_progress' => true]);
+        $traderOrder->allowProgressToNextStep();
 
         if ($traderOrder->isNeedToGenerateWakalaDocument()) {
             app()->make(GenerateClientWakala::class)->handle($traderOrder);
