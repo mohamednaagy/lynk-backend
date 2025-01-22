26dd2ae2047331199be7680d8bedf9c0240f5152
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2025-01-22 16:06:55 +0200

    allow remove comodity item API

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -211,7 +211,7 @@ Route::prefix('v1/admin')->name('api.v1.admins.')->group(function () {
 
         Route::prefix('commodity-items')->group(function () {
             Route::apiResource('/', CommodityItemController::class)
-                ->except(['delete', 'store']);
+                ->except(['delete']);
             Route::apiResource('{item}/inventories', LocalMarketInventoryController::class)->only(['index', 'store', 'update', 'show', 'destroy']);
         });
         Route::get('constants', [ConstantController::class, 'index']);
