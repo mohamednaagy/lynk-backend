962edf83747e22def55294f5429e4dca1f049731
aadell365 <a.mahmoud@lynk.sa>
2025-01-22 12:28:06 +0200

    fix typo error

--- app/Actions/Companies/UpdateCompanyAction.php
+++ app/Actions/Companies/UpdateCompanyAction.php
@@ -53,7 +53,7 @@ class UpdateCompanyAction implements UpdateCompany
 
         if (isset($data['preferred_commodity_types'])) {
             $lender->commodityTypes()->sync($data['preferred_commodity_types']);
-            $data['force_preferred_commodity_types'] = !empty($data['preferred_commodity_types']);
+            $data['force_preferred_commodity_type'] = ! empty($data['preferred_commodity_types']);
         }
 
         $lender->lenderDetail()->updateOrCreate(
