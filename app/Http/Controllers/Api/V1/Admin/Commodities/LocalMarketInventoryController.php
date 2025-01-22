7b645ecdf276686fea98fee52d8094470f9598b8
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2025-01-22 09:04:50 +0400

    Feat: updaate inventory from admin

--- app/Http/Controllers/Api/V1/Admin/Commodities/LocalMarketInventoryController.php
+++ app/Http/Controllers/Api/V1/Admin/Commodities/LocalMarketInventoryController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers\Api\V1\Admin\Commodities;
 
 use App\Actions\Contracts\Supplier\CommodityItem\Inventory\CreateLocalMarketInventory;
 use App\Actions\Contracts\Supplier\CommodityItem\Inventory\GetPaginatedCommodityInventories;
+use App\Actions\Contracts\Supplier\CommodityItem\Inventory\UpdateCommodityInventory;
 use App\Enums\Action;
 use App\Enums\Area;
 use App\Enums\LocalMarket\InventoryStatus;
 use App\Enums\Subject;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Admin\Commodities\CommodityInventory\StoreLocalMarketInventoryRequest;
+use App\Http\Requests\V1\Supplier\Inventories\UpdateLocalMarketInventoryRequest;
 use App\Models\CommodityItem;
+use App\Models\LocalMarketInventory;
 use App\Transformers\LocalMarketInventoryTransformer;
 use Illuminate\Http\JsonResponse;
 
@@ -27,6 +30,16 @@ class LocalMarketInventoryController extends Controller
             'permission:'.
                 perm(Area::SuperAdmin, [Subject::CommoditySupplierInventories, Action::Manage, Action::Create])
         )->only('store');
+
+        $this->middleware(
+            'permission:'.
+                perm(Area::SuperAdmin, [Subject::CommoditySupplierInventories, Action::Manage, Action::Show])
+        )->only('show');
+
+        $this->middleware(
+            'permission:'.
+                perm(Area::SuperAdmin, [Subject::CommoditySupplierInventories, Action::Manage, Action::Edit])
+        )->only('update');
     }
 
     /**
@@ -108,5 +121,57 @@ class LocalMarketInventoryController extends Controller
                 'status',
             ])
             ->respond();
+    }
+
+    public function show(CommodityItem $item, LocalMarketInventory $inventory): JsonResponse
+    {
+        return fractal($inventory, new LocalMarketInventoryTransformer)
+            ->parseIncludes([
+                'id',
+                'company_id',
+                'company_name',
+                'commodity_item_id',
+                'commodity_item',
+                'commodity_type',
+                'min_price',
+                'max_price',
+                'supplier_location_id',
+                'supplier_location',
+                'total_items',
+                'available_quantity',
+                'reserved_items',
+                'status',
+                'is_editable',
+                'is_deletable',
+            ])
+            ->respond();
+    }
+
+    public function update(CommodityItem $item, LocalMarketInventory $inventory, UpdateLocalMarketInventoryRequest $updateInventoryRequest, UpdateCommodityInventory $updateCommodityInventory)
+    {
+        try {
+            $inventory = $updateCommodityInventory->handle($inventory, $updateInventoryRequest->validated());
+
+            return fractal($inventory, new LocalMarketInventoryTransformer)
+                ->parseIncludes([
+                    'id',
+                    'company_id',
+                    'comapny_name',
+                    'commodity_item_id',
+                    'commodity_item',
+                    'commodity_type',
+                    'min_price',
+                    'max_price',
+                    'supplier_location_id',
+                    'supplier_location',
+                    'total_items',
+                    'available_quantity',
+                    'reserved_items',
+                    'status',
+                ])
+                ->respond();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
