2e2cfa9cac96b0629aa73c41ad02c4e395425a47
Mohamed Nagy <m.nagy@lynk.sa>
2025-01-28 12:22:08 +0200

    remove columns from trader order (Tech Debt)

---
+++ database/migrations/2025_01_28_101812_remove_expire_at_and_defualt_contract_signed_columns_from_trader_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trader_orders', function (Blueprint $table) {
+            $table->dropColumn(['expire_at', 'default_contract_sign_time_limit']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trader_orders', function (Blueprint $table) {
+            $table->dateTime('expire_at')->nullable()->after('default_contract_sign_time_limit');
+            $table->integer('default_contract_sign_time_limit')->nullable()->comment('Default measurement unit (minutes)');
+        });
+    }
+};
