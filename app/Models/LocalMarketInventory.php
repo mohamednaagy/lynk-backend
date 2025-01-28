153efc90e6f2f06a8169d05556d38e834e03d476
Mohamed Nagy <m.nagy@lynk.sa>
2025-01-28 16:16:02 +0200

    fix bugs

--- app/Models/LocalMarketInventory.php
+++ app/Models/LocalMarketInventory.php
@@ -15,6 +15,8 @@ class LocalMarketInventory extends Model
 {
     use HasFactory, LogsActivity, SoftDeletes;
 
+    protected $appends = ['max_price'];
+
     protected $fillable = [
         'commodity_item_id',
         'supplier_location_id',
@@ -136,5 +138,10 @@ class LocalMarketInventory extends Model
         $this->save();
 
         RebuildInventory::dispatch($this->id);
+    }
+
+    public function getMaxPriceAttribute()
+    {
+        return $this->item->max_price;
     }
 }
