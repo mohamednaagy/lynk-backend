dadd62255315c15aa6bbcc09c43ce94a47fd95b6
Mohamed Nagy <m.nagy@lynk.sa>
2025-02-03 10:18:14 +0200

    chek index exist or not

--- database/migrations/2025_01_29_090733_remove_indexes_from_inventory_units_table.php
+++ database/migrations/2025_01_29_090733_remove_indexes_from_inventory_units_table.php
@@ -14,10 +14,18 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('local_market_inventory_units', function (Blueprint $table) {
-            $table->dropIndex('inventory_units_inventory_id_index');
-            $table->dropIndex('local_market_inventory_units_status_index');
-            $table->dropIndex('idx_previous_company_id_owners');
+        $indexes = [
+            'inventory_units_inventory_id_index',
+            'local_market_inventory_units_status_index',
+            'idx_previous_company_id_owners',
+        ];
+        Schema::table('local_market_inventory_units', function (Blueprint $table) use ($indexes) {
+            foreach ($indexes as $index) {
+                $existingIndexes = DB::table('INFORMATION_SCHEMA.STATISTICS')->select('INDEX_NAME')->where('TABLE_SCHEMA', config('database.connections.mysql.database'))->where('TABLE_NAME', 'local_market_inventory_units')->distinct()->where('INDEX_NAME', 'idx_previous_company_id_owners')->exists();
+                if ($existingIndexes) {
+                    $table->dropIndex($index);
+                }
+            }
         });
     }
 
@@ -29,10 +37,7 @@ return new class extends Migration
     public function down()
     {
         DB::statement('CREATE INDEX local_market_inventory_units_status_index ON local_market_inventory_units ((CAST(status AS CHAR(512))))');
-
         DB::statement('CREATE INDEX inventory_units_inventory_id_index ON local_market_inventory_units ((CAST(local_market_inventory_id AS CHAR(512))))');
-
         DB::statement('CREATE INDEX idx_previous_company_id_owners ON local_market_inventory_units ((CAST(previous_company_id_owners AS CHAR(512))))');
-
     }
 };
