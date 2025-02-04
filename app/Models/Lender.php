26dd892bab39efdd409dcd8a9a4b0854001065e1
aadell365 <a.mahmoud@lynk.sa>
2025-02-04 10:26:16 +0200

    fix wallet with lender model

--- app/Models/Lender.php
+++ app/Models/Lender.php
@@ -5,8 +5,18 @@ namespace App\Models;
 
 class Lender extends Company
 {
+    public function getMorphClass()
+    {
+        return Company::class;
+    }
+
     public function lenderDetail()
     {
         return $this->hasOne(CompanyLenderDetail::class, 'company_id');
+    }
+
+    public function getWallet(string $name, bool $lock = true): ?Wallet
+    {
+       return parent::getWallet($name, $lock);
     }
 }
