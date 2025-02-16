0aeb19ad7f7f3768aa362c8841b743ff01352990
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2025-02-16 13:57:37 +0400

    Feat: delete supplier user

---
+++ tests/Feature/Endpoints/Api/V1/Admin/Commodity/CommoditySupplier/Users/SupplierUserControllerDeleteTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Endpoints\Api\V1\Admin\Commodity\CommoditySupplier\Users;
+
+use App\Models\Supplier;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Tests\Traits\InteractsWithCommoditySupplier;
+use Tests\Traits\InteractsWithSupplier;
+use Tests\Traits\InteractsWithUser;
+
+class SupplierUserControllerDeleteTest extends TestCase
+{
+    use InteractsWithCommoditySupplier, InteractsWithSupplier, InteractsWithUser, RefreshDatabase;
+
+    private static Supplier $supplier;
+
+    private static User $userAdmin;
+
+    private static User $supplierUser;
+
+    private static string $endpoint;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        self::$supplier = $this->createSupplier();
+        self::$userAdmin = $this->createSuperAdminUser();
+        self::$supplierUser = $this->createSupplierUser(self::$supplier->id);
+
+        self::$endpoint = 'api/v1/admin/commodity-suppliers/'.self::$supplier->id.'/users/';
+    }
+
+    public function test_un_auth_user_cant_delete_supplier_user(): void
+    {
+        $this->deleteJson(self::$endpoint.(int) self::$supplierUser->id)
+            ->assertUnauthorized()
+            ->assertExactJson([
+                'message' => __('Unauthenticated.'),
+            ]);
+    }
+
+    public function test_admin_user_can_delete_supplier_user_successful(): void
+    {
+        $this->actingAs(self::$userAdmin)
+            ->deleteJson(self::$endpoint.(int) self::$supplierUser->id)
+            ->assertOk()
+            ->assertExactJson(['data' => []]);
+    }
+}
