3e8434a8a29695309bc72f7c1da9d47f097f3f5e
noureddinbadran-lynk <n.badran@lynk.sa>
2025-03-10 01:28:56 +0200

    document the file

--- config/rate_limiter.php
+++ config/rate_limiter.php
@@ -1,6 +1,14 @@
 <?php
 
+/**
+ * Rate Limiter Configuration
+ *
+ * This configuration file defines custom rate limits for API routes in the application.
+ * Each key represents the **route name**, and the corresponding value defines the maximum allowed attempts
+ */
+
 return [
+    // Authentication Routes
     'api.v1.sign-up' => 6,
     'api.v1.lender.sign-up' => 6,
     'api.v1.supplier.sign-up' => 6,
@@ -8,6 +16,8 @@ return [
     'api.v1.supplier.login' => 60,
     'api.v1.verify.otp' => 5,
     'api.v1.verify.email' => 6,
+
+    // Visitor Enquiry Routes
     'api.v1.visitor.enquiry' => 6,
     'api.v1.visitor.enquiry.reply' => 6,
 ];
