f2753f901cefb4a0e7e5216ba36a2ae57a915559
Mohamed Nagy <m.nagy@lynk.sa>
2025-03-16 17:01:07 +0200

    remove type from order controller in admin portal

--- app/Transformers/ProductTransformer.php
+++ app/Transformers/ProductTransformer.php
@@ -10,6 +10,9 @@ use League\Fractal\TransformerAbstract;
 class ProductTransformer extends TransformerAbstract
 {
     protected array $defaultIncludes = [
+    ];
+
+    protected array $availableIncludes = [
         'name',
         'volume_unit',
         'quantity',
@@ -17,8 +20,6 @@ class ProductTransformer extends TransformerAbstract
         'currency',
         'type',
     ];
-
-    protected array $availableIncludes = [];
 
     public function transform(CommodityProductDto $productDto): array
     {
