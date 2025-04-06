67e1d207a16017d82335194d867ff220719ae34c
noureddinbadran-lynk <n.badran@lynk.sa>
2025-04-06 17:14:28 +0200

    Change isolation level to "read committed" and remove unnecessary lock

--- config/database.php
+++ config/database.php
@@ -58,6 +58,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => 'innodb',
+            'isolation_level' => 'read committed',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
