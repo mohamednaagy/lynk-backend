5e314b26172d57d888c549502ab2c7f8b480e53d
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2025-05-07 15:51:17 +0400

    Feat: create unit tests for Trader Products lite list

--- database/settings/2025_05_07_132626_add_column_bursam_default_preferred_commodity_type_to_international_murabaha.php
+++ database/settings/2025_05_07_132626_add_column_bursam_default_preferred_commodity_type_to_international_murabaha.php
@@ -10,10 +10,12 @@ return new class extends SettingsMigration
         $this->migrator->add('international_murabaha.bursam_default_preferred_commodity_type', $this->getDefaultBursamDefaultPreferredCommodityType());
     }
 
-    private function getDefaultBursamDefaultPreferredCommodityType(): int
+    private function getDefaultBursamDefaultPreferredCommodityType(): ?int
     {
         $code = env('APP_ENV') === 'production' ? 'PR-B MSIA14' : 'PR-B-MSIA14';
 
-        return TraderProduct::where('code', $code)->first()->id;
+        $product = TraderProduct::where('code', $code)->first();
+
+        return $product?->id;
     }
 };
