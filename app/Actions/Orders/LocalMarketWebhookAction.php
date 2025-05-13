0d53e89af68ea4a28c4a67dffbb994c51b8d4b7d
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2025-05-13 11:10:47 +0400

    Fix: remove not mandatory lockForUpdate

--- app/Actions/Orders/LocalMarketWebhookAction.php
+++ app/Actions/Orders/LocalMarketWebhookAction.php
@@ -27,9 +27,7 @@ class LocalMarketWebhookAction implements LocalMarketWebhook
 
     public function handle(): void
     {
-        $traderOrder = TraderOrder::lockForUpdate()
-            ->where('reference', $this->data['external_order_no'])
-            ->firstOrFail();
+        $traderOrder = TraderOrder::where('reference', $this->data['external_order_no'])->firstOrFail();
 
         Log::channel('local_market')->info(
             "Update trader by local market webhook. Trader Order ID: {$traderOrder->id} with case => ".$this->data['case']
