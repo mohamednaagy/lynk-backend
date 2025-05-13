c9ba1eb6c45fc2375049b950b2c3c65d7ec2d4ab
Mohamed Nagy <m.nagy@lynk.sa>
2025-05-13 09:16:40 +0300

    proceed order if user add double click

--- app/Services/TraderOrder/TraderOrderProceedCaseService.php
+++ app/Services/TraderOrder/TraderOrderProceedCaseService.php
@@ -28,7 +28,7 @@ class TraderOrderProceedCaseService
      */
     public function createCase(int $traderOrderId, int $case): TraderOrderProceedCase
     {
-        return TraderOrderProceedCase::create([
+        return TraderOrderProceedCase::updateOrCreate([
             'trader_order_id' => $traderOrderId,
             'case' => $case,
         ]);
