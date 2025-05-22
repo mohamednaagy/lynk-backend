29f1d6a07b3ae26346223af729fcb4ed4885fc1a
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2025-05-22 17:25:43 +0400

    Update BaseLynkStrategy.php

--- app/Support/Traders/TradingStrategies/Lynk/BaseLynkStrategy.php
+++ app/Support/Traders/TradingStrategies/Lynk/BaseLynkStrategy.php
@@ -17,6 +17,7 @@ use App\Support\Traders\TradingStrategies\Contracts\TraderStrategyInterface;
 use App\Support\Traders\Traits\TraderHelperTrait;
 use Carbon\CarbonImmutable;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 // TODO_LOCAL_MARKET need to review
 
@@ -91,6 +92,12 @@ abstract class BaseLynkStrategy implements TraderStrategyInterface
         if ($canUpdateOrderStatus) {
             $traderOrder->update([
                 'status' => TraderOrderStatus::Completed,
+            ]);
+        } else {
+            Log::error('LynkStrategy', [
+                'trader_order_id' => $traderOrder->id,
+                'last_action' => $traderOrder->traderHistories()->latest('id')->first()->action,
+                'message' => 'Failed to update order status to completed',
             ]);
         }
 
