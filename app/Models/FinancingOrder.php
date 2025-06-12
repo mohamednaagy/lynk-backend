a6dca9db766fe84ec63ed8b4bb5202e7efb891ea
noureddinbadran-lynk <n.badran@lynk.sa>
2025-06-12 10:12:13 +0300

    add preferred_commodity_type_id to financing_orders table

--- app/Models/FinancingOrder.php
+++ app/Models/FinancingOrder.php
@@ -70,6 +70,7 @@ class FinancingOrder extends Model implements HasMedia, Otpifiable
         'company_id',
         'created_at',
         'charged_trader_orders_count',
+        'preferred_commodity_type_id',
     ];
 
     protected $casts = [
