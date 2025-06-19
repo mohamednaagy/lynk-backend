6e40ea301b15f82ba180f33ff30afbe85c710817
noureddinbadran-lynk <n.badran@lynk.sa>
2025-06-19 14:13:58 +0300

    Remove unused param from get Auth user api

--- app/Http/Controllers/Api/V1/Lender/Auth/GetAuthUser.php
+++ app/Http/Controllers/Api/V1/Lender/Auth/GetAuthUser.php
@@ -32,7 +32,6 @@ class GetAuthUser extends Controller
                 'company.is_tiered',
                 'company.trading_mode',
                 'company.contract_number',
-                'company.allow_preferred_commodity_in_order',
                 'permissions',
                 'locale',
                 'phone_number',
