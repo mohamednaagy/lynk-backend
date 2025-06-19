39aafc775f41a5984b1f2bf8476b17110bdca0cb
Mohamed Nagy <m.nagy@lynk.sa>
2025-06-19 11:48:12 +0300

    enhancment

--- app/Support/Traders/Drivers/Bursam/Jobs/V2/ProcessBursamOrderResultYNN.php
+++ app/Support/Traders/Drivers/Bursam/Jobs/V2/ProcessBursamOrderResultYNN.php
@@ -80,7 +80,7 @@ class ProcessBursamOrderResultYNN implements ShouldBeUnique, ShouldQueue
 
     private function shouldSkipRetry(Exception $e): bool
     {
-        return in_array($e->getContext('failure_code'), BursamErrorCode::UNAVAILABLE_PRODUCT_ERROR_CODES) || in_array($e->getContext('failure_code'), BursamErrorCode::UNAVAILABLE_INVENTORY_ERROR_CODES);
+        return $this->isUnavailableCommoditiesCode($e->getContext('failure_code'));
     }
 
     public function failed($exception)
@@ -99,7 +99,7 @@ class ProcessBursamOrderResultYNN implements ShouldBeUnique, ShouldQueue
             $traderOrder->order->update([
                 'status' => FinancingOrderStatus::TradingFailure,
             ]);
-            $cancel_reason = in_array($exception->getContext('failure_code'), BursamErrorCode::UNAVAILABLE_PRODUCT_ERROR_CODES) || in_array($exception->getContext('failure_code'), BursamErrorCode::UNAVAILABLE_INVENTORY_ERROR_CODES) ? TraderOrderCancelReason::NoEligibleCommoditiesAvailable : TraderOrderCancelReason::FailureToPurchase;
+            $cancel_reason = $this->isUnavailableCommoditiesCode($exception->getContext('failure_code')) ? TraderOrderCancelReason::NoEligibleCommoditiesAvailable : TraderOrderCancelReason::FailureToPurchase;
             app(UpdateTraderOrderStatusToPendingCancel::class)->handle($traderOrder, $cancel_reason);
             app(UpdateTraderOrderStatusToCancel::class)->handle(
                 $traderOrder,
@@ -109,6 +109,13 @@ class ProcessBursamOrderResultYNN implements ShouldBeUnique, ShouldQueue
                     : ''
             );
         });
+    }
+
+    private function isUnavailableCommoditiesCode(string $code): bool
+    {
+        return in_array($code, array_merge(
+            BursamErrorCode::UNAVAILABLE_PRODUCT_ERROR_CODES, BursamErrorCode::UNAVAILABLE_INVENTORY_ERROR_CODES
+        ));
     }
 
     public function middleware(): array
