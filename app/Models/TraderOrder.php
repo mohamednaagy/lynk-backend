649213e0e0c22fed3587b33c648f3ef94fe8cccb
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2025-07-16 18:01:20 +0400

    Update TraderOrder.php

--- app/Models/TraderOrder.php
+++ app/Models/TraderOrder.php
@@ -40,11 +40,6 @@ class TraderOrder extends Model implements HasMedia
 {
     use HasFactory, InteractsWithMedia, VirtualColumn;
 
-    protected $fillable = [
-        'last_history_action',
-        'last_history_action_updated_at',
-    ];
-
     protected $guarded = [];
 
     public static function getCustomColumns(): array
