4d4eef7529483ba8b09041b7342b10cd54d5a8f0
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2025-07-21 16:58:26 +0400

    Feat: enhance validation

--- app/Http/Requests/V1/Admin/Companies/StoreCompanyRequest.php
+++ app/Http/Requests/V1/Admin/Companies/StoreCompanyRequest.php
@@ -178,7 +178,7 @@ class StoreCompanyRequest extends FormRequest
             ],
 
             'lender_order_allowed_commodity_types' => [
-                'nullable', 'array',
+                'nullable', 'array', 'required_if:allow_preferred_commodity_in_order,true',
             ],
 
             'lender_order_allowed_commodity_types.*' => [
--- app/Http/Requests/V1/Admin/Companies/UpdateCompanyRequest.php
+++ app/Http/Requests/V1/Admin/Companies/UpdateCompanyRequest.php
@@ -183,7 +183,7 @@ class UpdateCompanyRequest extends FormRequest
             ],
 
             'lender_order_allowed_commodity_types' => [
-                'nullable', 'array',
+                'nullable', 'array', 'required_if:allow_preferred_commodity_in_order,true',
             ],
 
             'lender_order_allowed_commodity_types.*' => [
