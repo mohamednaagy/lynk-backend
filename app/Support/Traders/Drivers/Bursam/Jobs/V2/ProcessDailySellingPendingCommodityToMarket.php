a180f901b44700bd3fdc0ff3dbb0f793452b4e67
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2025-08-14 18:28:15 +0300

    remove jobid

--- app/Support/Traders/Drivers/Bursam/Jobs/V2/ProcessDailySellingPendingCommodityToMarket.php
+++ app/Support/Traders/Drivers/Bursam/Jobs/V2/ProcessDailySellingPendingCommodityToMarket.php
@@ -24,7 +24,7 @@ class ProcessDailySellingPendingCommodityToMarket implements ShouldQueue
     public function __construct()
     {
         $this->onQueue('bursam');
-        Log::channel('bursam')->info('ProcessDailySellingPendingCommodityToMarket: Job constructor', ['job_id' => $this->job->getJobId()]);
+        Log::channel('bursam')->info('ProcessDailySellingPendingCommodityToMarket: Job constructor']);
     }
 
     /**
