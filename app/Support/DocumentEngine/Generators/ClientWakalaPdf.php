c64fcbf7165bc25a7d8f33e4ff68e707e3ff645c
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2025-08-18 13:52:09 +0400

    Feat: refactor client-wakala. pdf

--- app/Support/DocumentEngine/Generators/ClientWakalaPdf.php
+++ app/Support/DocumentEngine/Generators/ClientWakalaPdf.php
@@ -2,6 +2,9 @@
 
 namespace App\Support\DocumentEngine\Generators;
 
+use App\Actions\Contracts\Wakala\GetClientWakalaText;
+use App\Actions\Contracts\Wakala\GetWakalaTemplate;
+use App\Enums\MediaCollections\TraderOrderMediaCollection;
 use App\Support\DocumentEngine\BasePdfGenerator;
 use App\Support\DocumentEngine\Traits\HasTraderOrder;
 
@@ -9,30 +12,42 @@ class ClientWakalaPdf extends BasePdfGenerator
 {
     use HasTraderOrder;
 
+    protected $collectionName = TraderOrderMediaCollection::ClientWakala;
+
     public function getStorageCallback(): callable
     {
-        return function ($path) {
-            return storage_path($path);
+        return function ($fileResource) {
+            return $this->getTraderOrder()
+                ->addMediaFromStream($fileResource)
+                ->usingFileName($this->getTraderOrder()->order->getNationalId().'.pdf')
+                ->toMediaCollection($this->collectionName);
         };
     }
 
     public function isGeneratedBefore(): bool
     {
-        return false;
+        return $this->getTraderOrder()->hasMedia($this->collectionName);
     }
 
     public function getGeneratedBeforePath(): string
     {
-        return '';
+        return $this->getTraderOrder()->getMedia($this->collectionName)->first()->getPath();
     }
 
     protected function prepareData()
     {
-        return $this->context;
+        $getWakalaTemplate = app(GetWakalaTemplate::class);
+        $getClientWakalaText = app(GetClientWakalaText::class);
+        $lenderTemplate = $getWakalaTemplate->handle('client')['wakala_template'];
+        $template = $getClientWakalaText->handle($this->getTraderOrder(), $lenderTemplate);
+
+        return [
+            'template' => $template,
+        ];
     }
 
     protected function getTemplatePath(): string
     {
-        return 'pdf.client_wakala';
+        return 'templates.client-wakala';
     }
 }
