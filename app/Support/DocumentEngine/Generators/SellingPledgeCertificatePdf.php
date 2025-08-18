874b0f39e0e10c0ed81179b2a75d5a86aaf199dc
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2025-08-18 15:02:58 +0400

    Feat: stanndaradize sale peldge certificate

--- app/Support/DocumentEngine/Generators/SellingPledgeCertificatePdf.php
+++ app/Support/DocumentEngine/Generators/SellingPledgeCertificatePdf.php
@@ -2,30 +2,54 @@
 
 namespace App\Support\DocumentEngine\Generators;
 
+use App\Enums\MediaCollections\TraderOrderMediaCollection;
+use App\Support\DataTransferObjects\LynkCommodityProductDto;
 use App\Support\DocumentEngine\BasePdfGenerator;
+use App\Support\DocumentEngine\Traits\HasLynkCommodityProducts;
+use App\Support\DocumentEngine\Traits\HasTraderOrder;
+use Carbon\CarbonImmutable;
 
 class SellingPledgeCertificatePdf extends BasePdfGenerator
 {
+    use HasLynkCommodityProducts, HasTraderOrder;
+
+    protected $collectionName = TraderOrderMediaCollection::LynkSalePledgeCertificate;
+
     public function getStorageCallback(): callable
     {
-        return function ($path) {
-            return storage_path($path);
+        return function ($fileResource) {
+            $this->attachDocumentToOrder(
+                $this->getTraderOrder(),
+                $fileResource,
+                $this->collectionName
+            );
         };
     }
 
     public function isGeneratedBefore(): bool
     {
-        return false;
+        return $this->getTraderOrder()->hasMedia($this->collectionName);
     }
 
     public function getGeneratedBeforePath(): string
     {
-        return '';
+        return $this->getTraderOrder()->getMedia($this->collectionName)->first()->getPath();
     }
 
     protected function prepareData(): array
     {
-        return [];
+        $traderOrder = $this->getTraderOrder();
+        $financeOrder = $traderOrder->order;
+        $currentTimeInRiyadhTz = CarbonImmutable::now()->timezone('Asia/Riyadh');
+
+        return [
+            'products' => $this->transformProductsToLynkCommodityProductsDTO($traderOrder->products, LynkCommodityProductDto::groupedByKeys()),
+            'trader_order_reference' => $traderOrder->reference,
+            'amount' => $financeOrder->amount->convertAndFormatByDecimal(sperator: ','),
+            'customer_name' => $financeOrder->customer_name,
+            'current_date' => $currentTimeInRiyadhTz->toDateString(),
+            'current_time' => $currentTimeInRiyadhTz->toTimeString(),
+        ];
     }
 
     protected function getTemplatePath(): string
