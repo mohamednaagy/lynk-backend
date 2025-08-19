e748b1631c0ee1a35d313b19eae05696e28aef61
Mohamed Nagy <m.nagy@lynk.sa>
2025-08-19 18:07:42 +0300

    Add constructor to DeleteCommodityItem job for CommodityItem dependency injection

--- app/Jobs/LocalMarket/DeleteCommodityItem.php
+++ app/Jobs/LocalMarket/DeleteCommodityItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs\LocalMarket;
 
+use App\Models\CommodityItem;
 use App\Services\LocalMarket\InventoryService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -15,6 +16,14 @@ class DeleteCommodityItem implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    private CommodityItem $commodityItem;
+
+    
+    public function __construct(CommodityItem $commodityItem)
+    {
+        $this->commodityItem = $commodityItem;
+    }
+    
     /**
      * Execute the job.
      */
