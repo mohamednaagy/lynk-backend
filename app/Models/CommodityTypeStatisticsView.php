db75dca73ba1795b87fb618e0d6ee70dc2e05123
Mohamed Nagy <m.nagy@lynk.sa>
2025-08-19 16:41:45 +0300

    Implement currency casting for commodity type statistics and update view for active items only

--- app/Models/CommodityTypeStatisticsView.php
+++ app/Models/CommodityTypeStatisticsView.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Support\Money\Casts\MoneyStringCast;
 use Illuminate\Database\Eloquent\Model;
 
 class CommodityTypeStatisticsView extends Model
@@ -11,4 +12,11 @@ class CommodityTypeStatisticsView extends Model
     public $timestamps = false;
 
     protected $primaryKey = 'commodity_type_id';
+
+    protected $casts = [
+        'total_value' => MoneyStringCast::class.':currency',
+        'available_value' => MoneyStringCast::class.':currency',
+        'reserved_value' => MoneyStringCast::class.':currency',
+    ];
+
 }
