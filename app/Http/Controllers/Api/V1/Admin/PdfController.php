b29f646a25a62d013da8f6ea284affe133800b85
hossamAhmed345 <105289028+hossamAhmed345@users.noreply.github.com>
2025-08-21 12:56:02 +0400

    Feat: remove children ovveride

--- app/Http/Controllers/Api/V1/Admin/PdfController.php
+++ app/Http/Controllers/Api/V1/Admin/PdfController.php
@@ -3,13 +3,5 @@
 namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Api\V1\PdfController as BasePdfController;
-use App\Http\Requests\GeneratePdfRequest;
-use Illuminate\Http\JsonResponse;
 
-class PdfController extends BasePdfController
-{
-    public function generate(GeneratePdfRequest $request): JsonResponse
-    {
-        return parent::generate($request);
-    }
-}
+class PdfController extends BasePdfController {}
