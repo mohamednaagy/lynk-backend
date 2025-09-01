b9392a7fbefc403fd5d81a158963947e382fe901
Mohamed Nagy <m.nagy@lynk.sa>
2025-09-01 09:21:51 +0300

    Refactor validation rules in financing order validators
    - Updated phone number validation to be conditional based on the 'is_verification_required' field.
    - Changed amount validation to require a minimum value of 1 instead of greater than 0.
    - Ensured 'is_verification_required' field is consistently defined across all validators.

--- app/Http/Requests/V1/Admin/FinancingOrders/Validators/TimeDepositValidator.php
+++ app/Http/Requests/V1/Admin/FinancingOrders/Validators/TimeDepositValidator.php
@@ -13,13 +13,13 @@ class TimeDepositValidator implements FinancingProductValidatorInterface
         {
             return array_merge($baseRules, [
                 'company_id' => ['required', Rule::exists(Company::class, 'id')],
-                'is_verification_required' => ['required', 'boolean'],
                 'financing_product_id' => ['nullable', 'integer', new EnumValue(FinancingProductEnum::class, false)],
                 'national_id' => ['required', 'integer', 'digits:10', 'gt:0'],
                 'phone_country_code' => ['required_with:phone_number', 'string', 'size:2'],
-                'phone_number' => ['required', 'phone:phone_country_code,mobile', 'string'],
-                'amount' => ['required', 'numeric', 'gt:0'],
+                'phone_number' => ['required_if:is_verification_required,true', 'string', 'phone:phone_country_code,mobile'],
+                'amount' => ['required', 'numeric', 'gte:1'],
                 'selling_price' => ['required', 'numeric', 'gte:amount'],
+                'is_verification_required' => ['required', 'boolean'],
                 'customer_name' => ['required', 'string', 'max:255'],
             ]);
         }
