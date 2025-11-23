7bd412c3f624d7de00e1df05b87a661b14de5922
noureddinbadran-lynk <n.badran@lynk.sa>
2025-11-23 18:20:55 +0200

    Certificate URLs should not show up in GET Show Order until step is complete

--- app/Transformers/TraderHistoryTransformers/AbstractTraderHistoryTransformer.php
+++ app/Transformers/TraderHistoryTransformers/AbstractTraderHistoryTransformer.php
@@ -62,12 +62,8 @@ abstract class AbstractTraderHistoryTransformer extends TransformerAbstract
             'is_complete' => (bool) $history,
             'completed_at' => optional($history) ? saudi_now('Y-m-d h:i:s A', optional($history)->created_at) : null,
             'ownership_document' => [
-                'url' => formatMediaUrl(route('api.v1.admins.generate', [
-                    'document_type' => DocumentType::TRANSFER_OWNERSHIP_TO_LENDER,
-                    'context' => [
-                        'trader_order_id' => $this->traderOrder->id,
-                    ],
-                ])),
+                'url' => $history ? $this->getCertificateLURL(DocumentType::TRANSFER_OWNERSHIP_TO_LENDER,
+                    $this->traderOrder->id) : null,
                 'date' => $history ? saudi_now('Y-m-d h:i:s A', $history->created_at) : null,
             ],
             'duration' => $this->getDurationForHistoryStep($lastHistoryOfStepNode),
@@ -75,12 +71,8 @@ abstract class AbstractTraderHistoryTransformer extends TransformerAbstract
 
         if ($this->traderOrder->provider === TraderEnum::Bursam) {
             $data['cert_document'] = [
-                'url' => formatMediaUrl(route('api.v1.admins.generate', [
-                    'document_type' => DocumentType::BURSAM_BID_CERTIFICATE,
-                    'context' => [
-                        'trader_order_id' => $this->traderOrder->id,
-                    ],
-                ])),
+                'url' => $history ? $this->getCertificateLURL(DocumentType::BURSAM_BID_CERTIFICATE,
+                    $this->traderOrder->id) : null,
                 'date' => $history ? saudi_now('Y-m-d h:i:s A', $history->created_at) : null,
             ];
         }
@@ -103,7 +95,7 @@ abstract class AbstractTraderHistoryTransformer extends TransformerAbstract
             'is_deliverable' => $this->traderOrder->isDeliverable(),
             'client_wakala_message' => Trader::driver($this->traderOrder->provider, $this->traderOrder->version)->clientWakalaMessage($this->traderOrder),
             'signed_wakala_document' => [
-                'url' => $signedWakalaDocumentMediaFile?->file_url,
+                'url' => $history ? $signedWakalaDocumentMediaFile?->file_url : null,
                 'date' => $signedWakalaDocumentMediaFile ? saudi_now('Y-m-d h:i:s A', $signedWakalaDocumentMediaFile->created_at) : null,
             ],
             'duration' => $this->getDurationForHistoryStep($lastHistoryOfStepNode),
@@ -126,12 +118,8 @@ abstract class AbstractTraderHistoryTransformer extends TransformerAbstract
             'is_deliverable' => $this->traderOrder->isDeliverable(),
             'contract_signed_message' => Trader::driver($this->traderOrder->provider, $this->traderOrder->version)->contractSignedMessage($this->traderOrder),
             'wakala_document' => [
-                'url' => formatMediaUrl(route('api.v1.admins.generate', [
-                    'document_type' => DocumentType::CLIENT_WAKALA,
-                    'context' => [
-                        'trader_order_id' => $this->traderOrder->id,
-                    ],
-                ])),
+                'url' => $history ? $this->getCertificateLURL(DocumentType::CLIENT_WAKALA,
+                    $this->traderOrder->id) : null,
                 'date' => $transferOwnershipToLenderDocumentHistory ? saudi_now('Y-m-d h:i:s A', $transferOwnershipToLenderDocumentHistory->created_at) : null,
             ],
             'duration' => $this->getDurationForHistoryStep($lastHistoryOfStepNode),
@@ -155,12 +143,8 @@ abstract class AbstractTraderHistoryTransformer extends TransformerAbstract
             'is_complete' => (bool) $history,
             'completed_at' => $history ? saudi_now('Y-m-d h:i:s A', $history->created_at) : null,
             'borrower_document' => [
-                'url' => formatMediaUrl(route('api.v1.admins.generate', [
-                    'document_type' => DocumentType::SELLING_COMMODITY_TO_CUSTOMER,
-                    'context' => [
-                        'trader_order_id' => $this->traderOrder->id,
-                    ],
-                ])),
+                'url' => $history ? $this->getCertificateLURL(DocumentType::SELLING_COMMODITY_TO_CUSTOMER,
+                    $this->traderOrder->id) : null,
                 'date' => $history ? saudi_now('Y-m-d h:i:s A', $history->created_at) : null,
             ],
             'duration' => $this->getDurationForHistoryStep($lastHistoryOfStepNode),
@@ -178,12 +162,8 @@ abstract class AbstractTraderHistoryTransformer extends TransformerAbstract
             'is_complete' => (bool) $history,
             'completed_at' => $history ? saudi_now('Y-m-d h:i:s A', $history->created_at) : null,
             'mpo_document' => [
-                'url' => formatMediaUrl(route('api.v1.admins.generate', [
-                    'document_type' => DocumentType::getSellingPledgeCertificateType($this->traderOrder->provider),
-                    'context' => [
-                        'trader_order_id' => $this->traderOrder->id,
-                    ],
-                ])),
+                'url' => $history ? $this->getCertificateLURL(DocumentType::getSellingPledgeCertificateType($this->traderOrder->provider),
+                    $this->traderOrder->id) : null,
                 'date' => $history ? saudi_now('Y-m-d h:i:s A', $history->created_at) : null,
             ],
             'duration' => $this->getDurationForHistoryStep($lastHistoryOfStepNode),
@@ -201,12 +181,12 @@ abstract class AbstractTraderHistoryTransformer extends TransformerAbstract
             'is_complete' => (bool) $history,
             'completed_at' => optional($history)->created_at?->clone()->tz('Asia/Riyadh')->format('Y-m-d h:i:s A'),
             'warranty_document' => [
-                'url' => formatMediaUrl(route('api.v1.admins.generate', [
+                'url' => $history ? formatMediaUrl(route('api.v1.admins.generate', [
                     'document_type' => DocumentType::getSellingPledgeCertificateType($this->traderOrder->provider),
                     'context' => [
                         'trader_order_id' => $this->traderOrder->id,
                     ],
-                ])),
+                ])) : null,
                 'date' => $history ? saudi_now('Y-m-d h:i:s A', $history->created_at) : null,
             ],
             'duration' => $this->getDurationForHistoryStep($lastHistoryOfStepNode),
@@ -360,5 +340,15 @@ abstract class AbstractTraderHistoryTransformer extends TransformerAbstract
             ])),
             'date' => $settlement->created_at,
         ];
+    }
+
+    private function getCertificateLURL(string $type, int $traderOrderId): string
+    {
+        return formatMediaUrl(route('api.v1.admins.generate', [
+            'document_type' => $type,
+            'context' => [
+                'trader_order_id' => $traderOrderId,
+            ],
+        ]));
     }
 }
