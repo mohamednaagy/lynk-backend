34fad9dee56c125fa180111d5fb684bb104f24a9
Mohamed Nagy <m.nagy@lynk.sa>
2025-11-26 09:42:38 +0200

    refactor(UnitService): update query to use join instead of raw index and drop unused index migration

--- app/Services/LocalMarket/UnitService.php
+++ app/Services/LocalMarket/UnitService.php
@@ -238,18 +238,19 @@ class UnitService
         $ownershipTypeOriginalSupplier = OwnershipTypes::OriginalSupplier;
         $previousOrdersText = trans('local-market.old_request', [], 'ar');
 
-        return LocalMarketInventoryUnits::selectRaw(
-            'local_market_inventory_units.local_market_inventory_id,
+        return LocalMarketInventoryUnits::select(
+            'local_market_inventory_units.local_market_inventory_id',
+            DB::raw("
             CASE
-                WHEN local_market_inventory_units.previous_owner_type = ?
+                WHEN local_market_inventory_units.previous_owner_type = $ownershipTypeOriginalSupplier
                 THEN companies.name
-                ELSE ?
-            END AS previous_owner,
-            local_market_inventory_units.previous_owner_type,
-            COUNT(*) AS unit_count',
-            [$ownershipTypeOriginalSupplier, $previousOrdersText]
+                ELSE '$previousOrdersText'
+            END AS previous_owner
+        "),
+            'local_market_inventory_units.previous_owner_type',
+            DB::raw('COUNT(*) AS unit_count'),
         )
-            ->fromRaw('local_market_inventory_units FORCE INDEX (idx_units_previous_owner_grouping)')
+            ->join('local_market_inventories', 'local_market_inventories.id', '=', 'local_market_inventory_units.local_market_inventory_id')
             ->leftJoin('companies', 'companies.id', '=', 'local_market_inventory_units.previous_owner')
             ->where('local_market_inventory_units.hold_for', $localMarketOrder->id)
             ->groupBy(
