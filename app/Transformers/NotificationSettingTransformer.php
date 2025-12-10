545d48edfcbcdee85963dab748f23cfc2f38a46d
hossamAhmed345 <hossam@github.com>
2025-12-10 11:07:30 +0400

    feat(NotificationSettingTransformer): add label field using SystemNotificationType
    Add label field to transformed notification settings by getting description from SystemNotificationType enum. This provides more user-friendly display names for notification types.

--- app/Transformers/NotificationSettingTransformer.php
+++ app/Transformers/NotificationSettingTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Transformers;
 
+use App\Enums\SystemNotificationType;
 use League\Fractal\TransformerAbstract;
 
 class NotificationSettingTransformer extends TransformerAbstract
@@ -9,16 +10,22 @@ class NotificationSettingTransformer extends TransformerAbstract
     public function transform($data): array
     {
         if (is_array($data)) {
+            $label = SystemNotificationType::getDescription($data['name']);
+
             return [
                 'id' => $data['id'],
                 'name' => $data['name'],
+                'label' => $label,
                 'is_enabled' => (bool) $data['is_enabled'],
             ];
         }
 
+        $label = SystemNotificationType::getDescription($data->name);
+
         return [
             'id' => $data->id,
             'name' => $data->name,
+            'label' => $label,
             'is_enabled' => (bool) $data->is_enabled,
         ];
     }
