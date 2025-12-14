43778b9844534ab9d2cb71d212935fc8e8201b43
Mohamed Mahdy <m.mahdy@lynk.sa>
2025-12-14 10:08:39 +0200

    feat(migrations): add down method for wallet notifications table

--- database/migrations/2025_12_11_102432_drop_wallet_notifications_table.php
+++ database/migrations/2025_12_11_102432_drop_wallet_notifications_table.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Company;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -8,5 +10,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::dropIfExists('wallet_notifications');
+    }
+
+    public function down(): void
+    {
+        Schema::create('wallet_notifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Company::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('wallet_id')->index();
+            $table->tinyInteger('type');
+            $table->decimal('value', 64, 0);
+            $table->timestamp('notified_at')->nullable();
+            $table->timestamps();
+        });
     }
 };
