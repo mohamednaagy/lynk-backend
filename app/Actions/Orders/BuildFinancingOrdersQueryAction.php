0e085f9d281d5047a49e46e0078e2dc5b6a38fd2
Mohamed Nagy <m.nagy@lynk.sa>
2025-12-14 13:14:10 +0200

    feat: add filter charged transactions at order list

--- app/Actions/Orders/BuildFinancingOrdersQueryAction.php
+++ app/Actions/Orders/BuildFinancingOrdersQueryAction.php
@@ -8,6 +8,7 @@ use App\Models\Company;
 use App\Models\FinancingOrder;
 use App\Support\QueryScoper\Scopes\FinancingOrders\OrderAmountScope;
 use App\Support\QueryScoper\Scopes\FinancingOrders\OrderAssignableScope;
+use App\Support\QueryScoper\Scopes\FinancingOrders\OrderChargedTransactionScope;
 use App\Support\QueryScoper\Scopes\FinancingOrders\OrderCompanyScope;
 use App\Support\QueryScoper\Scopes\FinancingOrders\OrderDateScope;
 use App\Support\QueryScoper\Scopes\FinancingOrders\OrderFilterScope;
@@ -47,6 +48,7 @@ class BuildFinancingOrdersQueryAction implements BuildFinancingOrdersQuery
             'filter' => new OrderFilterScope,
             'company' => new OrderCompanyScope,
             'date' => new OrderDateScope,
+            'charged_transaction' => new OrderChargedTransactionScope,
         ];
     }
 
