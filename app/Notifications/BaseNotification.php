64cc6d11d0f7d11b6d547259b5db75ca89aa7cd9
Mohamed Mahdy <m.mahdy@lynk.sa>
2026-01-18 11:46:06 +0200

    feat(notification): add role-based notification filtering
    Implement role-based access control for system notifications.
    A new `userHasAllowedRole` method checks if the recipient has one of the roles configured for the specific notification type. Notifications will only be sent to users who possess an allowed role, as defined in `config('notification-types.<type>.roles')`. If no roles are specified, it defaults to allowing all users.

--- app/Notifications/BaseNotification.php
+++ app/Notifications/BaseNotification.php
@@ -31,6 +31,11 @@ abstract class BaseNotification extends Notification
      */
     public function via($notifiable): array
     {
+        // Check if the user has allowed roles for this notification type
+        if (! $this->userHasAllowedRole($notifiable)) {
+            return [];
+        }
+
         $typeSettings = app(NotificationPreferenceService::class)->getUserNotificationTypeSettings($notifiable, $this->getType());
 
         return $this->getUserEnabledChannels($typeSettings);
@@ -80,6 +85,32 @@ abstract class BaseNotification extends Notification
      * @param  mixed  $notifiable
      */
     abstract public function getDescription($notifiable): string;
+
+    /**
+     * Check if the user has allowed roles for this notification type.
+     *
+     * @param  mixed  $notifiable
+     */
+    private function userHasAllowedRole($notifiable): bool
+    {
+        $notificationType = $this->getType()->value;
+        $allowedRoles = config("notification-types.{$notificationType}.roles", []);
+
+        // If there are no allowed roles defined in config, allow all users
+        if (empty($allowedRoles)) {
+            return true;
+        }
+
+        // Validate that the user has one of the allowed roles
+        foreach ($allowedRoles as $role) {
+            if ($notifiable->hasRole($role)) {
+                return true;
+            }
+        }
+
+        // User doesn't have any of the allowed roles
+        return false;
+    }
 
     /**
      * Specify which queue should handle which channels.
