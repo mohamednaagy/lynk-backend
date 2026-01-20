3dd761c9235376e3ab2ad28d28b0ff1031ffb7bd
Mohamed Nagy <m.nagy@lynk.sa>
2026-01-20 14:13:13 +0200

    fix(requests): increase max length for commodity item name validation from 16 to 32 characters in Store and Update requests

--- app/Http/Requests/V1/Admin/Commodities/CommodityType/StoreCommodityTypeRequest.php
+++ app/Http/Requests/V1/Admin/Commodities/CommodityType/StoreCommodityTypeRequest.php
@@ -48,6 +48,7 @@ class StoreCommodityTypeRequest extends FormRequest
                 'required',
                 'string',
                 'min:3',
+                'max:16',
                 new CommodityTypeUniqueNameRole,
                 Rule::unique(CommodityType::class, 'unique_name'),
 
--- app/Http/Requests/V1/Admin/Commodities/CommodityType/UpdateCommodityTypeRequest.php
+++ app/Http/Requests/V1/Admin/Commodities/CommodityType/UpdateCommodityTypeRequest.php
@@ -48,6 +48,7 @@ class UpdateCommodityTypeRequest extends FormRequest
                 'required',
                 'string',
                 'min:3',
+                'max:16',
                 new CommodityTypeUniqueNameRole,
                 Rule::unique(CommodityType::class, 'unique_name')->ignore($this->route('commodity_type')),
 
