8a56f2aa7c7bc9eff0749892ed03aa93db986e0b
Mohamed Mahdy <m.mahdy@lynk.sa>
2026-01-26 17:21:43 +0200

    Add deprecated code plugin

--- app/Http/Controllers/Api/V1/Lender/Orders/CancelTraderOrder.php
+++ app/Http/Controllers/Api/V1/Lender/Orders/CancelTraderOrder.php
@@ -15,7 +15,6 @@ use App\Jobs\FinancingOrders\NotifyAboutTraderOrderCancelled;
 use App\Models\TraderOrder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CancelTraderOrder extends Controller
@@ -73,21 +72,5 @@ class CancelTraderOrder extends Controller
 
             return $this->successResponse();
         });
-    }
-
-    private function dead()
-    {
-        Log::channel(LOG_CHANNEL_LYNK)->info('Test phpstan dead code detection');
-        $this->testDeprecatedCodeDetection();
-    }
-
-    /**
-     * @deprecated
-     *
-     * @return void
-     */
-    public function testDeprecatedCodeDetection()
-    {
-        Log::channel(LOG_CHANNEL_LYNK)->info('Test phpstan deprecated code detection');
     }
 }
