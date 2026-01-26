46b44e3d842b250d61991633a753e692f2223039
Mohamed Mahdy <m.mahdy@lynk.sa>
2026-01-26 18:13:30 +0200

    Enhance hook script

--- app/Jobs/FinancingOrders/NotifyAboutTraderOrderCancelled.php
+++ app/Jobs/FinancingOrders/NotifyAboutTraderOrderCancelled.php
@@ -35,7 +35,6 @@ class NotifyAboutTraderOrderCancelled implements ShouldQueue
     {
         $companyId = $this->traderOrder->order->company_id;
 
-        // good query
         $notifiables = app(NotificationPreferenceService::class)
             ->getEnabledUsersFor(SystemNotificationType::TRADE_REQUEST_CANCELLED, function ($query) use ($companyId) {
                 $query->forTradeRequestCancelledNotification($companyId);
