1f79165a39833742f97d53f3d311814601382d23
Mohamed Mahdy <m.mahdy@lynk.sa>
2026-01-26 13:20:44 +0200

    Test pre-commit

--- app/Http/Controllers/Api/V1/Lender/Orders/CancelTraderOrder.php
+++ app/Http/Controllers/Api/V1/Lender/Orders/CancelTraderOrder.php
@@ -67,6 +67,7 @@ class CancelTraderOrder extends Controller
                 TraderOrderCancelReason::TraderOrderIsCancelled
             );
 
+            // test pre-commit hook
             dispatch(new NotifyAboutTraderOrderCancelled($traderOrder, $canceller));
 
             return $this->successResponse();
