3810631f303ff5cc45c9716dbfdc5c1ee3c035f1
Mohamed Mahdy <m.mahdy@lynk.sa>
2026-01-26 13:35:17 +0200

    Test pre-commit

--- app/Http/Controllers/Api/V1/Lender/Orders/CancelTraderOrder.php
+++ app/Http/Controllers/Api/V1/Lender/Orders/CancelTraderOrder.php
@@ -43,7 +43,7 @@ class CancelTraderOrder extends Controller
         return DB::transaction(function () use ($request, $cancelTraderOrder, $traderOrder) {
             $traderOrder = TraderOrder::lockForUpdate()->findOrFail($traderOrder);
 
-            if (! $traderOrder->canBeCancelled()) {
+            if (! $traderOrder->canBeCancelled(10)) {
                 return $this->errorResponse(
                     __('error.unable_to_cancel_order'),
                     Response::HTTP_FORBIDDEN,
