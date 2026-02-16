6f1d0eb3f0edc99f9618bd32fcfaa42c60952759
noureddinbadran-lynk <n.badran@lynk.sa>
2026-02-16 16:43:01 +0200

    make-in-progress-orders-notificaiton-api-available-for-other-envs-temporarly

--- routes/api/test.php
+++ routes/api/test.php
@@ -9,6 +9,8 @@ Route::prefix('v1/test')->group(function () {
     if (app()->environment('local')) {
         Route::post('loan-coverage', [LoanController::class, 'calculateLoanCoverage']);
         Route::post('reports/generate-supplier-monthly-usage', [ReportsController::class, 'generateSupplierMonthlyUsage']);
-        Route::post('notifications/send-in-progress-orders', [NotificationsController::class, 'sendInProgressOrders']);
     }
+
+    // TODO move it to inside the if statement after the release v1.34.0 go to production
+    Route::post('notifications/send-in-progress-orders', [NotificationsController::class, 'sendInProgressOrders']);
 });
