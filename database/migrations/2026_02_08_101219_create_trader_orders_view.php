5eff6487be7ca38af3cbbfc62b5d7f2079c5431a
Mohamed Naser <163412155+mohamednaseramein@users.noreply.github.com>
2026-03-01 11:58:58 +0200

    add created at and refrence columns to trader order views

--- database/migrations/2026_02_08_101219_create_trader_orders_view.php
+++ database/migrations/2026_02_08_101219_create_trader_orders_view.php
@@ -11,13 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('trader_orders_view');
         DB::statement('
             CREATE VIEW trader_orders_view AS
             SELECT
                 t.id AS id,
                 t.financing_order_id,
                 t.mode,
+                t.reference,
                 t.provider,
+                t.created_at,
                 ttl.effective_at as expire_at
             FROM trader_orders t
             LEFT JOIN trader_order_time_limits ttl
