7d89ef4e4972e56363b5ce3c6677e878dbf35483
Mohamed Nagy <m.nagy@lynk.sa>
2026-03-16 11:07:01 +0200

    Enhance OrderStatus Enum: Add Delivered and FailedDelivery statuses

--- app/Enums/LocalMarket/OrderStatus.php
+++ app/Enums/LocalMarket/OrderStatus.php
@@ -102,6 +102,8 @@ final class OrderStatus extends Enum implements LocalizedEnum
         self::TransferOwnershipToCustomer => [
             self::PendingSellCommodities,
             self::PendingCancellation,
+            self::Delivered,
+            self::FailedDelivery,
         ],
 
     ];
